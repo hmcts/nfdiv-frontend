a4ddd2966f7f00ec00003356fa8267e9d6fc1f02
Linus Norton <linusnorton@gmail.com>
2020-12-01 08:00:30 +0000

    smoke tests

---
+++ src/test/smoke/smoke.ts
@@ -0,0 +1,13 @@
+const superagent = require('superagent');
+const { config } = require('../../../config');
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+describe('Smoke Test', () => {
+  describe('healthcheck', () => {
+    test('should return status 200', async () => {
+      const response = await superagent.get(config.TEST_URL);
+
+      expect(response.statusCode).toBe(200);
+    });
+  });
+});
