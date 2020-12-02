9f41d371bded31fdfcd747f08f5ca3b205dc8ce7
Linus Norton <linusnorton@gmail.com>
2020-12-02 20:07:12 +0000

    update sonar rules

--- src/main/app/controller/GetController.ts
+++ src/main/app/controller/GetController.ts
@@ -7,7 +7,7 @@ export class GetController {
 
   constructor(
     protected readonly name: string,
-    protected readonly content: Record<string, any>,
+    protected readonly content: Record<string, any>
   ) {}
 
   public async get(req: AppRequest, res: Response): Promise<void> {
