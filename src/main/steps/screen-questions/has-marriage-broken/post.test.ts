2ec3fca50a30ffd19ed6ddef05bc6c262ad6f610
Linus Norton <linusnorton@gmail.com>
2021-01-08 06:57:49 +0000

    remove more generic form references

--- src/main/steps/screen-questions/has-marriage-broken/post.test.ts
+++ src/main/steps/screen-questions/has-marriage-broken/post.test.ts
@@ -2,11 +2,10 @@ import { mockRequest } from '../../../../test/unit/utils/mockRequest';
 import { mockResponse } from '../../../../test/unit/utils/mockResponse';
 import { HasMarriageBrokenPostController } from './post';
 import { Form } from '../../../app/form/Form';
-import { HasMarriageBrokenForm } from './content';
 import { RESPONDENT_ADDRESS_URL } from '../../urls';
 
 describe('HasMarriageBrokenPostController', () => {
-  const mockForm: Form<HasMarriageBrokenForm> = { getErrors: () => [] as never[] } as any;
+  const mockForm: Form = { getErrors: () => [] as never[] } as any;
   const controller = new HasMarriageBrokenPostController(mockForm);
 
   test('Should redirect', async () => {
--- src/main/steps/screen-questions/respondent-address/post.test.ts
+++ src/main/steps/screen-questions/respondent-address/post.test.ts
@@ -2,11 +2,10 @@ import { mockRequest } from '../../../../test/unit/utils/mockRequest';
 import { mockResponse } from '../../../../test/unit/utils/mockResponse';
 import { RespondentAddressPostController } from './post';
 import { Form } from '../../../app/form/Form';
-import { RespondentAddressForm } from './content';
 import { MARRIAGE_CERTIFICATE_URL } from '../../urls';
 
 describe('HasMarriageBrokenPostController', () => {
-  const mockForm: Form<RespondentAddressForm> = { getErrors: () => [] as never[] } as any;
+  const mockForm: Form = { getErrors: () => [] as never[] } as any;
   const controller = new RespondentAddressPostController(mockForm);
 
   test('Should redirect', async () => {
