ce91e317a72afb28456a17969aa84b57a6510479
velimd <duzgunce.vd@gmail.com>
2021-01-14 11:40:42 +0000

    setup

--- src/main/routes.ts
+++ src/main/routes.ts
@@ -4,8 +4,10 @@ import {
   FIRST_PAGE_URL,
   HAS_MARRIAGE_BROKEN_URL,
   HOME_URL,
-  LANGUAGE_PREFERENCE_URL, MARRIAGE_CERTIFICATE_URL,
-  RESPONDENT_ADDRESS_URL
+  LANGUAGE_PREFERENCE_URL,
+  MARRIAGE_CERTIFICATE_URL,
+  RESPONDENT_ADDRESS_URL,
+  TERMS_AND_CONDITIONS_URL
 } from './steps/urls';
 
 export class Routes {
@@ -13,6 +15,7 @@ export class Routes {
 
     app.get(HOME_URL, app.locals.container.cradle.homeGetController.get);
     app.get(FIRST_PAGE_URL, app.locals.container.cradle.firstPageGetController.get);
+    app.get(TERMS_AND_CONDITIONS_URL, app.locals.container.cradle.termsAndConditionsGetController.get);
     app.post(FIRST_PAGE_URL, app.locals.container.cradle.firstPagePostController.post);
     app.get(LANGUAGE_PREFERENCE_URL, app.locals.container.cradle.languagePreferenceGetController.get);
     app.post(LANGUAGE_PREFERENCE_URL, app.locals.container.cradle.languagePreferencePostController.post);
