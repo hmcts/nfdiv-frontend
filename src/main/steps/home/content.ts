170b4de2d47211eddb2534aea94ef2f59992192a
Seb Flippence <sebflipper@users.noreply.github.com>
2021-01-18 15:17:33 +0000

    chore: localise holding page via existing i18n file

--- src/main/steps/home/content.ts
+++ src/main/steps/home/content.ts
@@ -1,23 +1,7 @@
 import { LANGUAGE_PREFERENCE_URL } from '../urls';
 
-const en = {
-  mainText: {
-    divorce: 'Let\'s get our divorce on!',
-    civil: 'Time to end that civil partnership!'
-  },
-  linkText: 'Click to go next'
-};
-
-const cy: typeof en = {
-  mainText: {
-    divorce: 'Let\'s get our divorce on!',
-    civil: 'Time to end that civil partnership!'
-  },
-  linkText: 'Welsh link text'
-};
-
 const common = {
   link: LANGUAGE_PREFERENCE_URL
 };
 
-export const homeContent = { en, cy, common };
+export const homeContent = { common };
