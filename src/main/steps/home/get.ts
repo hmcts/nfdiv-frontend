c66dbb7ddceed955615ce7031503f8ebde751ac0
velimd <duzgunce.vd@gmail.com>
2021-01-18 12:24:35 +0000

    tackling nfdiv-40 and redirecting to the screener pages.

--- src/main/steps/home/get.ts
+++ src/main/steps/home/get.ts
@@ -1,10 +1,17 @@
 import { GetController } from '../../app/controller/GetController';
 import { homeContent } from './content';
+import { AppRequest } from '../../app/controller/AppRequest';
+import { Response } from 'express';
+import { LANGUAGE_PREFERENCE_URL } from '../urls';
 
 export class HomeGetController extends GetController {
 
   constructor() {
     super(__dirname + '/template', homeContent);
+  }
+
+  public async get(req: AppRequest, res: Response): Promise<void> {
+    res.redirect(LANGUAGE_PREFERENCE_URL);
   }
 
 }
