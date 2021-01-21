6986a5e34f7e6692709626087235935305fac27c
Linus Norton <linusnorton@gmail.com>
2021-01-21 13:11:55 +0000

    update content

--- src/main/steps/home/content.ts
+++ src/main/steps/home/content.ts
@@ -1,10 +1,18 @@
 import { LANGUAGE_PREFERENCE_URL } from '../urls';
 
 const en = {
+  mainText: {
+    divorce: 'Let\'s get our divorce on!',
+    civil: 'Time to end that civil partnership!'
+  },
   linkText: 'Click to go next'
 };
 
 const cy: typeof en = {
+  mainText: {
+    divorce: 'Let\'s get our divorce on!',
+    civil: 'Time to end that civil partnership!'
+  },
   linkText: 'Welsh link text'
 };
 
