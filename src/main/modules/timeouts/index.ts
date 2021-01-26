9e20f8e3a27d99bdf5165c5bdfbafe1b67761191
Seb Flippence <sebflipper@users.noreply.github.com>
2021-01-26 11:52:06 +0000

    chore: moved timout value to config

--- src/main/modules/timeouts/index.ts
+++ src/main/modules/timeouts/index.ts
@@ -1,23 +1,24 @@
 import { Application } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import config from 'config';
 
 import { HTTPError } from '../../steps/error/error.controller';
 
-const MAX_TIME_LIMIT = 30 * 1000;
-
 export class LoadTimeouts {
   public enableFor(app: Application): void {
+    const timeoutMs = config.get<number>('timeout');
+
     app.use((req, res, next) => {
       const { errorController } = app.locals.container.cradle;
 
       // Set the timeout for all HTTP requests
-      req.setTimeout(MAX_TIME_LIMIT, () => {
+      req.setTimeout(timeoutMs, () => {
         const err = new HTTPError('Request Timeout', StatusCodes.REQUEST_TIMEOUT);
         errorController.internalServerError(err, req, res);
       });
 
       // Set the server response timeout for all HTTP requests
-      res.setTimeout(MAX_TIME_LIMIT, () => {
+      res.setTimeout(timeoutMs, () => {
         const err = new HTTPError('Service Unavailable', StatusCodes.SERVICE_UNAVAILABLE);
         errorController.internalServerError(err, req, res);
       });
