892efb01994e1e405fb648e9569163e015bc76d1
Seb Flippence <sebflipper@users.noreply.github.com>
2021-01-26 10:41:31 +0000

    feat(NFDIV-379): added error options from existing app

--- src/main/steps/error/content.ts
+++ src/main/steps/error/content.ts
@@ -1,33 +1,50 @@
 import { StatusCodes } from 'http-status-codes';
 
+const common = {
+  en: {
+    info: 'This could be because you’ve followed a broken or outdated link, or because there’s an error on our site.',
+    youCan: 'You can either:',
+    goBack: 'go back to the previous page',
+    signInAgain: 'sign in again',
+  },
+  cy: {
+    info: 'Efallai eich bod wedi defnyddio dolen nad yw’n gweithio neu hen ddolen, neu bod yna nam ar ein safle.',
+    youCan: 'Gallwch:',
+    goBack: 'fynd yn ôl i’r dudalen flaenorol',
+    signInAgain: 'mewngofnodi eto',
+  },
+};
+
 const en = {
   [StatusCodes.BAD_REQUEST]: {
+    ...common.en,
     title: 'Bad request',
-    info: 'This could be because you’ve followed a broken or outdated link, or because there’s an error on our site.',
   },
   [StatusCodes.NOT_FOUND]: {
+    ...common.en,
     title: 'Page not found',
-    info: 'This could be because you’ve followed a broken or outdated link, or because there’s an error on our site.',
   },
   [StatusCodes.INTERNAL_SERVER_ERROR]: {
+    ...common.en,
     title: 'Sorry, we’re having technical problems',
-    info: 'Please try again in a few minutes'
-  }
+    info: 'Please try again in a few minutes',
+  },
 };
 
 const cy: typeof en = {
   [StatusCodes.BAD_REQUEST]: {
+    ...common.cy,
     title: 'Cais gwael',
-    info: 'Efallai eich bod wedi defnyddio dolen nad yw’n gweithio neu hen ddolen, neu bod yna nam ar ein safle.',
   },
   [StatusCodes.NOT_FOUND]: {
+    ...common.cy,
     title: 'Ni ellir dod o hyd i’r dudalen',
-    info: 'Efallai eich bod wedi defnyddio dolen nad yw’n gweithio neu hen ddolen, neu bod yna nam ar ein safle.',
   },
   [StatusCodes.INTERNAL_SERVER_ERROR]: {
+    ...common.cy,
     title: 'Yn anffodus, rydym yn cael problemau technegol',
-    info: 'Rhowch gynnig arall arni ymhen ychydig funudau'
-  }
+    info: 'Rhowch gynnig arall arni ymhen ychydig funudau',
+  },
 };
 
 export const errorContent = { en, cy };
