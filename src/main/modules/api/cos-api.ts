e17dd944fcfb4d1e1134992f5e81c60f3f894275
velimd <duzgunce.vd@gmail.com>
2021-01-28 11:33:33 +0000

    api clean up (2)

--- src/main/modules/api/cos-api.ts
+++ src/main/modules/api/cos-api.ts
@@ -7,25 +7,32 @@ export class CosApi {
     private readonly logger: LoggerInstance
   ) { }
 
+  //TODO change return type when backend has been implemented.
   public getCase(): Promise<Record<string, unknown>> {
     return this.axios
       .get('/case')
-      .then(results => {
-        return results.data;
-      })
+      .then(results => results.data)
       .catch(err => {
         this.logger.error(err);
         return false;
       });
   }
 
-  public createCase(data: Record<string, string>): Promise<boolean> {
+  public createCase(data: Record<string, string>): Promise<CaseCreationResponse> {
     return this.axios
       .post('/case', data)
-      .then(results => !!results)
+      .then(results => results.data)
       .catch(err => {
         this.logger.error(err);
         throw new Error('Case could not be created. Please try again.');
       });
   }
+}
+
+
+export interface CaseCreationResponse {
+  caseId: string,
+  error: string,
+  status: string,
+  allocatedCourt: Record<string, string>
 }
