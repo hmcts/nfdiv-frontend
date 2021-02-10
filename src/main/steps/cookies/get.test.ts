0a026f8c928e16e43fff05606c7254927fa130c9
velimd <duzgunce.vd@gmail.com>
2021-02-10 16:44:02 +0000

    include test

--- src/main/steps/cookies/get.test.ts
+++ src/main/steps/cookies/get.test.ts
@@ -1,23 +1,35 @@
 import { mockRequest } from '../../../test/unit/utils/mockRequest';
 import { mockResponse } from '../../../test/unit/utils/mockResponse';
 import { commonContent } from '../common/common.content';
+
+import { generateContent } from './content';
 import { CookiesGetController } from './get';
-import { CookiesContent } from './content';
 
 describe('RespondentAddressGetController', () => {
   const controller = new CookiesGetController();
 
-  test('Should render the page', async () => {
+  test('Should render the cookie page with divorce content', async () => {
     const req = mockRequest();
     const res = mockResponse();
     await controller.get(req, res);
 
     expect(res.render).toBeCalledWith(expect.anything(), {
-      ...CookiesContent.en,
-      ...CookiesContent.common,
+      ...generateContent(true).en,
       ...commonContent.en,
-      sessionErrors: []
+      sessionErrors: [],
     });
   });
 
+  test('Should render the cookie page with civil content', async () => {
+    const req = mockRequest();
+    const res = mockResponse();
+    res.locals.serviceType = 'civil';
+    await controller.get(req, res);
+
+    expect(res.render).toBeCalledWith(expect.anything(), {
+      ...generateContent(false).en,
+      ...commonContent.en,
+      sessionErrors: [],
+    });
+  });
 });
