e0f047cf57c15074f213cacb8479dc9abe37563a
Linus Norton <linusnorton@gmail.com>
2021-02-18 07:03:41 +0000

    update test account

--- src/test/config.ts
+++ src/test/config.ts
@@ -2,7 +2,7 @@ export const config = {
   TEST_URL: process.env.TEST_URL || 'http://localhost:3001',
   TestHeadlessBrowser: process.env.TEST_HEADLESS ? process.env.TEST_HEADLESS === 'true' : true,
   TestSlowMo: 250,
-  TestUser: 'hmcts.nfdiv@gmail.com',
+  TestUser: 'nfdiv.frontend.test@hmcts.net',
   TestPass: 'Pa55word11',
   Gherkin: {
     features: './features/*.feature',
