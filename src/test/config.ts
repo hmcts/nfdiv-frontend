fa02af9adf27e3a723cca8f6dc2328b7e2ed32f5
Linus Norton <linusnorton@gmail.com>
2021-02-19 10:31:53 +0000

    each worker thread gets it's own user

--- src/test/config.ts
+++ src/test/config.ts
@@ -1,8 +1,27 @@
+import { closeSync, openSync, readFileSync, writeFileSync } from 'fs';
+
+import * as lockFile from 'lockfile';
+import { fileExistsSync } from 'tsconfig-paths/lib/filesystem';
+
+const lock = '/tmp/concepts.worker.lock';
+lockFile.lockSync(lock);
+
+const filename = '/tmp/concepts.worker.id';
+if (!fileExistsSync(filename)) {
+  closeSync(openSync(filename, 'w'));
+}
+
+const content = readFileSync(filename).toString();
+const updatedContent = (content === '' || +content > 8 ? 0 : +content) + 1;
+
+writeFileSync(filename, updatedContent + '');
+lockFile.unlockSync(lock);
+
 export const config = {
   TEST_URL: process.env.TEST_URL || 'http://localhost:3001',
   TestHeadlessBrowser: process.env.TEST_HEADLESS ? process.env.TEST_HEADLESS === 'true' : true,
   TestSlowMo: 250,
-  TestUser: 'nfdiv.frontend.test@hmcts.net',
+  TestUser: `nfdiv.frontend.test${updatedContent}@hmcts.net`,
   TestPass: 'Pa55word11',
   Gherkin: {
     features: './features/*.feature',
