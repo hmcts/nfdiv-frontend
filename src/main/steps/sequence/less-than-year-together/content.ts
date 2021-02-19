d956099f8fa8f10727c844a2cdfadbe5a649084b
Seb Flippence <144435+SebFlippence@users.noreply.github.com>
2021-02-19 17:31:13 +0000

    chore: tidy up logic

--- src/main/steps/sequence/less-than-year-together/content.ts
+++ src/main/steps/sequence/less-than-year-together/content.ts
@@ -3,7 +3,6 @@ import { TranslationFn } from '../../../app/controller/GetController';
 export const generateContent: TranslationFn = ({ isDivorce }) => {
   //TODO change when ticket is picked up and implememnted
   const en = {
-    isExitPage: true,
     title: isDivorce ? 'You have not been married for long enough' : 'You have not been together for long enough',
     line1: 'you need to be married for longer',
   };
