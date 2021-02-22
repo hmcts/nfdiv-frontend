d245c391857fa053f53f7985745b323cfbdb99b1
Seb Flippence <144435+SebFlippence@users.noreply.github.com>
2021-02-22 10:14:16 +0000

    chore: move a11y changes to another PR

--- jest.a11y.config.js
+++ jest.a11y.config.js
@@ -3,7 +3,7 @@ module.exports = {
   testRegex: '(/src/test/.*|\\.(test|spec))\\.(ts|js)$',
   moduleFileExtensions: ['ts', 'js'],
   testEnvironment: 'node',
-  testTimeout: 10000,
+  testTimeout: 15000,
   transform: {
     '^.+\\.ts$': 'ts-jest',
   },
