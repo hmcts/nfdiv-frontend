dfc2866adc2083e6659f0b49733de8e688f932e0
Linus Norton <linusnorton@gmail.com>
2021-02-24 12:08:33 +0000

    Add service auth provider client (#240)
    * Add service auth provider client
    
    * Gotta get that coverage
    
    * Remove auth header
    
    * Refactor oidc (#242)
    
    * tidy up oidc code
    
    * dont load config on require
    
    * fix test
    
    * fixes
    
    * Added tests for oidc
    
    * chore: remove unnecessary promise
    
    * Revert "chore: remove unnecessary promise"
    
    This reverts commit 7ac10dac675e8d6df7dea5836805ba7f25f35dc0.
    
    Co-authored-by: Seb Flippence <144435+SebFlippence@users.noreply.github.com>

--- src/main/modules/oidc/index.ts
+++ src/main/modules/oidc/index.ts
@@ -1,10 +1,7 @@
-import Axios from 'axios';
 import config from 'config';
-import { Application, NextFunction, Request, Response } from 'express';
-import jwt_decode from 'jwt-decode';
+import { Application, NextFunction, Response } from 'express';
 
-import { CaseApi } from '../../app/case/CaseApi';
-import { Case, CaseType } from '../../app/case/case';
+import { CALLBACK_URL, getCaseApi, getRedirectUrl, getUserDetails } from '../../app/auth/user/oidc';
 import { AppRequest } from '../../app/controller/AppRequest';
 import { SAVE_SIGN_OUT_URL, SIGN_IN_URL, SIGN_OUT_URL } from '../../steps/urls';
 
@@ -13,94 +10,34 @@ import { SAVE_SIGN_OUT_URL, SIGN_IN_URL, SIGN_OUT_URL } from '../../steps/urls';
  */
 export class OidcMiddleware {
   public enableFor(app: Application): void {
-    const loginUrl: string = config.get('services.idam.authorizationURL');
-    const tokenUrl: string = config.get('services.idam.tokenURL');
-    const clientId: string = config.get('services.idam.clientID');
-    const clientSecret: string = config.get('services.idam.clientSecret');
     const protocol = app.locals.developmentMode ? 'http://' : 'https://';
     const port = app.locals.developmentMode ? `:${config.get('port')}` : '';
     const { errorHandler } = app.locals;
 
+    app.get(SIGN_IN_URL, (req, res) => res.redirect(getRedirectUrl(`${protocol}${res.locals.host}${port}`)));
+    app.get(SIGN_OUT_URL, (req, res) => req.session.destroy(() => res.redirect('/')));
     app.get(
-      SIGN_IN_URL,
-      errorHandler((req: AppRequest, res: Response) => {
-        const redirectUri = encodeURI(`${protocol}${res.locals.host}${port}/oauth2/callback`);
-        res.redirect(`${loginUrl}?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}`);
-      })
-    );
-
-    app.get(
-      '/oauth2/callback',
-      errorHandler(async (req: AppRequest, res: Response) => {
-        const redirectUri = encodeURI(`${protocol}${res.locals.host}${port}/oauth2/callback`);
-        const response = await Axios.post(
-          tokenUrl,
-          `client_id=${clientId}&client_secret=${clientSecret}&grant_type=authorization_code&redirect_uri=${redirectUri}&code=${encodeURIComponent(
-            req.query.code as string
-          )}`,
-          {
-            headers: {
-              Accept: 'application/json',
-              'Content-Type': 'application/x-www-form-urlencoded',
-            },
-          }
-        );
-
-        req.session.user = {
-          ...response.data,
-          jwt: jwt_decode(response.data.id_token),
-        };
+      CALLBACK_URL,
+      errorHandler(async (req, res) => {
+        req.session.user = await getUserDetails(`${protocol}${res.locals.host}${port}`, req.query.code as string);
         req.session.save(() => res.redirect('/'));
       })
     );
-
-    app.get(SIGN_OUT_URL, (req: Request, res: Response) => {
-      req.session.destroy(() => res.redirect('/'));
-    });
 
     app.use(
       errorHandler(async (req: AppRequest, res: Response, next: NextFunction) => {
         if (req.session.user) {
           res.locals.isLoggedIn = true;
-          req.locals.api = new CaseApi(
-            Axios.create({
-              baseURL: config.get('services.cos.baseURL'),
-              headers: {
-                Authorization: 'Bearer ' + req.session.user.access_token,
-                IdToken: req.session.user.id_token,
-              },
-            }),
-            req.locals.logger
-          );
-
-          if (!req.session.userCase) {
-            const userCase = await req.locals.api.getCase();
-
-            if (!userCase) {
-              req.session.userCase = await req.locals.api.createCase({
-                divorceOrDissolution:
-                  res.locals.serviceType === CaseType.Dissolution ? CaseType.Dissolution : CaseType.Divorce,
-              });
-            } else {
-              req.session.userCase = userCase;
-            }
-          }
+          req.locals.api = getCaseApi(req.session.user.accessToken, req.locals.logger);
+          req.session.userCase = req.session.userCase || (await req.locals.api.getOrCreateCase(res.locals.serviceType));
 
           return next();
-        }
-        if (req.originalUrl !== SAVE_SIGN_OUT_URL) {
+        } else if (req.originalUrl !== SAVE_SIGN_OUT_URL) {
           res.redirect(SIGN_IN_URL);
         } else {
           return next();
         }
       })
     );
-  }
-}
-
-declare module 'express-session' {
-  export interface SessionData {
-    user: Record<string, Record<string, unknown>>;
-    userCase: Case;
   }
 }
