7dfdfc007a0718e4210d4787b110feadd651ade3
velimd <duzgunce.vd@gmail.com>
2021-03-01 15:27:05 +0000

    clean up to make more sense in test

--- src/main/app/controller/PostController.test.ts
+++ src/main/app/controller/PostController.test.ts
@@ -28,7 +28,7 @@ describe('PostController', () => {
     expect(req.session.userCase).toEqual({ divorceOrDissolution: 'divorce', gender: 'female', id: '1234' });
     expect(req.locals.api.updateCase).not.toHaveBeenCalled();
 
-    expect(getNextStepUrlMock).toBeCalledWith(req, body);
+    expect(getNextStepUrlMock).toBeCalledWith(req, mockForm.getParsedBody(body));
     expect(res.redirect).toBeCalledWith(req.path);
     expect(req.session.errors).toBe(errors);
   });
@@ -50,7 +50,7 @@ describe('PostController', () => {
     expect(req.session.userCase).toEqual({ divorceOrDissolution: 'divorce', gender: 'female', id: '1234' });
     expect(req.locals.api.updateCase).toHaveBeenCalledWith('1234', { gender: 'female' });
 
-    expect(getNextStepUrlMock).toBeCalledWith(req, body);
+    expect(getNextStepUrlMock).toBeCalledWith(req, mockForm.getParsedBody(body));
     expect(res.redirect).toBeCalledWith('/next-step-url');
     expect(req.session.errors).toBe(undefined);
   });
@@ -91,7 +91,7 @@ describe('PostController', () => {
     await expect(controller.post(req, res)).rejects.toEqual('An error while saving session');
 
     expect(mockSave).toHaveBeenCalled();
-    expect(getNextStepUrlMock).toBeCalledWith(req, body);
+    expect(getNextStepUrlMock).toBeCalledWith(req, mockForm.getParsedBody(body));
     expect(res.redirect).not.toHaveBeenCalled();
     expect(req.session.errors).toBe(undefined);
   });
