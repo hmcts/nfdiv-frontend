49204f0fc9ec2cae8571c151cb74f371a10085b5
velimd <duzgunce.vd@gmail.com>
2021-03-02 16:28:13 +0000

    fix unit test

--- src/main/steps/timed-out/get.test.ts
+++ src/main/steps/timed-out/get.test.ts
@@ -1,17 +1,17 @@
 import { mockRequest } from '../../../test/unit/utils/mockRequest';
 import { mockResponse } from '../../../test/unit/utils/mockResponse';
 import { commonContent } from '../common/common.content';
-import { SIGN_IN_URL, TIMED_OUT_URL } from '../urls';
+import { TIMED_OUT_URL } from '../urls';
 
 import { timedOutContent } from './content';
 import { TimedOutGetController } from './get';
 
-describe('TermsAndConditionsGetController', () => {
+describe('TimedOutGetController', () => {
   const controller = new TimedOutGetController();
 
   test('Should destroy session and render timeout page', async () => {
     const req = mockRequest();
-    req.path = TIMED_OUT_URL;
+    req.originalUrl = TIMED_OUT_URL;
     const res = mockResponse();
     await controller.get(req, res);
 
@@ -32,9 +32,17 @@ describe('TermsAndConditionsGetController', () => {
         user: undefined,
       },
     });
+    req.originalUrl = TIMED_OUT_URL;
     const res = mockResponse();
     await controller.get(req, res);
 
-    expect(res.redirect).toBeCalledWith(SIGN_IN_URL);
+    expect(res.render).toBeCalledWith(expect.anything(), {
+      ...commonContent.en,
+      ...timedOutContent.en,
+      sessionErrors: [],
+      formState: req.session.userCase,
+      hideNavigationButton: true,
+      hideBackButton: true,
+    });
   });
 });
