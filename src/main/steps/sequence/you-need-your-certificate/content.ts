25d28d0916dc1057fb141ab08cdf67ea3d8e01ad
Seb Flippence <144435+SebFlippence@users.noreply.github.com>
2021-03-03 15:21:20 +0000

    feat(NFDIV-373): Welsh You need your certificate

--- src/main/steps/sequence/you-need-your-certificate/content.ts
+++ src/main/steps/sequence/you-need-your-certificate/content.ts
@@ -2,8 +2,8 @@ import { TranslationFn } from '../../../app/controller/GetController';
 
 export const generateContent: TranslationFn = ({ isDivorce, partner }) => {
   const en = {
-    title: isDivorce ? 'You need your marriage certificate' : 'You need your civil partnership certificate',
-    line1: `You need your ${isDivorce ? 'marriage certificate' : 'civil partnership certificate'} to use this service.`,
+    title: `You need your ${isDivorce ? 'marriage' : 'civil partnership'} certificate`,
+    line1: `You need your ${isDivorce ? 'marriage' : 'civil partnership'} certificate to use this service.`,
     line2: `You should ask your ${
       isDivorce ? partner : 'civil partner'
     } for it, if they have it. Or you can <a href="https://www.gov.uk/order-copy-birth-death-marriage-certificate" class="govuk-link" target="_blank">order a certified copy online (opens in a new tab)</a>, if you ${
@@ -14,8 +14,20 @@ export const generateContent: TranslationFn = ({ isDivorce, partner }) => {
     } is not in English, you need to get it translated and certified. You can find translation services online which include certification as part of their service.`,
   };
 
-  //TODO translation
-  const cy: typeof en = { ...en };
+  const cy: typeof en = {
+    title: `Mae arnoch angen eich ${isDivorce ? 'tystysgrif priodas' : 'tystysgrif partneriaeth sifil'}`,
+    line1: `Mae arnoch angen eich ${
+      isDivorce ? 'tystysgrif priodas' : 'tystysgrif partneriaeth sifil'
+    } i ddefnyddio'r gwasanaeth hwn. `,
+    line2: `Dylech ofyn i'ch ${
+      isDivorce ? partner : 'partner sifil'
+    } amdani, os yw'r dystysgrif ganddyn nhw. Fel arall, gallwch <a href="https://www.gov.uk/order-copy-birth-death-marriage-certificate" class="govuk-link" target="_blank">archebu copi ardystiedig ar-lein (agor mewn ffenest newydd)</a>, os gwnaethoch chi ${
+      isDivorce ? 'briodi' : 'ffurfio eich partneriaeth sifil'
+    } yng Nghymru neu Loegr.`,
+    line3: `Os nad yw eich ${
+      isDivorce ? 'tystysgrif priodas' : 'tystysgrif partneriaeth sifil'
+    } wreiddiol yn Saesneg, mae arnoch angen trefnu iddi gael ei chyfieithu a'i hardystio. Mae cwmnïau a fydd yn gallu gwneud y gwaith cyfieithu a'r ardystio.`,
+  };
 
   const common = {};
 
