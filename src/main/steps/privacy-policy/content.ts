b6ea910381fc9eb7a7be3be37fa7a1668b3f59fe
velimd <duzgunce.vd@gmail.com>
2021-03-03 15:09:05 +0000

    welsh translation

--- src/main/steps/privacy-policy/content.ts
+++ src/main/steps/privacy-policy/content.ts
@@ -135,7 +135,133 @@ export const generateContent = ({ isDivorce }: { isDivorce: boolean }): Translat
   };
 
   const cy: typeof en = {
-    ...en,
+    title: 'Polisi preifatrwydd',
+    info:
+      'Mae’r polisi preifatrwydd hwn yn egluro pam rydym yn casglu eich data personol a beth rydym yn ei wneud ag ef. Mae hefyd yn egluro eich hawliau a sut i’w gorfodi.',
+    whoManages: "Pwy sy’n rheoli'r gwasanaeth hwn",
+    managedBy:
+      'Gwasanaeth Llysoedd a Thribiwnlysoedd EM (GLlTEM) sy’n rheoli’r gwasanaeth hwn, sef un o asiantaethau gweithredol y Weinyddiaeth Gyfiawnder (MoJ).',
+    moj:
+      "MoJ yw’r rheolydd data at ddibenion diogelu data. <a class='govuk-link' href='https://www.gov.uk/government/organisations/ministry-of-justice/about/personal-information-charter' target='_blank'>Mae siarter gwybodaeth bersonol yr MoJ</a> yn egluro sut mae'r MoJ yn prosesu data personol.",
+    responsibleForDeciding:
+      "Fel rhan o’r MoJ, mae GLlTEM yn gyfrifol am benderfynu sut mae eich data personol yn cael ei ddefnyddio ac am ddiogelu'r data personol rydych yn ei ddarparu.",
+    moreInformation:
+      "Mae rhagor o wybodaeth am ddefnyddio’r gwasanaeth hwn yn y <a class='govuk-link' href='/terms-and-conditions'>telerau ac amodau</a>.",
+    thePersonal: 'Y data personol sydd ei angen arnom',
+    whenYouUseDivorceService: 'Rydym angen y data personol canlynol ar gyfer y gwasanaeth hwn:',
+    applicantName: "enw'r ceisydd",
+    respondentName: `enw'r unigolyn y mae'r ceisydd yn ${isDivorce ? 'ysgaru' : 'diddymu eu partneriaeth sifil â nhw'}`,
+    changedApplicantName: `tystiolaeth o newid enw'r ceisydd os yw wedi'i newid ers iddynt ${
+      isDivorce ? 'briodi' : 'ffurfio eu partneriaeth sifil'
+    }`,
+    changedRespondentName: `tystiolaeth o newid enw'r unigolyn y mae'r ceisydd eisiau ${
+      isDivorce ? 'ysgaru' : 'diddymu eu partneriaeth sifil â nhw'
+    } (os gwnaethant ei newid)`,
+    marriageCert: `copi o'r ${
+      isDivorce ? 'dystysgrif priodas' : 'dystysgrif partneriaeth sifil'
+    } a'r manylion sydd arni`,
+    countryApplicantMarried: 'y wlad y mae’r ceisydd yn preswylio’n arferol ynddi',
+    countryRespondentMarried: `y wlad y mae'r unigolyn y mae'r ceisydd eisiau ${
+      isDivorce ? 'ei ysgaru' : 'diddymu eu partneriaeth sifil â nhw'
+    } yn preswylio'n arferol ynddi`,
+    applicantHabitualResidence: "p'un a yw'r ceisydd yn preswylio'n arferol yng Nghymru neu Loegr",
+    respondentHabitualResidence: `p'un a yw'r unigolyn y mae'r ceisydd eisiau ${
+      isDivorce ? 'ei ysgaru' : 'diddymu eu partneriaeth sifil â nhw'
+    } yn preswylio'n arferol yng Nghymru neu Loegr`,
+    applicantDomicile: "p'un a yw domisil y ceisydd yng Nghymru neu Loegr",
+    respondentDomicile: `p'un a yw domisil yr unigolyn y mae'r ceisydd eisiau ${
+      isDivorce ? 'ei ysgaru' : 'diddymu eu partneriaeth sifil â nhw'
+    } yng Nghymru neu Loegr`,
+    applicantEmailAndPhone: 'cyfeiriadau, cyfeiriadau e-bost a rhifau ffôn y ceisydd',
+    respondentEmailAndPhone: `cyfeiriad, cyfeiriad e-bost a rhif ffôn yr unigolyn y mae'r ceisydd eisiau ${
+      isDivorce ? 'ei ysgaru' : 'diddymu eu partneriaeth sifil â nhw'
+    }`,
+    applicantAddressAndNames: 'cyfeiriad, cyfeiriad e-bost a rhif ffôn cyfreithiwr y ceisydd (os oes ganddynt un)',
+    respondentAddressAndNames: `cyfeiriad, cyfeiriad e-bost a rhif ffôn cyfreithiwr yr unigolyn y mae'r ceisydd eisiau ${
+      isDivorce ? 'ei ysgaru' : 'diddymu eu partneriaeth sifil â nhw'
+    } (os oes ganddynt un)`,
+    applicantOtherCourtCases: "manylion achosion llys eraill y mae'r ceisydd wedi bod yn ymwneud â nhw",
+    respondentOtherCourtCases: `manylion unrhyw achosion eraill y mae'r unigolyn y mae'r ceisydd eisiau ${
+      isDivorce ? 'ei ysgaru' : 'diddymu eu partneriaeth sifil â nhw'
+    } wedi bod yn ymwneud â nhw (os oes rhai)`,
+    receivingNotifications: 'Hysbysiadau',
+    youNeedSignUp: `Mae angen ichi gofrestru i gael hysbysiadau i ddefnyddio’r gwasanaeth ${
+      isDivorce ? 'ysgaru' : 'diddymu partneriaeth sifil'
+    }. Mae hwn yn ofyniad cyfreithiol er mwyn i’r cais fynd yn ei flaen.`,
+    storingYourData: 'Storio eich data',
+    askedForEmailAddress:
+      "Pan fyddwch yn defnyddio'r gwasanaeth hwn fe ofynnir ichi ddefnyddio’ch cyfeiriad e-bost i greu cyfrif. Byddwch yn gallu defnyddio’r cyfeiriad e-bost hwn a chyfrinair i fewngofnodi i wasanaethau eraill GLlTEM.",
+    holdYourData: `Tra byddwch yn llenwi neu'n ymateb i gais ${
+      isDivorce ? 'am ysgariad' : 'i ddiddymu partneriaeth sifil'
+    } byddwn yn cadw eich data am hyd at 6 mis. Os na fyddwch yn cwblhau’r cais yn ystod yr amser hwn, bydd rhaid ichi ddechrau eto.`,
+    whenADivorceIsFinalised: `Pan gwblheir ${
+      isDivorce ? 'eich ysgariad' : 'diddymiad eich partneriaeth sifil'
+    } cedwir manylion yr achos am 18 mlynedd. Ar ôl y cyfnod hwnnw, dilëir peth data (o’r dyfarniad amodol a'r dyfarniad terfynol).`,
+    theRemainder:
+      'Cedwir gweddill gwybodaeth yr achos am 82 blynedd arall. Ar ôl cyfanswm o 100 mlynedd dilëir y data hwn.',
+
+    whyWeCollect: 'Pam rydym ni’n casglu eich data personol',
+    personalData: 'Rydym ni’n casglu eich data personol i:',
+    processApplication: 'brosesu eich cais',
+    legalRequirements: 'bodloni gofynion cyfreithiol',
+    improveService: 'gwella’r gwasanaeth hwn',
+    staffUse:
+      'Mae ein staff yn defnyddio eich data personol i brosesu eich cais. Maent yn gweithio yn y DU ac mae eich data yn cael ei storio yn y DU.',
+    usingYourData: 'Defnyddio eich data',
+    asPart:
+      "Fel rhan o'ch cais fe ofynnir ichi ddefnyddio’ch cyfeiriad e-bost i greu cyfrif. Byddwch yn gallu defnyddio’r cyfeiriad e-bost hwn a chyfrinair i fewngofnodi i wasanaethau eraill GLlTEM.",
+    weMayAskForPermission:
+      'Byddwn yn gofyn am eich caniatâd i ddefnyddio’ch cyfeiriad e-bost i anfon negeseuon e-bost atoch trwy’r system GOV.UK Notify. O fewn Ardal Economaidd Ewrop yn unig y mae’r system yn prosesu negeseuon e-bost hyd nes y pwynt y mae negeseuon e-bost yn cael eu trosglwyddo i’r darparwr e-bost rydych chi’n ei ddefnyddio.',
+    weUseCookies:
+      "Rydym yn defnyddio <a class='govuk-link' href='cookie'>cwcis</a> i gasglu data am sut rydych yn defnyddio'r gwasanaeth hwn, yn cynnwys:",
+    ifYouOpenEmail: 'os byddwch yn agor neges e-bost gennym neu’n clicio ar ddolen mewn e-bost',
+    yourComputer: 'cyfeiriad IP eich cyfrifiadur, ffôn symudol neu dabled',
+    theRegion: 'yr ardal neu’r dref lle rydych yn defnyddio’ch cyfrifiadur, ffôn symudol neu dabled',
+    theWeb: 'y porwr gwe rydych yn ei ddefnyddio',
+    sharingYourData: 'Rhannu eich data',
+    ifAnother:
+      'Pan fydd eich hawliad neu eich cais yn cael ei brosesu, mae’n bosib y byddwn angen cysylltu ag adran, asiantaeth neu sefydliad arall yn y llywodraeth ac efallai y byddwn yn rhannu eich data gyda nhw.',
+    dataSharedWithPrintingServiceProvider: `Bydd unrhyw ddata a ddarperir gennych sydd angen ei argraffu yn cael ei rannu gyda Xerox (UK) Ltd. Er enghraifft, bydd y cais ${
+      isDivorce ? 'am ysgariad' : 'i ddiddymu partneriaeth sifil'
+    } yn cael ei argraffu fel y gellir ei anfon at yr atebydd drwy’r post.`,
+    ifYouContactUs:
+      "Os byddwch yn cysylltu â ni ac yn gofyn am help gyda'r gwasanaeth rydych yn ei ddefnyddio, efallai y byddwn yn rhannu eich data personol gyda’r Good Things Foundation. Rydym yn gweithio mewn partneriaeth â'r cwmni hwn i gynnig cefnogaeth wyneb yn wyneb.",
+    crime:
+      'Mewn rhai amgylchiadau efallai y byddwn yn rhannu eich data, er enghraifft er mwyn atal neu i ddatrys trosedd, neu i gynhyrchu ystadegau cyffredinol.',
+    weUse:
+      "Rydym yn defnyddio Google Analytics i gasglu data am sut y defnyddir gwefan. Mae'r data cyffredinol hwn yn cael ei rannu gyda Google. Mae rhagor o wybodaeth am hyn yn ein <a class='govuk-link' href='terms-and-conditions'>telerau ac amodau</a>.",
+    storeAndShare: 'Storio a rhannu eich data’n rhyngwladol',
+    personalInformation:
+      "Weithiau efallai y bydd angen inni anfon eich gwybodaeth bersonol tu allan i'r DU. Pan fyddwn yn gwneud hyn, byddwn yn cydymffurfio â chyfraith diogelu data.",
+    yourRights: 'Eich hawliau',
+    youCanAsk: 'Gallwch ofyn:',
+    toSeePersonal: 'i gael gweld y data personol rydym yn ei gadw amdanoch',
+    toHavePersonal: "i'r data personol gael ei gywiro",
+    toHaveDataRemoved:
+      "i'r data personol gael ei ddileu (bydd hyn yn ddibynnol ar yr amgylchiadau, er enghraifft os ydych chi’n penderfynu peidio â pharhau gyda’ch cais)",
+    thatAccessIsRestricted:
+      "i gyfyngu ar y mynediad at y data personol (er enghraifft, gallwch ofyn i'ch data gael ei storio am gyfnod hirach a pheidio â chael ei ddileu'n awtomatig)",
+    ifYouWantToSeePersonal: 'Os ydych eisiau gweld y data personol rydym yn ei gadw amdanoch, gallwch:',
+    completeForm:
+      "lenwi ffurflen i wneud <a class='govuk-link' href='https://www.gov.uk/government/publications/request-your-personal-data-from-moj' target='_blank'>cais gwrthrych am wybodaeth</a> - bydd eich cais yn mynd i'r rheolydd data, sef MoJ",
+    writeToUs: 'ysgrifennu atom yn: Disclosure Team, Post point 10.38, 102 Petty France, Llundain, SW1H 9AJ',
+    askMoreInformation: 'Gallwch ofyn am fwy o wybodaeth am:',
+    agreementsWeHave: 'gytundebau sydd gennym ar rannu gwybodaeth gyda sefydliadau eraill',
+    withoutTellingYou: 'pryd y caniateir inni drosglwyddo gwybodaeth bersonol amdanoch heb roi gwybod ichi',
+    ourInstructions: 'ein cyfarwyddiadau i staff ynghylch sut i gasglu, defnyddio neu ddileu eich gwybodaeth bersonol',
+    howWeCheck: 'sut rydym yn gwirio bod yr wybodaeth sydd gennym yn gywir ac yn gyfredol',
+    mojProtection: 'Gallwch gysylltu â swyddog diogelu data’r MoJ drwy:',
+    pettyFrance: 'ysgrifennu atom yn: Post point 10.38, 102 Petty France, Llundain, SW1H 9AJ',
+    emailing:
+      'anfon neges e-bost i: <a href="mailto:data.compliance@justice.gov.uk" class="govuk-link">data.compliance@justice.gov.uk</a>',
+    howToComplain: 'Sut i gwyno',
+    seeOurComplaints:
+      "Gweler ein <a class='govuk-link' href='https://www.gov.uk/government/organisations/hm-courts-and-tribunals-service/about/complaints-procedure' target='_blank'>proses gwyno</a> os ydych eisiau cwyno am sut rydym wedi trin eich data personol.",
+    writeTo: 'Ysgrifennwch at: Post point 10.38, 102 Petty France, Llundain, SW1H 9AJ',
+    email:
+      'Anfonwch neges e-bost i: <a href="mailto:data.compliance@justice.gov.uk" class="govuk-link">data.compliance@justice.gov.uk</a>',
+    youCanAlsoComplain:
+      "Gallwch hefyd gyflwyno cwyn i <a class='govuk-link' href='https://ico.org.uk/global/contact-us' target='_blank'>Swyddfa’r Comisiynydd Gwybodaeth</a> os ydych yn anfodlon â’n hymateb neu'n credu nad ydym yn prosesu eich data personol yn gyfreithlon.",
   };
 
   const common = {};
