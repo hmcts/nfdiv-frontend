0d568be892904e375324f382229fdae88709be34
velimd <duzgunce.vd@gmail.com>
2021-03-03 15:12:10 +0000

    test

--- src/main/steps/privacy-policy/get.test.ts
+++ src/main/steps/privacy-policy/get.test.ts
@@ -1,24 +1,39 @@
 import { mockRequest } from '../../../test/unit/utils/mockRequest';
 import { mockResponse } from '../../../test/unit/utils/mockResponse';
+import { CaseType } from '../../app/case/case';
 import { commonContent } from '../common/common.content';
 
-import { privacyPolicyContent } from './content';
+import { generateContent } from './content';
 import { PrivacyPolicyGetController } from './get';
 
 describe('PrivacyPolicyGetController', () => {
   const controller = new PrivacyPolicyGetController();
 
-  test('Should render the privacy policy page', async () => {
+  test('Should render the privacy policy page with divorce content', async () => {
     const req = mockRequest();
     const res = mockResponse();
     await controller.get(req, res);
 
     expect(res.render).toBeCalledWith(expect.anything(), {
-      ...privacyPolicyContent.en,
-      ...privacyPolicyContent.common,
+      ...generateContent({ isDivorce: true }).en,
       ...commonContent.en,
-      hideBackButton: false,
       formState: req.session.userCase,
+      hideBackButton: false,
+      sessionErrors: [],
+    });
+  });
+
+  test('Should render the privacy policy page with civil content', async () => {
+    const req = mockRequest();
+    const res = mockResponse();
+    res.locals.serviceType = CaseType.Dissolution;
+    await controller.get(req, res);
+
+    expect(res.render).toBeCalledWith(expect.anything(), {
+      ...generateContent({ isDivorce: false }).en,
+      ...commonContent.en,
+      formState: req.session.userCase,
+      hideBackButton: false,
       sessionErrors: [],
     });
   });
