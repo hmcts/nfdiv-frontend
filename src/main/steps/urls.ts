0b7b2f796ab411501a87dcd4a6e3b65c5f802057
velimd <duzgunce.vd@gmail.com>
2021-03-04 09:16:33 +0000

    implementation

--- src/main/steps/urls.ts
+++ src/main/steps/urls.ts
@@ -20,6 +20,7 @@ export const HELP_PAYING_NEED_TO_APPLY: PageLink = '/apply-for-help-with-fees';
 export const IN_THE_UK: PageLink = '/in-the-uk';
 export const CERTIFICATE_IN_ENGLISH: PageLink = '/certificate-in-english';
 export const CERTIFIED_TRANSLATION: PageLink = '/certified-translation';
+export const GET_CERTIFIED_TRANSLATION: PageLink = '/get-certified-translation';
 
 export const RELATIONSHIP_DATE_URL: PageLink = '/date-from-certificate';
 export const RELATIONSHIP_NOT_LONG_ENOUGH_URL: PageLink = '/not-long-enough-to-apply';
