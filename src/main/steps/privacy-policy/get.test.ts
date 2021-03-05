c4b74b2162d7a6db1baa38737f9f5678193c5181
Velican <31857663+velimd@users.noreply.github.com>
2021-03-05 14:43:49 +0000

    Nfdiv 293/privacy policy content update (#282)
    * updating content
    
    * welsh translation
    
    * test
    
    * links opening in the same tab
    
    * fix footer order
    
    * fix e2e test

--- src/main/steps/privacy-policy/get.test.ts
+++ src/main/steps/privacy-policy/get.test.ts
@@ -1,22 +1,35 @@
 import { defaultViewArgs } from '../../../test/unit/utils/defaultViewArgs';
 import { mockRequest } from '../../../test/unit/utils/mockRequest';
 import { mockResponse } from '../../../test/unit/utils/mockResponse';
+import { CaseType } from '../../app/case/case';
 
-import { privacyPolicyContent } from './content';
+import { generateContent } from './content';
 import { PrivacyPolicyGetController } from './get';
 
 describe('PrivacyPolicyGetController', () => {
   const controller = new PrivacyPolicyGetController();
 
-  test('Should render the privacy policy page', async () => {
+  test('Should render the privacy policy page with divorce content', async () => {
     const req = mockRequest();
     const res = mockResponse();
     await controller.get(req, res);
 
     expect(res.render).toBeCalledWith(expect.anything(), {
+      ...generateContent({ isDivorce: true }).en,
       ...defaultViewArgs,
-      ...privacyPolicyContent.en,
-      ...privacyPolicyContent.common,
+      formState: req.session.userCase,
+    });
+  });
+
+  test('Should render the privacy policy page with civil content', async () => {
+    const req = mockRequest();
+    const res = mockResponse();
+    res.locals.serviceType = CaseType.Dissolution;
+    await controller.get(req, res);
+
+    expect(res.render).toBeCalledWith(expect.anything(), {
+      ...generateContent({ isDivorce: false }).en,
+      ...defaultViewArgs,
       formState: req.session.userCase,
     });
   });
