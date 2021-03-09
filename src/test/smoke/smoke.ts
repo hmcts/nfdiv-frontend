4883d3be776e42df480ac076e3ff96332a8c9fdf
Seb Flippence <144435+SebFlippence@users.noreply.github.com>
2021-03-09 10:13:32 +0000

    chore: smoke test web apps dependencies before running functional tests

--- src/test/smoke/smoke.ts
+++ src/test/smoke/smoke.ts
@@ -2,26 +2,34 @@ import axios from 'axios';
 
 import { config } from '../config';
 
-jest.retryTimes(20); // 20 retries at 3 second intervals = 1 minute
+jest.retryTimes(20); // 20 retries at 1 second intervals
 jest.setTimeout(15000);
+
+const servicesToCheck = [
+  { name: 'No Fault Divorce Web', url: config.TEST_URL },
+  { name: 'IDAM Web', url: 'https://idam-web-public.aat.platform.hmcts.net' },
+  { name: 'IDAM API', url: 'https://idam-api.aat.platform.hmcts.net' },
+  { name: 'Auth Provider', url: 'http://rpe-service-auth-provider-aat.service.core-compute-aat.internal' },
+  { name: 'CCD Data Store', url: 'http://ccd-data-store-api-aat.service.core-compute-aat.internal' },
+];
 
 describe('Smoke Test', () => {
   describe('Health Check', () => {
-    test('should return status 200', async () => {
-      let response = { status: 0 };
-      try {
-        response = await axios.get(`${config.TEST_URL}/health`);
-      } catch (e) {
-        response.status = e;
-      }
+    test.each(servicesToCheck)('should return status 200 status: UP %o', async ({ name, url }) => {
+      const checkService = async () => {
+        try {
+          const response = await axios.get(`${url}/health`);
+          if (response.status !== 200 || response.data?.status !== 'UP') {
+            throw new Error(`Status: ${response.status} Data: '${JSON.stringify(response.data)}'`);
+          }
+        } catch (e) {
+          await new Promise((resolve, reject) =>
+            setTimeout(() => reject(`'${name}' endpoint is not up: '${url}': ${e}`), 1000)
+          );
+        }
+      };
 
-      if (response.status !== 200) {
-        await new Promise((resolve, reject) =>
-          setTimeout(() => reject(`App health endpoint did not return a 200 OK response: ${response.status}`), 3000)
-        );
-      }
-
-      expect(response.status).toBe(200);
+      await expect(checkService()).resolves.not.toThrow();
     });
   });
 });
