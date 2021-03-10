8e321a3fe4b932d5f754e1a1a5536f4e17263f18
Seb Flippence <144435+SebFlippence@users.noreply.github.com>
2021-03-10 18:41:38 +0000

    fix: use service urls from config

--- src/test/smoke/smoke.ts
+++ src/test/smoke/smoke.ts
@@ -1,35 +1,40 @@
 import axios from 'axios';
+import config from 'config';
 
-import { config } from '../config';
+import { config as testConfig } from '../config';
 
 jest.retryTimes(20); // 20 retries at 1 second intervals
 jest.setTimeout(15000);
 
 const servicesToCheck = [
-  { name: 'No Fault Divorce Web', url: config.TEST_URL },
-  { name: 'IDAM Web', url: 'https://idam-web-public.aat.platform.hmcts.net' },
-  { name: 'IDAM API', url: 'https://idam-api.aat.platform.hmcts.net' },
-  { name: 'Auth Provider', url: 'http://rpe-service-auth-provider-aat.service.core-compute-aat.internal' },
-  { name: 'CCD Data Store', url: 'http://ccd-data-store-api-aat.service.core-compute-aat.internal' },
+  { name: 'No Fault Divorce Web', url: testConfig.TEST_URL },
+  { name: 'IDAM Web', url: config.get('services.idam.authorizationURL') },
+  { name: 'IDAM API', url: config.get('services.idam.tokenURL') },
+  { name: 'Auth Provider', url: config.get('services.authProvider.url') },
+  { name: 'CCD Data Store', url: config.get('services.case.url') },
 ];
 
 describe('Smoke Test', () => {
   describe('Health Check', () => {
-    test.each(servicesToCheck)('should return status 200 status: UP %o', async ({ name, url }) => {
-      const checkService = async () => {
-        try {
-          const response = await axios.get(`${url}/health`);
-          if (response.status !== 200 || response.data?.status !== 'UP') {
-            throw new Error(`Status: ${response.status} Data: '${JSON.stringify(response.data)}'`);
-          }
-        } catch (e) {
-          await new Promise((resolve, reject) =>
-            setTimeout(() => reject(`'${name}' endpoint is not up: '${url}': ${e}`), 1000)
-          );
-        }
-      };
+    describe.each(servicesToCheck)('Required services should return 200 status UP', ({ name, url }) => {
+      const parsedUrl = new URL('/health', url as string).toString();
 
-      await expect(checkService()).resolves.not.toThrow();
+      test(`${name}: ${parsedUrl}`, async () => {
+        const checkService = async () => {
+          try {
+            const response = await axios.get(parsedUrl);
+            if (response.status !== 200 || response.data?.status !== 'UP') {
+              throw new Error(`Status: ${response.status} Data: '${JSON.stringify(response.data)}'`);
+            }
+          } catch (e) {
+            await new Promise((resolve, reject) =>
+              setTimeout(() => reject(`'${name}' endpoint is not up: '${parsedUrl}': ${e}`), 1000)
+            );
+          }
+        };
+
+        await expect(checkService()).resolves.not.toThrow();
+      });
     });
   });
 });
