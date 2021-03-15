e270c5f869bd485f29fefb085027fb789d2bc7e4
Sarah <sarahjanelittlejohn@gmail.com>
2021-03-15 17:45:27 +0000

    NFDIV-494: review comments

--- src/main/assets/js/go-back.ts
+++ src/main/assets/js/go-back.ts
@@ -1,8 +1,10 @@
+import { CHECK_JURISDICTION, JURISDICTION_INTERSTITIAL_URL } from '../../steps/urls';
+
 const backLink: HTMLElement | null = document.querySelector('.govuk-back-link');
 if (backLink) {
   backLink.onclick = function (e) {
-    if (document.location.pathname === '/jurisdiction/interstitial') {
-      document.location.pathname = '/check-jurisdiction';
+    if (document.location.pathname === JURISDICTION_INTERSTITIAL_URL) {
+      document.location.pathname = CHECK_JURISDICTION;
     } else {
       e.preventDefault();
       history.go(-1);
