c81bc90c237638ad0c1f5a1a722abfe9c6b19315
Sarah <sarahjanelittlejohn@gmail.com>
2021-03-22 17:34:58 +0000

    NFDIV-515: clean up

--- src/main/app/controller/PostController.ts
+++ src/main/app/controller/PostController.ts
@@ -3,7 +3,7 @@ import { Response } from 'express';
 
 import { getNextStepUrl } from '../../steps';
 import { addConnection } from '../../steps/jurisdiction/interstitial/connections';
-import { ENGLISH_WELSH_COURTS, JURISDICTION_INTERSTITIAL_URL, SAVE_AND_SIGN_OUT } from '../../steps/urls';
+import { JURISDICTION_INTERSTITIAL_URL, SAVE_AND_SIGN_OUT } from '../../steps/urls';
 import { Case } from '../case/case';
 import { PATCH_CASE, SAVE_AND_CLOSE } from '../case/definition';
 import { Form } from '../form/Form';
@@ -53,7 +53,7 @@ export class PostController<T extends AnyObject> {
       nextUrl = req.url;
     } else {
       nextUrl = getNextStepUrl(req, req.session.userCase);
-      if (nextUrl === ENGLISH_WELSH_COURTS || JURISDICTION_INTERSTITIAL_URL) {
+      if (nextUrl === JURISDICTION_INTERSTITIAL_URL) {
         const connection = addConnection(req.session.userCase);
         if (connection) {
           formData.connections = [connection];
