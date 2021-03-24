6b31e9443913015fbcc27e624a2dddce32612f22
Seb Flippence <144435+SebFlippence@users.noreply.github.com>
2021-03-24 16:39:45 +0000

    chore: moved isEmpty check to RegEx

--- src/main/app/form/validation.ts
+++ src/main/app/form/validation.ts
@@ -90,11 +90,7 @@ export const isInvalidHelpWithFeesRef: Validator = value => {
 };
 
 export const isPhoneNoValid: Validator = value => {
-  if (!value) {
-    return;
-  }
-
   if (typeof value === 'string') {
-    return !value.match(/^[0-9 +().-]{9,}$/) ? 'invalid' : undefined;
+    return !value.match(/^$|^[0-9 +().-]{9,}$/) ? 'invalid' : undefined;
   }
 };
