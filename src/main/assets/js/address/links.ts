61304c41f444dc5a7f234a791c5ca311ca695651
Seb Flippence <144435+SebFlippence@users.noreply.github.com>
2021-03-31 15:29:30 +0100

    fix: address back link

--- src/main/assets/js/address/links.ts
+++ src/main/assets/js/address/links.ts
@@ -16,15 +16,17 @@ const toggleInternationalAddressFields = (toggle: string) => {
 export const showInternationalAddressFields = (): void => toggleInternationalAddressFields('remove');
 export const hideInternationalAddressFields = (): void => toggleInternationalAddressFields('add');
 
-export const showUkAddressFields = (): void => {
+const toggleUkAddressFields = (toggle: string): void => {
   (getById('selectAddress') as HTMLElement).classList.add(hidden);
-  (qs('.govuk-form-group.address1') as HTMLElement).classList.remove(hidden);
-  (qs('.govuk-form-group.address2') as HTMLElement).classList.remove(hidden);
-  (qs('.govuk-form-group.addressTown') as HTMLElement).classList.remove(hidden);
-  (qs('.govuk-form-group.addressCounty') as HTMLElement).classList.remove(hidden);
-  (qs('.govuk-form-group.addressPostcode') as HTMLElement).classList.remove(hidden);
-  (getById('main-form-submit') as HTMLElement).classList.remove(hidden);
+  (qs('.govuk-form-group.address1') as HTMLElement).classList[toggle](hidden);
+  (qs('.govuk-form-group.address2') as HTMLElement).classList[toggle](hidden);
+  (qs('.govuk-form-group.addressTown') as HTMLElement).classList[toggle](hidden);
+  (qs('.govuk-form-group.addressCounty') as HTMLElement).classList[toggle](hidden);
+  (qs('.govuk-form-group.addressPostcode') as HTMLElement).classList[toggle](hidden);
+  (getById('main-form-submit') as HTMLElement).classList[toggle](hidden);
 };
+export const showUkAddressFields = (): void => toggleUkAddressFields('remove');
+export const hideUkAddressFields = (): void => toggleUkAddressFields('add');
 
 const cannotEnterUkPostcode = getById('cannot-enter-uk-postcode') as HTMLAnchorElement;
 if (cannotEnterUkPostcode) {
@@ -48,26 +50,40 @@ if (cannotFindAddress) {
   };
 }
 
-const resetPostcodeLookupLinks = document.querySelectorAll(
-  '[data-link="resetPostcodeLookup"]'
-) as NodeListOf<HTMLElement>;
-if (resetPostcodeLookupLinks) {
-  const resetPostcodeLookupClick = e => {
-    e.preventDefault();
-    hideErrors();
-    hideSelectAddress();
-    hideInternationalAddressFields();
+const onResetPostcodeLookup = e => {
+  e.preventDefault();
+  hideErrors();
+  hideSelectAddress();
+  hideInternationalAddressFields();
+  hideUkAddressFields();
 
-    for (const el of qsa('option:not([value="-1"])')) {
-      el.remove();
-    }
-    for (const el of qsa('.govuk-error-summary:not([id="addressErrorSummary"])')) {
-      el.remove();
-    }
-
-    showEnterPostcode();
-  };
-  for (const el of resetPostcodeLookupLinks) {
-    el.onclick = resetPostcodeLookupClick;
+  for (const el of qsa('option:not([value="-1"])')) {
+    el.remove();
   }
+  for (const el of qsa('.govuk-error-summary:not([id="addressErrorSummary"])')) {
+    el.remove();
+  }
+
+  showEnterPostcode();
+};
+
+const resetPostcodeLookupLinks = qsa('[data-link="resetPostcodeLookup"]') as NodeListOf<HTMLElement>;
+if (resetPostcodeLookupLinks) {
+  for (const el of resetPostcodeLookupLinks) {
+    el.onclick = onResetPostcodeLookup;
+  }
+}
+
+const backLink = qs('.govuk-back-link');
+if (backLink) {
+  backLink.onclick = function (e) {
+    e.preventDefault();
+    const notOnPostcodeEntry = (getById('enterPostcode') as HTMLElement).classList.contains(hidden);
+    if (notOnPostcodeEntry) {
+      (e.target as HTMLAnchorElement).blur();
+      onResetPostcodeLookup(e);
+    } else {
+      history.go(-1);
+    }
+  };
 }
