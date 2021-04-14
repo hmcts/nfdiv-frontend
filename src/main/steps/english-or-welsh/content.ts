c65ae98488292d8a27fcc089187751f18b85a773
Sarah <sarahjanelittlejohn@gmail.com>
2021-04-14 15:44:35 +0100

    NFDIV-522: clean up

--- src/main/steps/english-or-welsh/content.ts
+++ src/main/steps/english-or-welsh/content.ts
@@ -15,7 +15,7 @@ const en = ({ required }) => ({
 const cy = () => ({
   title: 'Ym mha iaith hoffech chi gael negeseuon e-bost a dogfennau?',
   errors: {
-    englishOrWelsh: { required: 'Nid ydych wedi ateb y cwestiwn. Mae angen ichi ddewis ateb cyn parhau.' },
+    englishOrWelsh: 'Nid ydych wedi ateb y cwestiwn. Mae angen ichi ddewis ateb cyn parhau.',
   },
 });
 
