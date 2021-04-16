222505379552a213a1dbfef2fe3e6d13704ea52c
Seb Flippence <144435+SebFlippence@users.noreply.github.com>
2021-04-16 15:54:15 +0100

    fix: Sauce Labs powered cross browser tests

--- src/test/steps/common.ts
+++ src/test/steps/common.ts
@@ -17,7 +17,7 @@ export const iAmOnPage = (text: string): void => {
 Given('I go to {string}', iAmOnPage);
 
 Then('the page URL should be {string}', (url: string) => {
-  I.waitUrlEquals(url);
+  I.waitInUrl(url);
 });
 
 Given('I login', () => {
--- src/test/steps/happy-path.ts
+++ src/test/steps/happy-path.ts
@@ -15,11 +15,11 @@ Given("I've completed all happy path questions correctly to get to check your an
   iClick('My husband');
   iClick('Continue');
 
-  I.waitUrlEquals('/irretrievable-breakdown');
+  I.waitInUrl('/irretrievable-breakdown');
   iClick('Yes, my marriage has irretrievably broken down');
   iClick('Continue');
 
-  I.waitUrlEquals('/date-from-certificate');
+  I.waitInUrl('/date-from-certificate');
   iClearTheForm();
   iClick('Day');
   I.type('31');
@@ -29,30 +29,30 @@ Given("I've completed all happy path questions correctly to get to check your an
   I.type('1999');
   iClick('Continue');
 
-  I.waitUrlEquals('/do-you-have-your-certificate');
+  I.waitInUrl('/do-you-have-your-certificate');
   iClick('Yes, I have my marriage certificate');
   iClick('Continue');
 
-  I.waitUrlEquals('/help-with-your-fee');
+  I.waitInUrl('/help-with-your-fee');
   iClick('I do not need help paying the fee');
   iClick('Continue');
 
-  I.waitUrlEquals('/in-the-uk');
+  I.waitInUrl('/in-the-uk');
   iClick('Yes');
   iClick('Continue');
 
-  I.waitUrlEquals('/check-jurisdiction');
+  I.waitInUrl('/check-jurisdiction');
   iClick('Continue');
 
-  I.waitUrlEquals('/where-your-lives-are-based');
+  I.waitInUrl('/where-your-lives-are-based');
   checkOptionFor('Yes', 'Is your life mainly based in England or Wales?');
   checkOptionFor('Yes', 'Is your husband’s life mainly based in England or Wales?');
   iClick('Continue');
 
-  I.waitUrlEquals('/you-can-use-english-welsh-courts');
+  I.waitInUrl('/you-can-use-english-welsh-courts');
   iClick('Continue');
 
-  I.waitUrlEquals('/your-names-on-certificate');
+  I.waitInUrl('/your-names-on-certificate');
   iClearTheForm();
   iClick('Copy your full name');
   I.type('First name Last name');
@@ -60,25 +60,25 @@ Given("I've completed all happy path questions correctly to get to check your an
   I.type('Husbands name');
   iClick('Continue');
 
-  I.waitUrlEquals('/changes-to-your-name');
+  I.waitInUrl('/changes-to-your-name');
   checkOptionFor('No', 'Did you change your last name when you got married?');
   checkOptionFor('No', 'Have you changed any part of your name since getting married?');
   iClick('Continue');
 
-  I.waitUrlEquals('/how-the-court-will-contact-you');
+  I.waitInUrl('/how-the-court-will-contact-you');
   iClearTheForm();
   iClick('I agree that the divorce service can send me notifications');
   iClick('Continue');
 
-  I.waitUrlEquals('/english-or-welsh');
+  I.waitInUrl('/english-or-welsh');
   iClick('English');
   iClick('Continue');
 
-  I.waitUrlEquals('/address-private');
+  I.waitInUrl('/address-private');
   iClick('I do not need my contact details kept private');
   iClick('Continue');
 
-  I.waitUrlEquals('/enter-your-address');
+  I.waitInUrl('/enter-your-address');
   iResetThePostCodeLookUpForm();
   iClick('Enter a UK postcode');
   I.type('SW1A 1AA');
@@ -87,17 +87,17 @@ Given("I've completed all happy path questions correctly to get to check your an
   I.selectOption('Select an address', 'BUCKINGHAM PALACE, LONDON, SW1A 1AA');
   iClick('Continue');
 
-  I.waitUrlEquals('/their-email-address');
+  I.waitInUrl('/their-email-address');
   iClearTheForm();
   iClick("Your husband's email address");
   I.type('husband@example.com');
   iClick('Continue');
 
-  I.waitUrlEquals('/do-you-have-address');
+  I.waitInUrl('/do-you-have-address');
   iClick('Yes, I have their address');
   iClick('Continue');
 
-  I.waitUrlEquals('/enter-their-address');
+  I.waitInUrl('/enter-their-address');
   iResetThePostCodeLookUpForm();
   iClick('Enter a UK postcode');
   I.type('SW1H 9AJ');
@@ -106,9 +106,9 @@ Given("I've completed all happy path questions correctly to get to check your an
   I.selectOption('Select an address', 'MINISTRY OF JUSTICE, SEVENTH FLOOR, 102, PETTY FRANCE, LONDON, SW1H 9AJ');
   iClick('Continue');
 
-  I.waitUrlEquals('/other-court-cases');
+  I.waitInUrl('/other-court-cases');
   iClick('No');
   iClick('Continue');
 
-  I.waitUrlEquals('/money-property');
+  I.waitInUrl('/money-property');
 });
