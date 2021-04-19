6885c2d261bf7d7e8f4841b4f1eb47adbb5490a7
velimd <duzgunce.vd@gmail.com>
2021-04-19 18:05:26 +0100

    content fix up

--- src/main/steps/connection-summary/content.ts
+++ src/main/steps/connection-summary/content.ts
@@ -1,13 +1,27 @@
+import { Checkbox } from '../../app/case/case';
+import { YesOrNo } from '../../app/case/definition';
 import { TranslationFn } from '../../app/controller/GetController';
 import { FormContent } from '../../app/form/Form';
+import { CommonContent } from '../common/common.content';
 import { habitualAndDomicileHelp } from '../you-can-use-english-welsh-courts/content';
 
-const en = ({ partner, apply }) => ({
-  title: `You can use English or Welsh courts to ${apply}`,
-  line1: `Your answers indicate that you can ${apply} in England and Wales because:`,
-  line2: `both you and your ${partner} are habitually resident`,
-  ...habitualAndDomicileHelp.en,
-});
+const en = ({ isDivorce, applyForDivorce, applyForDissolution, partner, formState }: CommonContent) => {
+  const apply = isDivorce ? applyForDivorce : applyForDissolution;
+  let reasonsToApplyInEnglandOrWales;
+  if (formState?.jurisdictionResidualEligible === YesOrNo.YES) {
+    reasonsToApplyInEnglandOrWales = 'the courts of England and Wales have jurisdiction on a residual basis';
+  } else if (formState?.lastHabituallyResident === YesOrNo.NO && formState?.sameSex === Checkbox.Checked) {
+    reasonsToApplyInEnglandOrWales = `your ${partner} is domiciled in England or Wales`;
+  } else {
+    reasonsToApplyInEnglandOrWales = 'you are domiciled in England and Wales';
+  }
+  return {
+    title: `You can use English or Welsh courts to ${apply}`,
+    line1: `Your answers indicate that you can ${apply} in England and Wales because:`,
+    line2: reasonsToApplyInEnglandOrWales,
+    ...habitualAndDomicileHelp.en,
+  };
+};
 
 const cy = en;
 
@@ -21,8 +35,7 @@ export const form: FormContent = {
 const languages = { en, cy };
 
 export const generateContent: TranslationFn = content => {
-  const apply = content.isDivorce ? content.applyForDivorce : content.applyForDissolution;
-  const translations = languages[content.language]({ ...content, apply });
+  const translations = languages[content.language](content);
   return {
     ...translations,
     form,
