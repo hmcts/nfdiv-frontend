793dd784153dec3a73b1fb2fa9738250dd555252
Tom Elliott <tomelliott@Toms-MBP.home>
2021-05-17 12:50:33 +0100

    NFDIV-666: Rename partner to applicant 2 in relevant locations

--- src/main/steps/do-you-have-address/content.ts
+++ src/main/steps/do-you-have-address/content.ts
@@ -3,55 +3,55 @@ import { TranslationFn } from '../../app/controller/GetController';
 import { FormContent } from '../../app/form/Form';
 import { isFieldFilledIn } from '../../app/form/validation';
 
-const en = ({ partner, isDivorce, partnerEmailProvided, required }) => ({
-  title: `Do you have your ${partner}'s postal address?`,
+const en = ({ applicant2, isDivorce, applicant2EmailProvided, required }) => ({
+  title: `Do you have your ${applicant2}'s postal address?`,
   line1: `${
-    partnerEmailProvided
-      ? `The court needs your ${partner}'s address, to notify them about the ${
+    applicant2EmailProvided
+      ? `The court needs your ${applicant2}'s address, to notify them about the ${
           isDivorce ? 'divorce' : 'ending your civil partnership'
         }.`
-      : `It’s important you provide your ${partner}'s address. You did not provide their email address and the court needs a way to ‘serve’ (deliver) the ${
+      : `It’s important you provide your ${applicant2}'s address. You did not provide their email address and the court needs a way to ‘serve’ (deliver) the ${
           isDivorce ? 'divorce' : 'ending your civil partnership'
         } papers to them.`
   }`,
   line2:
     'It can be their home address or their solicitor’s address. It can be UK or international. If you use their work address, you need to ask their permission.',
   line3: `If you do not know their current address then you can use their last-known address. ${
-    !partnerEmailProvided
+    !applicant2EmailProvided
       ? 'If they do not respond at the address you provide, then you will need to make a separate application to serve (deliver) the papers to them another way.'
       : ''
   }`,
   haveTheirAddress: 'Yes, I have their address',
   doNotHaveTheirAddress: 'No, I do not have their address',
   errors: {
-    knowPartnersAddress: {
+    knowApplicant2Address: {
       required,
     },
   },
 });
 
-const cy: typeof en = ({ partner, isDivorce, partnerEmailProvided, required }) => ({
-  title: `A oes gennych gyfeiriad post eich ${partner}?`,
+const cy: typeof en = ({ applicant2, isDivorce, applicant2EmailProvided, required }) => ({
+  title: `A oes gennych gyfeiriad post eich ${applicant2}?`,
   line1: `${
-    partnerEmailProvided
-      ? `Mae angen cyfeiriad eich ${partner} ar y llys, i'w hysbysu am yr ${
+    applicant2EmailProvided
+      ? `Mae angen cyfeiriad eich ${applicant2} ar y llys, i'w hysbysu am yr ${
           isDivorce ? 'ysgariad' : 'y cais i ddod â phartneriaeth sifil i ben'
         }.`
-      : `Mae'n bwysig eich bod yn darparu cyfeiriad eich ${partner}. Ni wnaethoch ddarparu ei gyfeiriad/chyfeiriad e-bost ac mae'r llys angen ffordd i 'gyflwyno' (danfon) y papurau ${
+      : `Mae'n bwysig eich bod yn darparu cyfeiriad eich ${applicant2}. Ni wnaethoch ddarparu ei gyfeiriad/chyfeiriad e-bost ac mae'r llys angen ffordd i 'gyflwyno' (danfon) y papurau ${
           isDivorce ? 'ysgariad' : "dod â'ch partneriaeth sifil i ben"
         } iddo/iddi.`
   }`,
   line2:
     "Gall fod yn gyfeiriad cartref iddo/iddi neu'n gyfeiriad ei gyfreithiwr/chyfreithiwr. Gall fod yn y DU neu'n rhyngwladol. Os ydych yn defnyddio ei gyfeiriad/chyfeiriad gwaith, mae angen i chi ofyn am ganiatâd.",
   line3: `Os nad ydych yn gwybod beth yw ei gyfeiriad/chyfeiriad presennol yna gallwch ddefnyddio ei gyfeiriad/chyfeiriad hysbys diwethaf. ${
-    !partnerEmailProvided
+    !applicant2EmailProvided
       ? "Os nad yw'n ymateb yn y cyfeiriad a roddwch, yna bydd angen i chi wneud cais ar wahân i gyflwyno (danfon) y papurau ato/ati mewn ffordd arall."
       : ''
   }`,
   haveTheirAddress: 'Oes, mae gennyf ei gyfeiriad/chyfeiriad',
   doNotHaveTheirAddress: 'Na, nid yw ei gyfeiriad/chyfeiriad gennyf',
   errors: {
-    knowPartnersAddress: {
+    knowApplicant2Address: {
       required,
     },
   },
@@ -59,7 +59,7 @@ const cy: typeof en = ({ partner, isDivorce, partnerEmailProvided, required }) =
 
 export const form: FormContent = {
   fields: {
-    knowPartnersAddress: {
+    knowApplicant2Address: {
       type: 'radios',
       classes: 'govuk-radios',
       label: l => l.title,
--- src/main/steps/your-names-on-certificate/content.ts
+++ src/main/steps/your-names-on-certificate/content.ts
@@ -3,7 +3,7 @@ import { TranslationFn } from '../../app/controller/GetController';
 import { FormContent } from '../../app/form/Form';
 import { isFieldFilledIn, isFieldLetters } from '../../app/form/validation';
 
-const en = ({ isDivorce, relationship, partner, selectedGender }) => {
+const en = ({ isDivorce, relationship, applicant2, selectedGender }) => {
   let personOfMaidenName = 'you or your civil partner';
   if (isDivorce) {
     personOfMaidenName = selectedGender === Gender.MALE ? 'you' : 'your wife';
@@ -11,19 +11,19 @@ const en = ({ isDivorce, relationship, partner, selectedGender }) => {
 
   return {
     title: `Your names on your ${relationship} certificate`,
-    line1: `These are the names you and your ${partner} used before you ${
+    line1: `These are the names you and your ${applicant2} used before you ${
       isDivorce ? 'were married' : 'formed your civil partnership'
     }. They can sometimes be different from your current names. For example, if ${personOfMaidenName} had a maiden name.`,
     warningText: `Copy the ${relationship} certificate exactly. For example, if it says ‘Sarah Brown (formerly known as Sarah Smith)’, then enter that.`,
     fullNameOnCertificate: `Copy your full name from the ${relationship} certificate`,
-    partnersFullNameOnCertificate: `Copy your ${partner}'s full name from the ${relationship} certificate`,
+    applicant2FullNameOnCertificate: `Copy your ${applicant2}'s full name from the ${relationship} certificate`,
     hint: 'Include all the text related to the name',
     errors: {
       fullNameOnCertificate: {
         required: 'You have not entered anything. Enter your full name as it appears on your marriage certificate.',
         invalid: 'You have entered an invalid character, like a number. Enter your name using letters only.',
       },
-      partnersFullNameOnCertificate: {
+      applicant2FullNameOnCertificate: {
         required: 'You have not entered anything. Enter their full name as it appears on your marriage certificate.',
         invalid: 'You have entered an invalid character, like a number. Enter your name using letters only.',
       },
@@ -31,7 +31,7 @@ const en = ({ isDivorce, relationship, partner, selectedGender }) => {
   };
 };
 
-const cy = ({ isDivorce, relationship, partner, selectedGender }) => {
+const cy = ({ isDivorce, relationship, applicant2, selectedGender }) => {
   let personOfMaidenName = 'gan eich partner sifil';
   if (isDivorce) {
     personOfMaidenName = selectedGender === Gender.MALE ? 'gennych chi' : 'gan eich gwraig';
@@ -39,12 +39,12 @@ const cy = ({ isDivorce, relationship, partner, selectedGender }) => {
 
   return {
     title: `Eich enwau fel y maent yn ymddangos ar eich tystysgrif ${relationship}`,
-    line1: `Dyma'r enwau yr oeddech chi a'ch ${partner} yn eu defnyddio cyn i chi ${
+    line1: `Dyma'r enwau yr oeddech chi a'ch ${applicant2} yn eu defnyddio cyn i chi ${
       isDivorce ? 'briodi' : 'ffurfio eich partneriaeth sifil'
     }. Weithiau gallant fod yn wahanol i'ch enwau cyfredol. Er enghraifft, os oedd ${personOfMaidenName} enw cyn priodi.`,
     warningText: `Copïwch union eriad y dystysgrif ${relationship}. Er enghraifft, os yw'n dweud ‘Sarah Brown (a elwid yn flaenorol yn Sarah Smith)’, yna rhowch hynny.`,
     fullNameOnCertificate: `Copïwch eich enw yn llawn fel y mae'n ymddangos ar y dystysgrif ${relationship}`,
-    partnersFullNameOnCertificate: `Copïwch enw llawn eich ${partner} fel y mae'n ymddangos ar y dystysgrif ${relationship}`,
+    applicant2FullNameOnCertificate: `Copïwch enw llawn eich ${applicant2} fel y mae'n ymddangos ar y dystysgrif ${relationship}`,
     hint: 'Dylech gynnwys testun eich enw yn llawn',
     errors: {
       fullNameOnCertificate: {
@@ -53,7 +53,7 @@ const cy = ({ isDivorce, relationship, partner, selectedGender }) => {
         invalid:
           'Rydych wedi defnyddio nod annillys, er enghraifft rhif. Nodwch eich enw gan ddefnyddio llythrennau yn unig. ',
       },
-      partnersFullNameOnCertificate: {
+      applicant2FullNameOnCertificate: {
         required:
           'Nid ydych wedi nodi unrhyw beth. Rhowch eu henw yn llawn fel y mae’n ymddangos ar eich tystysgrif priodas',
         invalid:
@@ -72,10 +72,10 @@ export const form: FormContent = {
       hint: l => l.hint,
       validator: value => isFieldFilledIn(value) || isFieldLetters(value),
     },
-    partnersFullNameOnCertificate: {
+    applicant2FullNameOnCertificate: {
       type: 'text',
       classes: 'govuk-input--width-20',
-      label: l => l.partnersFullNameOnCertificate,
+      label: l => l.applicant2FullNameOnCertificate,
       hint: l => l.hint,
       validator: value => isFieldFilledIn(value) || isFieldLetters(value),
     },
