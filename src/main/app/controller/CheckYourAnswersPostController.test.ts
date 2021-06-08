19ed2c60083d1c4f54e891294468e383b5641c87
velimd <duzgunce.vd@gmail.com>
2021-06-08 16:01:06 +0100

    merge fix with InviteApplicant2PostController

--- src/main/app/controller/CheckYourAnswersPostController.test.ts
+++ src/main/app/controller/CheckYourAnswersPostController.test.ts
@@ -1,7 +1,7 @@
 import { mockRequest } from '../../../test/unit/utils/mockRequest';
 import { mockResponse } from '../../../test/unit/utils/mockResponse';
 import { Checkbox } from '../../app/case/case';
-import { CITIZEN_SUBMIT, State } from '../../app/case/definition';
+import { ApplicationType, CITIZEN_INVITE_APPLICANT_2, CITIZEN_SUBMIT, State } from '../../app/case/definition';
 import { Form } from '../../app/form/Form';
 
 import { CheckYourAnswersPostController } from './CheckYourAnswersPostController';
@@ -34,6 +34,37 @@ describe('CheckYourAnswersPostController', () => {
     await checkYourAnswersPostController.post(req, res, next);
 
     expect(req.locals.api.triggerEvent).toHaveBeenCalledWith('1234', body, CITIZEN_SUBMIT);
+    expect(req.session.errors).toStrictEqual([]);
+    expect(req.session.userCase).toEqual(expectedUserCase);
+  });
+
+  test('Should add dueDate field and call trigger CITIZEN_INVITE_APPLICANT_2 if a joint application type', async () => {
+    const errors = [] as never[];
+    const body = {
+      applicationType: ApplicationType.JOINT_APPLICATION,
+    };
+    const bodyWithConnection = {
+      applicationType: ApplicationType.JOINT_APPLICATION,
+    };
+    const mockForm = {
+      setFormState: jest.fn(),
+      getErrors: () => errors,
+      getParsedBody: () => body,
+    } as unknown as Form;
+
+    const checkYourAnswersPostController = new CheckYourAnswersPostController(mockForm);
+    const expectedUserCase = {
+      id: '1234',
+      applicationType: ApplicationType.JOINT_APPLICATION,
+    };
+
+    const req = mockRequest({ body });
+    (req.locals.api.triggerEvent as jest.Mock).mockResolvedValueOnce(expectedUserCase);
+    const res = mockResponse();
+    const next = jest.fn();
+    await checkYourAnswersPostController.post(req, res, next);
+
+    expect(req.locals.api.triggerEvent).toHaveBeenCalledWith('1234', bodyWithConnection, CITIZEN_INVITE_APPLICANT_2);
     expect(req.session.errors).toStrictEqual([]);
     expect(req.session.userCase).toEqual(expectedUserCase);
   });
--- src/main/app/controller/CheckYourAnswersPostController.ts
+++ src/main/app/controller/CheckYourAnswersPostController.ts
@@ -1,10 +1,12 @@
 import autobind from 'autobind-decorator';
 import { NextFunction, Response } from 'express';
 
-import { CITIZEN_SUBMIT } from '../../app/case/definition';
+import { ApplicationType, CITIZEN_INVITE_APPLICANT_2, CITIZEN_SUBMIT } from '../../app/case/definition';
 import { AppRequest } from '../../app/controller/AppRequest';
 import { AnyObject, PostController } from '../../app/controller/PostController';
 import { Form } from '../../app/form/Form';
+import { getNextStepUrl } from '../../steps';
+import { Case } from '../case/case';
 
 @autobind
 export class CheckYourAnswersPostController extends PostController<AnyObject> {
@@ -13,6 +15,37 @@ export class CheckYourAnswersPostController extends PostController<AnyObject> {
   }
 
   public async post(req: AppRequest<AnyObject>, res: Response, next: NextFunction): Promise<void> {
-    await super.post(req, res, next, CITIZEN_SUBMIT);
+    const { ...formData } = this.form.getParsedBody(req.body);
+
+    if (formData.applicationType === ApplicationType.JOINT_APPLICATION) {
+      await this.sendToApplicant2ForReview(req, res, formData);
+    } else {
+      await super.post(req, res, next, CITIZEN_SUBMIT);
+    }
+  }
+
+  public async sendToApplicant2ForReview(
+    req: AppRequest<AnyObject>,
+    res: Response,
+    formData: Partial<Case>
+  ): Promise<void> {
+    Object.assign(req.session.userCase, formData);
+    this.form.setFormState(req.session.userCase);
+    req.session.errors = this.form.getErrors(formData);
+
+    try {
+      req.session.userCase = await this.save(req, formData, CITIZEN_INVITE_APPLICANT_2);
+    } catch (err) {
+      // do nothing
+    }
+
+    const nextUrl = req.session.errors.length > 0 ? req.url : getNextStepUrl(req, req.session.userCase);
+
+    req.session.save(err => {
+      if (err) {
+        throw err;
+      }
+      res.redirect(nextUrl);
+    });
   }
 }
