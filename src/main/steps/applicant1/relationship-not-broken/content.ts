c2ae1fbf881522ce1ffe8f559461b68b53d1dbf9
velimd <duzgunce.vd@gmail.com>
2021-06-23 17:09:53 +0100

    implementation of NFDIV-762

--- src/main/steps/applicant1/relationship-not-broken/content.ts
+++ src/main/steps/applicant1/relationship-not-broken/content.ts
@@ -12,7 +12,7 @@ const cy: typeof en = ({ isDivorce }) => ({
   title: `Ni allwch wneud cais i ${isDivorce ? 'gael ysgariad' : 'ddod â’ch partneriaeth sifil i ben'}`,
   line1: `Rhaid bod eich ${isDivorce ? 'priodas' : 'perthynas'} wedi chwalu'n gyfan gwbl i chi allu ${
     isDivorce ? 'cael ysgariad' : "dod â'ch partneriaeth sifil i ben"
-  }. Dyma yw'r gyfraith yng Nghymru a Lloegr.  `,
+  }. Dyma yw'r gyfraith yng Nghymru a Lloegr.`,
   line2: `Os nad ydych yn siŵr am ${
     isDivorce ? 'gael ysgariad' : "ddod â'ch partneriaeth sifil i ben"
   }, efallai yr hoffech ystyried cael cyngor ynghylch eich perthynas neu gwnsela. Mae hwn ar gael gan therapyddion preifat ac elusennau fel <a href="https://www.relate.org.uk" class="govuk-link" target="_blank">Relate</a>.`,
