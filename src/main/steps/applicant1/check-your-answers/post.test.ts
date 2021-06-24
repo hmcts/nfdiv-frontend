7925f6ca09156d4f1d0d3862bd64b3da8a2553ed
Velican <31857663+velimd@users.noreply.github.com>
2021-06-24 16:32:01 +0100

    Folder restructing + NFDIV-762 (#760)
    * moving steps to applicant1
    
    * clean up
    
    * implementation of NFDIV-762
    
    * clean up
    
    * clean up (2)
    
    * clean up (2)

---
+++ src/main/steps/applicant1/check-your-answers/post.test.ts
@@ -0,0 +1,49 @@
+import { mockRequest } from '../../../../test/unit/utils/mockRequest';
+import { mockResponse } from '../../../../test/unit/utils/mockResponse';
+import { Checkbox } from '../../../app/case/case';
+import { ApplicationType, CITIZEN_INVITE_APPLICANT_2, CITIZEN_SUBMIT } from '../../../app/case/definition';
+import { Form } from '../../../app/form/Form';
+
+import CheckYourAnswersPostController from './post';
+
+describe('CheckYourAnswersPostController', () => {
+  it('triggers CITIZEN_SUBMIT when sole application', async () => {
+    const body = {
+      applicationType: ApplicationType.SOLE_APPLICATION,
+      iConfirmPrayer: Checkbox.Checked,
+      iBelieveApplicationIsTrue: Checkbox.Checked,
+    };
+    const mockForm = {
+      setFormState: jest.fn(),
+      getErrors: () => [],
+      getParsedBody: () => body,
+    } as unknown as Form;
+    const checkYourAnswerPostController = new CheckYourAnswersPostController(mockForm);
+
+    const req = mockRequest({ body });
+    const res = mockResponse();
+    await checkYourAnswerPostController.post(req, res);
+
+    expect(req.locals.api.triggerEvent).toHaveBeenCalledWith('1234', body, CITIZEN_SUBMIT);
+  });
+
+  it('triggers CITIZEN_INVITE_APPLICANT_2 when join application', async () => {
+    const body = {
+      applicationType: ApplicationType.JOINT_APPLICATION,
+      iConfirmPrayer: Checkbox.Checked,
+      iBelieveApplicationIsTrue: Checkbox.Checked,
+    };
+    const mockForm = {
+      setFormState: jest.fn(),
+      getErrors: () => [],
+      getParsedBody: () => body,
+    } as unknown as Form;
+    const checkYourAnswerPostController = new CheckYourAnswersPostController(mockForm);
+
+    const req = mockRequest({ body });
+    const res = mockResponse();
+    await checkYourAnswerPostController.post(req, res);
+
+    expect(req.locals.api.triggerEvent).toHaveBeenCalledWith('1234', body, CITIZEN_INVITE_APPLICANT_2);
+  });
+});
--- src/main/steps/applicant2Sequence.ts
+++ src/main/steps/applicant2Sequence.ts
@@ -1,13 +1,38 @@
-import { Step } from './applicant1Sequence';
-import { HAS_RELATIONSHIP_BROKEN_APPLICANT2, HOME_URL, YOU_NEED_TO_REVIEW_YOUR_APPLICATION } from './urls';
+import { YesOrNo } from '../app/case/definition';
 
-export const applicant2Sequence: Step[] = [
+import { Step } from './applicant1Sequence';
+import {
+  APPLICANT_2,
+  HAS_RELATIONSHIP_BROKEN_URL,
+  HOME_URL,
+  NOT_CONFIRMED_JOINT_APPLICATION,
+  RELATIONSHIP_NOT_BROKEN_URL,
+  YOU_NEED_TO_REVIEW_YOUR_APPLICATION,
+} from './urls';
+
+const sequences: Step[] = [
   {
     url: YOU_NEED_TO_REVIEW_YOUR_APPLICATION,
-    getNextStep: () => HAS_RELATIONSHIP_BROKEN_APPLICANT2,
+    getNextStep: () => HAS_RELATIONSHIP_BROKEN_URL,
   },
   {
-    url: HAS_RELATIONSHIP_BROKEN_APPLICANT2,
-    getNextStep: () => HOME_URL,
+    url: HAS_RELATIONSHIP_BROKEN_URL,
+    getNextStep: data => (data.screenHasApplicant2UnionBroken === YesOrNo.NO ? RELATIONSHIP_NOT_BROKEN_URL : HOME_URL),
+  },
+  {
+    url: RELATIONSHIP_NOT_BROKEN_URL,
+    getNextStep: () => NOT_CONFIRMED_JOINT_APPLICATION,
+  },
+  {
+    url: NOT_CONFIRMED_JOINT_APPLICATION,
+    getNextStep: () => RELATIONSHIP_NOT_BROKEN_URL,
   },
 ];
+
+export const applicant2Sequence = ((): Step[] => {
+  return sequences.map(sequence => ({
+    ...sequence,
+    url: `${APPLICANT_2}${sequence.url}`,
+    getNextStep: data => `${APPLICANT_2}${sequence.getNextStep(data)}`,
+  }));
+})();
