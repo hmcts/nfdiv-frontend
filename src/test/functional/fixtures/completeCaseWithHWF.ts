715b8bef24cec6b42bdc75179d9b8393831b557c
Tom Elliott <tomelliott@Toms-MBP.home>
2021-07-02 13:54:46 +0100

    NFDIV-758: Update fixture to use joint application type

--- src/test/functional/fixtures/completeCaseWithHWF.ts
+++ src/test/functional/fixtures/completeCaseWithHWF.ts
@@ -1,10 +1,11 @@
-import { YesOrNo } from '../../../main/app/case/definition';
+import { ApplicationType, YesOrNo } from '../../../main/app/case/definition';
 import { BrowserCase } from '../../steps/common';
 
 import { completeCase } from './completeCase';
 
 export const completeCaseWithHWF: Partial<BrowserCase> = {
   ...completeCase,
+  applicationType: ApplicationType.JOINT_APPLICATION,
   applicant1HelpPayingNeeded: YesOrNo.YES,
   applicant1AlreadyAppliedForHelpPaying: YesOrNo.YES,
   applicant1HelpWithFeesRefNo: 'HWF-ABC-123',
