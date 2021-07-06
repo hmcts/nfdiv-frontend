a7a17e1688c8fa6d41c98d64d8c137b1db6ab3f4
velimd <duzgunce.vd@gmail.com>
2021-07-06 14:37:12 +0100

    include form for save and sign out (post controller)

--- src/main/steps/applicant2/how-to-apply-financial-order/content.ts
+++ src/main/steps/applicant2/how-to-apply-financial-order/content.ts
@@ -1,5 +1,13 @@
 import { TranslationFn } from '../../../app/controller/GetController';
-import { generateContent as applicant1GenerateContent } from '../../applicant1/how-to-apply-financial-order/content';
+import { FormContent } from '../../../app/form/Form';
+import {
+  form as applicant1Form,
+  generateContent as applicant1GenerateContent,
+} from '../../applicant1/how-to-apply-financial-order/content';
+
+export const form: FormContent = {
+  ...applicant1Form,
+};
 
 export const generateContent: TranslationFn = content => {
   return applicant1GenerateContent(content);
