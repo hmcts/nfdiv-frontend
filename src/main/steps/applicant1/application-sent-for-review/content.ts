1e856bbb01ca428619232d9cb638850a117f27d6
Tom Elliott <tomxelliott@hotmail.co.uk>
2021-07-13 14:47:29 +0100

    NFDIV-964: Only use dayjs() when dueDate has not been created in B/E (#863)
    Co-authored-by: Tom Elliott <tomelliott@Toms-MBP.home>

--- src/main/steps/applicant1/application-sent-for-review/content.ts
+++ src/main/steps/applicant1/application-sent-for-review/content.ts
@@ -6,11 +6,11 @@ import type { CommonContent } from '../../common/common.content';
 
 dayjs.extend(advancedFormat);
 
-const en = ({ partner }: CommonContent) => ({
+const en = ({ partner, formState }: CommonContent) => ({
   title: `Your answers have been sent to your ${partner} to review`,
   sentTo: `Your answers have been sent to your ${partner} at the following email address:`,
   theyShouldReviewBy: 'They should review them and provide some of their own information by',
-  reviewDate: dayjs().add(2, 'weeks').format('MMMM Do YYYY'),
+  reviewDate: formState?.dueDate || dayjs().add(2, 'weeks').format('MMMM Do YYYY'),
   line3:
     'You will receive a notification when they have reviewed. If they do not review then you will be told what you can do to progress the application.',
 });
