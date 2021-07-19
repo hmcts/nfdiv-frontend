9e891e4bd6932da8d559b9853fcf75043cae55ba
Velican <31857663+velimd@users.noreply.github.com>
2021-07-19 14:56:30 +0100

    moving access code from applicant2Sequence (#891)
    * moving access code from applicant2Sequence
    
    * fix e2e test
    
    * redirect to you need to review your joint application
    
    * fix unit test
    
    * redirect to include applicant2
    
    * fix unit test to set to correct redirect

--- src/main/steps/applicant2/enter-your-access-code/post.ts
+++ src/main/steps/applicant2/enter-your-access-code/post.ts
@@ -1,3 +1,58 @@
-import { AccessCodePostController } from '../../../app/access-code/AccessCodePostController';
+import autobind from 'autobind-decorator';
+import { Response } from 'express';
 
-export default AccessCodePostController;
+import { getSystemUser } from '../../../app/auth/user/oidc';
+import { getCaseApi } from '../../../app/case/CaseApi';
+import { SYSTEM_LINK_APPLICANT_2 } from '../../../app/case/definition';
+import { AppRequest } from '../../../app/controller/AppRequest';
+import { AnyObject } from '../../../app/controller/PostController';
+import { Form } from '../../../app/form/Form';
+import { APPLICANT_2, YOU_NEED_TO_REVIEW_YOUR_APPLICATION } from '../../urls';
+
+@autobind
+export class AccessCodePostController {
+  constructor(protected readonly form: Form) {}
+
+  public async post(req: AppRequest<AnyObject>, res: Response): Promise<void> {
+    const caseworkerUser = await getSystemUser();
+    req.locals.api = getCaseApi(caseworkerUser, req.locals.logger);
+
+    const { saveAndSignOut, saveBeforeSessionTimeout, _csrf, ...formData } = this.form.getParsedBody(req.body);
+
+    formData.respondentUserId = req.session.user.id;
+    req.session.errors = this.form.getErrors(formData);
+    const caseReference = formData.caseReference?.replace(/-/g, '');
+
+    try {
+      const caseData = await req.locals.api.getCaseById(caseReference as string);
+
+      if (caseData.accessCode !== formData.accessCode) {
+        req.session.errors.push({ errorType: 'invalidAccessCode', propertyName: 'accessCode' });
+      }
+    } catch (err) {
+      req.session.errors.push({ errorType: 'invalidReference', propertyName: 'caseReference' });
+    }
+
+    if (req.session.errors.length === 0) {
+      try {
+        req.session.userCase = await req.locals.api.triggerEvent(
+          caseReference as string,
+          formData,
+          SYSTEM_LINK_APPLICANT_2
+        );
+      } catch (err) {
+        req.locals.logger.error('Error linking applicant 2 to joint application', err);
+        req.session.errors.push({ errorType: 'errorSaving', propertyName: '*' });
+      }
+    }
+
+    const nextUrl = req.session.errors.length > 0 ? req.url : `${APPLICANT_2}${YOU_NEED_TO_REVIEW_YOUR_APPLICATION}`;
+
+    req.session.save(err => {
+      if (err) {
+        throw err;
+      }
+      res.redirect(nextUrl);
+    });
+  }
+}
