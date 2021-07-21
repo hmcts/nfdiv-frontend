5f447f490b9e1317da41411ce54c617085f42554
Lara <laratollington@gmail.com>
2021-07-21 17:15:14 +0100

    Update error messages

--- src/main/steps/applicant2/how-did-you-change-your-name/content.ts
+++ src/main/steps/applicant2/how-did-you-change-your-name/content.ts
@@ -3,17 +3,15 @@ import { TranslationFn } from '../../../app/controller/GetController';
 import { FormContent } from '../../../app/form/Form';
 import { isFieldFilledIn } from '../../../app/form/validation';
 import { generateContent as applicant1GenerateContent } from '../../applicant1/how-did-you-change-your-name/content';
-import { CommonContent } from '../../common/common.content';
 
-const labels = ({ required }: CommonContent) => {
+const labels = applicant1Content => {
   return {
     errors: {
-      applicant2ChangedNameHow: {
-        required,
+      applicant2NameChangedHow: {
+        ...applicant1Content.errors.applicant1NameChangedHow,
       },
       applicant2ChangedNameHowAnotherWay: {
-        required:
-          'You have said you changed your name another way but not provided details. Provide details of how you changed your name.',
+        ...applicant1Content.errors.applicant2ChangedNameHowAnotherWay,
       },
     },
   };
@@ -63,7 +61,7 @@ export const generateContent: TranslationFn = content => {
   const applicant1Content = applicant1GenerateContent(content);
   return {
     ...applicant1Content,
-    ...labels(content),
+    ...labels(applicant1Content),
     form,
   };
 };
