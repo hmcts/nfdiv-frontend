30d87c20d2342cc38cc187d50a4fd77b36abe79a
Lara <laratollington@gmail.com>
2021-08-03 17:26:50 +0100

    Fix jurisdiction on CYA page

--- src/main/steps/applicant1/connection-summary/content.ts
+++ src/main/steps/applicant1/connection-summary/content.ts
@@ -3,54 +3,89 @@ import { TranslationFn } from '../../../app/controller/GetController';
 import { FormContent } from '../../../app/form/Form';
 import { CommonContent } from '../../common/common.content';
 
-const en = ({ isDivorce, partner, formState, habitualResidentHelpText1, habitualResidentHelpText2 }: CommonContent) => {
-  const enHabitualResident = {
-    helpText1: 'Habitual residence',
-    helpText2:
-      'If your lives are mainly based in England or Wales then you’re what is legally known as ‘habitually resident’.',
-    helpText3: habitualResidentHelpText1,
-    helpText4: habitualResidentHelpText2,
-  };
-  const enDomicile = {
-    helpText5: 'Domicile',
-    helpText6:
-      'Your domicile is usually the place in which you were born, regard as your permanent home and to which you have the closest ties.',
-    helpText7:
-      'However, domicile can be more complex, for example, if you or your parents have moved countries in the past.',
-    helpText8: "When you’re born, you acquire a 'domicile of origin'. This is usually:",
-    helpText9:
-      '<ul class="govuk-list govuk-list--bullet"><li class="govuk-list govuk-list--bullet">the country your father was domiciled in if your parents were married </li> <li class="govuk-list govuk-list--bullet">the country your mother was domiciled in if your parents were unmarried, or your father had died before you were born</li></ul>',
-    helpText10:
-      "If you leave your domicile of origin and settle in another country as an adult, the new country may become your 'domicile of choice'.",
-    helpText11: 'If you’re not sure about your domicile, you should get legal advice.',
+export const jurisdictionMoreDetailsContent: (formState) => { connectedToEnglandWales: string; readMore: string } =
+  formState => {
+    const resConnection = enContainsHabitualResConnection(formState.connections);
+    const domConnection = enContainsDomConnection(formState.connections);
+
+    const resTitleReplacement = domConnection ? '<strong>Habitual residence</strong><br><br>' : '';
+    const domTitleReplacement = resConnection ? '<strong>Domicile</strong><br><br>' : '';
+    const bothTexts = resConnection && domConnection ? '<br><br>' : '';
+
+    const resConnectionText = resConnection
+      ? Object.values(resConnection).join('<br><br>').replace('Habitual residence<br><br>', resTitleReplacement)
+      : '';
+    const domConnectionText = domConnection
+      ? Object.values(domConnection)
+          .join('<br><br>')
+          .replace('Domicile<br><br>', domTitleReplacement)
+          .replace('</ul><br><br>', '</ul>')
+          .replace('<br><ul', '<ul')
+      : '';
+
+    const totalText = (resConnection ? resConnectionText : '') + bothTexts + domConnectionText;
+    const readMoreText =
+      resConnection && domConnection
+        ? 'Read more about your connections'
+        : resConnection
+        ? 'Read more about habitual residence'
+        : 'Read more about domicile';
+
+    return { connectedToEnglandWales: totalText, readMore: readMoreText };
   };
 
-  const enContainsHabitualResConnection = (
-    connections: JurisdictionConnections[] | undefined
-  ): typeof enHabitualResident | undefined => {
-    if (
-      connections &&
-      (connections.includes(JurisdictionConnections.APP_1_APP_2_LAST_RESIDENT) ||
-        connections.includes(JurisdictionConnections.APP_2_RESIDENT) ||
-        connections.includes(JurisdictionConnections.APP_1_RESIDENT_SIX_MONTHS))
-    ) {
-      return enHabitualResident;
-    }
-  };
+const enHabitualResident = {
+  helpText1: 'Habitual residence',
+  helpText2:
+    'If your lives are mainly based in England or Wales then you’re what is legally known as ‘habitually resident’.',
+  helpText3:
+    'This may include working, owning property, having children in school, and your main family life taking place in England or Wales.',
+  helpText4:
+    'The examples above aren’t a complete list of what makes up habitual residence, and just because some of them apply to you doesn’t mean you’re habitually resident. If you’re not sure, you should get legal advice.',
+};
+const enDomicile = {
+  helpText5: 'Domicile',
+  helpText6:
+    'Your domicile is usually the place in which you were born, regard as your permanent home and to which you have the closest ties.',
+  helpText7:
+    'However, domicile can be more complex, for example, if you or your parents have moved countries in the past.',
+  helpText8: "When you’re born, you acquire a 'domicile of origin'. This is usually:",
+  helpText9:
+    '<ul class="govuk-list govuk-list--bullet"><li class="govuk-list govuk-list--bullet">the country your father was domiciled in if your parents were married </li> <li class="govuk-list govuk-list--bullet">the country your mother was domiciled in if your parents were unmarried, or your father had died before you were born</li></ul>',
+  helpText10:
+    "If you leave your domicile of origin and settle in another country as an adult, the new country may become your 'domicile of choice'.",
+  helpText11: 'If you’re not sure about your domicile, you should get legal advice.',
+};
 
-  const enContainsDomConnection = (
-    connections: JurisdictionConnections[] | undefined
-  ): typeof enDomicile | undefined => {
-    if (
-      connections &&
-      (connections.includes(JurisdictionConnections.APP_1_APP_2_DOMICILED) ||
-        connections.includes(JurisdictionConnections.APP_1_DOMICILED) ||
-        connections.includes(JurisdictionConnections.APP_2_DOMICILED))
-    ) {
-      return enDomicile;
-    }
-  };
+const enContainsHabitualResConnection = (
+  connections: JurisdictionConnections[] | undefined
+): typeof enHabitualResident | undefined => {
+  if (
+    connections &&
+    (connections.includes(JurisdictionConnections.APP_1_APP_2_LAST_RESIDENT) ||
+      connections.includes(JurisdictionConnections.APP_2_RESIDENT) ||
+      connections.includes(JurisdictionConnections.APP_1_RESIDENT_SIX_MONTHS) ||
+      connections.includes(JurisdictionConnections.APP_1_APP_2_RESIDENT) ||
+      connections.includes(JurisdictionConnections.APP_1_RESIDENT_JOINT) ||
+      connections.includes(JurisdictionConnections.RESIDUAL_JURISDICTION) ||
+      connections.includes(JurisdictionConnections.APP_1_RESIDENT_TWELVE_MONTHS))
+  ) {
+    return enHabitualResident;
+  }
+};
 
+const enContainsDomConnection = (connections: JurisdictionConnections[] | undefined): typeof enDomicile | undefined => {
+  if (
+    connections &&
+    (connections.includes(JurisdictionConnections.APP_1_APP_2_DOMICILED) ||
+      connections.includes(JurisdictionConnections.APP_1_DOMICILED) ||
+      connections.includes(JurisdictionConnections.APP_2_DOMICILED))
+  ) {
+    return enDomicile;
+  }
+};
+
+const en = ({ isDivorce, partner, formState }: CommonContent) => {
   return {
     title: `You can use English or Welsh courts to ${isDivorce ? 'get a divorce' : 'end your civil partnership'}`,
     line1: `Your answers indicate that you can apply ${
@@ -77,7 +112,13 @@ const en = ({ isDivorce, partner, formState, habitualResidentHelpText1, habitual
 const cy: typeof en = en;
 
 export const form: FormContent = {
-  fields: {},
+  fields: {
+    connections: {
+      type: 'hidden',
+      label: l => l.title,
+      labelHidden: true,
+    },
+  },
   submit: {
     text: l => l.continue,
   },
