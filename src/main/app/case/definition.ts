9bd8973085132a76c22ff654737f167074a39917
Mark Butler <mark.butler@solirius.com>
2021-09-07 15:57:59 +0100

    Import the index.ts file from the nfdiv-case-api NFDIV-1144 ticket changes to bring the Jurisdiction and LegalConnections classes in sync.

--- src/main/app/case/definition.ts
+++ src/main/app/case/definition.ts
@@ -1,6 +1,6 @@
 /* tslint:disable */
 /* eslint-disable */
-// Generated using typescript-generator version 2.32.889 on 2021-08-25 13:37:50.
+// Generated using typescript-generator version 2.32.889 on 2021-09-07 15:37:15.
 
 export interface Address {
   AddressLine1: string;
@@ -161,7 +161,6 @@ export interface Application {
   jurisdictionResidualEligible: YesOrNo;
   jurisdictionBothLastHabituallyResident: YesOrNo;
   jurisdictionConnections: JurisdictionConnections[];
-  jurisdictionLegalConnections: LegalConnections[];
   applicant1HWFReferenceNumber: string;
   applicant1HWFNeedHelp: YesOrNo;
   applicant1HWFAppliedForFees: YesOrNo;
@@ -200,6 +199,7 @@ export interface Application {
   applicant2ConfirmApplicant1Information: YesOrNo;
   applicant2ExplainsApplicant1IncorrectInformation: string;
   issueDate: DateAsString;
+  createdDate: DateAsString;
   rejectReason: RejectReason;
   previousState: State;
   applicationPayments: ListValue<Payment>[];
@@ -297,7 +297,6 @@ export interface CaseData {
   jurisdictionResidualEligible: YesOrNo;
   jurisdictionBothLastHabituallyResident: YesOrNo;
   jurisdictionConnections: JurisdictionConnections[];
-  jurisdictionLegalConnections: LegalConnections[];
   applicant1HWFReferenceNumber: string;
   applicant1HWFNeedHelp: YesOrNo;
   applicant1HWFAppliedForFees: YesOrNo;
@@ -336,6 +335,7 @@ export interface CaseData {
   applicant2ConfirmApplicant1Information: YesOrNo;
   applicant2ExplainsApplicant1IncorrectInformation: string;
   issueDate: DateAsString;
+  createdDate: DateAsString;
   rejectReason: RejectReason;
   previousState: State;
   applicationPayments: ListValue<Payment>[];
@@ -468,7 +468,6 @@ export interface Jurisdiction {
   ResidualEligible: YesOrNo;
   BothLastHabituallyResident: YesOrNo;
   Connections: JurisdictionConnections[];
-  LegalConnections: LegalConnections[];
 }
 
 export interface LabelContent {
@@ -764,75 +763,63 @@ export const enum GeneralReferralType {
 
 /**
  * Values:
- * - `A` - The applicant and the respondent are habitually resident in England and Wales
- * - `B` - The applicant and respondent were last habitually resident in England and Wales and one of them still resides there
- * - `C` - The respondent is habitually resident in England and Wales
- * - `D` - The applicant is habitually resident in England and Wales and has resided there for at least a year immediately prior to the presentation of the application
- * - `E` - The applicant is domiciled and habitually resident in England and Wales and has resided there for at least six months immediately prior to the application
- * - `F` - The applicant and respondent are both domiciled in England and Wales
- * - `G` - The applicant is domiciled in England and Wales
- * - `H` - The respondent is domiciled in England and Wales
- * - `I` - The Applicant and Respondent registered as civil partners of each other in England or Wales or, in the case of a same sex couple, married each other under the law of England and Wales and it would be in the interests of justice for the court to assume jurisdiction in this case (same sex married couples only)
- * - `J` - The applicant habitually resides in England and Wales
+ * - `J` - APP_1_RESIDENT_JOINT
+ * - `A` - APP_1_APP_2_RESIDENT
+ * - `B` - APP_1_APP_2_LAST_RESIDENT
+ * - `C` - APP_2_RESIDENT
+ * - `D` - APP_1_RESIDENT_TWELVE_MONTHS
+ * - `E` - APP_1_RESIDENT_SIX_MONTHS
+ * - `F` - APP_1_APP_2_DOMICILED
+ * - `G` - APP_1_DOMICILED
+ * - `H` - APP_2_DOMICILED
+ * - `I` - RESIDUAL_JURISDICTION
  */
 export const enum JurisdictionConnections {
   /**
-   * The applicant and the respondent are habitually resident in England and Wales
+   * APP_1_RESIDENT_JOINT
+   */
+  APP_1_RESIDENT_JOINT = 'J',
+  /**
+   * APP_1_APP_2_RESIDENT
    */
   APP_1_APP_2_RESIDENT = 'A',
   /**
-   * The applicant and respondent were last habitually resident in England and Wales and one of them still resides there
+   * APP_1_APP_2_LAST_RESIDENT
    */
   APP_1_APP_2_LAST_RESIDENT = 'B',
   /**
-   * The respondent is habitually resident in England and Wales
+   * APP_2_RESIDENT
    */
   APP_2_RESIDENT = 'C',
   /**
-   * The applicant is habitually resident in England and Wales and has resided there for at least a year immediately prior to the presentation of the application
+   * APP_1_RESIDENT_TWELVE_MONTHS
    */
   APP_1_RESIDENT_TWELVE_MONTHS = 'D',
   /**
-   * The applicant is domiciled and habitually resident in England and Wales and has resided there for at least six months immediately prior to the application
+   * APP_1_RESIDENT_SIX_MONTHS
    */
   APP_1_RESIDENT_SIX_MONTHS = 'E',
   /**
-   * The applicant and respondent are both domiciled in England and Wales
+   * APP_1_APP_2_DOMICILED
    */
   APP_1_APP_2_DOMICILED = 'F',
   /**
-   * The applicant is domiciled in England and Wales
+   * APP_1_DOMICILED
    */
   APP_1_DOMICILED = 'G',
   /**
-   * The respondent is domiciled in England and Wales
+   * APP_2_DOMICILED
    */
   APP_2_DOMICILED = 'H',
   /**
-   * The Applicant and Respondent registered as civil partners of each other in England or Wales or, in the case of a same sex couple, married each other under the law of England and Wales and it would be in the interests of justice for the court to assume jurisdiction in this case (same sex married couples only)
+   * RESIDUAL_JURISDICTION
    */
   RESIDUAL_JURISDICTION = 'I',
-  /**
-   * The applicant habitually resides in England and Wales
-   */
-  APP_1_RESIDENT_JOINT = 'J',
 }
 
 export const enum LanguagePreference {
   ENGLISH = 'ENGLISH',
   WELSH = 'WELSH',
-}
-
-export const enum LegalConnections {
-  APPLICANT_RESPONDENT_RESIDENT = 'A',
-  APPLICANT_RESPONDENT_ONE_RESIDENT = 'B',
-  RESPONDENT_RESIDENT = 'C',
-  APPLICANT_RESIDENT = 'D',
-  APPLICANT_DOMICILED_RESIDENT = 'E',
-  APPLICANT_RESPONDENT_DOMICILED = 'F',
-  APPLICANT_DOMICILED = 'G',
-  RESPONDENT_DOMICILED = 'H',
-  COURTS_RESIDUAL_JURISDICTION = 'I',
 }
 
 export const enum LegalProceedingsRelated {
