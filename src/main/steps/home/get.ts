3c4a5831c0b626860fbd0757115ec774c0c45f18
Lara <laratollington@gmail.com>
2021-09-07 11:13:22 +0100

    Fix app2 home get controller and clean up

--- src/main/steps/home/get.ts
+++ src/main/steps/home/get.ts
@@ -1,5 +1,6 @@
 import { Response } from 'express';
 
+import { Case } from '../../app/case/case';
 import { State, YesOrNo } from '../../app/case/definition';
 import { AppRequest } from '../../app/controller/AppRequest';
 import { Form } from '../../app/form/Form';
@@ -12,6 +13,7 @@ import {
   HUB_1,
   SENT_TO_APPLICANT2_FOR_REVIEW,
   YOUR_DETAILS_URL,
+  YOUR_SPOUSE_NEEDS_TO_CONFIRM_YOUR_JOINT_APPLICATION,
   YOU_NEED_TO_REVIEW_YOUR_APPLICATION,
 } from '../../steps/urls';
 import { form as applicant1FirstQuestionForm } from '../applicant1/your-details/content';
@@ -28,40 +30,64 @@ export class HomeGetController {
     );
     const isFirstQuestionComplete = firstQuestionForm.getErrors(req.session.userCase).length === 0;
 
-    if (req.session.userCase.state === State.Holding) {
-      res.redirect(HUB_1);
-    }
-
     if (req.session.isApplicant2) {
-      return res.redirect(
-        isFirstQuestionComplete
-          ? `${APPLICANT_2}${CHECK_ANSWERS_URL}`
-          : `${APPLICANT_2}${YOU_NEED_TO_REVIEW_YOUR_APPLICATION}`
+      res.redirect(applicant2RedirectPageSwitch(req.session.userCase.state, isFirstQuestionComplete));
+    } else {
+      res.redirect(
+        applicant1RedirectPageSwitch(req.session.userCase.state, req.session.userCase, isFirstQuestionComplete)
       );
-    }
-
-    switch (req.session.userCase.state) {
-      case State.AwaitingApplicant1Response: {
-        req.session.userCase.applicant2ScreenHasUnionBroken === YesOrNo.NO
-          ? res.redirect(APPLICATION_ENDED)
-          : res.redirect(CHECK_ANSWERS_URL);
-        break;
-      }
-      case State.AwaitingApplicant2Response: {
-        res.redirect(SENT_TO_APPLICANT2_FOR_REVIEW);
-        break;
-      }
-      case State.Applicant2Approved: {
-        res.redirect(CONFIRM_JOINT_APPLICATION);
-        break;
-      }
-      case State.Submitted: {
-        res.redirect(APPLICATION_SUBMITTED);
-        break;
-      }
-      default: {
-        res.redirect(isFirstQuestionComplete ? CHECK_ANSWERS_URL : YOUR_DETAILS_URL);
-      }
     }
   }
 }
+
+const applicant1RedirectPageSwitch = (caseState: State, userCase: Partial<Case>, isFirstQuestionComplete: boolean) => {
+  let redirectLink;
+
+  switch (caseState) {
+    case State.AwaitingApplicant1Response: {
+      redirectLink = userCase.applicant2ScreenHasUnionBroken === YesOrNo.NO ? APPLICATION_ENDED : CHECK_ANSWERS_URL;
+      break;
+    }
+    case State.AwaitingApplicant2Response: {
+      redirectLink = SENT_TO_APPLICANT2_FOR_REVIEW;
+      break;
+    }
+    case State.Applicant2Approved: {
+      redirectLink = CONFIRM_JOINT_APPLICATION;
+      break;
+    }
+    case State.Submitted: {
+      redirectLink = APPLICATION_SUBMITTED;
+      break;
+    }
+    case State.Holding: {
+      redirectLink = HUB_1;
+      break;
+    }
+    default: {
+      redirectLink = isFirstQuestionComplete ? CHECK_ANSWERS_URL : YOUR_DETAILS_URL;
+    }
+  }
+  return redirectLink;
+};
+
+const applicant2RedirectPageSwitch = (caseState: State, isFirstQuestionComplete: boolean) => {
+  let redirectLink;
+
+  switch (caseState) {
+    case State.Holding: {
+      redirectLink = `${APPLICANT_2}${HUB_1}`;
+      break;
+    }
+    case State.Applicant2Approved: {
+      redirectLink = `${APPLICANT_2}${YOUR_SPOUSE_NEEDS_TO_CONFIRM_YOUR_JOINT_APPLICATION}`;
+      break;
+    }
+    default: {
+      redirectLink = isFirstQuestionComplete
+        ? `${APPLICANT_2}${CHECK_ANSWERS_URL}`
+        : `${APPLICANT_2}${YOU_NEED_TO_REVIEW_YOUR_APPLICATION}`;
+    }
+  }
+  return redirectLink;
+};
