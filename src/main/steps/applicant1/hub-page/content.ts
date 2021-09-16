af0686d4154535c502b540ac3df66dc8a881d9e4
Velican <31857663+velimd@users.noreply.github.com>
2021-09-16 15:03:40 +0100

    Nfdiv 1010 clean up (#1165)
    * clean up
    
    * clean up

--- src/main/steps/applicant1/hub-page/content.ts
+++ src/main/steps/applicant1/hub-page/content.ts
@@ -5,7 +5,7 @@ import type { CommonContent } from '../../common/common.content';
 import { REVIEW_CONTACT_DETAILS } from '../../urls';
 
 const en = ({ isDivorce, formState, partner, referenceNumber }: CommonContent) => ({
-  title: formState?.applicant1FullNameOnCertificate + ' & ' + formState?.applicant2FullNameOnCertificate,
+  title: `${formState?.applicant1FullNameOnCertificate} & ${formState?.applicant2FullNameOnCertificate}`,
   referenceNumber: `Reference Number: ${referenceNumber}`,
   applicationSubmitted: 'Application submitted',
   conditionalOrderApplication: 'Conditional order application',
@@ -39,14 +39,14 @@ const en = ({ isDivorce, formState, partner, referenceNumber }: CommonContent) =
       You will receive an email to remind you.`,
   },
   line3:
-    ' <a class="govuk-link" href="https://www.gov.uk/money-property-when-relationship-ends" target="_blank">Find out about dividing money and property</a>',
-  line4: ` <a class="govuk-link" href="/downloads/${
+    '<a class="govuk-link" href="https://www.gov.uk/money-property-when-relationship-ends" target="_blank">Find out about dividing money and property</a>',
+  line4: `<a class="govuk-link" href="/downloads/${
     isDivorce ? 'divorce-application' : 'application-to-end-civil-partnership'
   }"
   download="${isDivorce ? 'Divorce-application' : 'Civil-partnership-application'}">View the ${
     isDivorce ? 'divorce application' : 'application to end your civil partnership'
   } (PDF)</a>`,
-  line5: ` <a class="govuk-link" href="${REVIEW_CONTACT_DETAILS}">View or update my contact details</a>`,
+  line5: `<a class="govuk-link" href="${REVIEW_CONTACT_DETAILS}">View or update my contact details</a>`,
   email: `<strong>Email</strong><br> <a class="govuk-link" href="mailto:${
     isDivorce ? 'contactdivorce@justice.gov.uk' : 'civilpartnership.case@justice.gov.uk'
   }">${isDivorce ? 'contactdivorce@justice.gov.uk' : 'civilpartnership.case@justice.gov.uk'}</a>`,
@@ -54,13 +54,13 @@ const en = ({ isDivorce, formState, partner, referenceNumber }: CommonContent) =
   confirmReceipt: 'Confirm receipt',
   subHeading4: 'Getting help',
   telephone: '<strong>Phone</strong></br> 0300 303 0642</br> (Monday to Friday, 8am to 8PM, Saturday 8AM to 2PM)',
-  post:
-    '<strong>Post</strong></br>' +
-    'Courts and Tribunals Service Centre</br>' +
-    'Digital Divorce</br>' +
-    'PO Box 12706</br>' +
-    'Harlow</br>' +
-    'CM20 9QT',
+  post: `
+    <strong>Post</strong></br>
+    Courts and Tribunals Service Centre</br>
+    Digital Divorce</br>
+    PO Box 12706</br>
+    Harlow</br>
+    CM20 9QT`,
 });
 
 // @TODO translations
