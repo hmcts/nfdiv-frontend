98d40c679a9977d8e67176334f12501dc279c501
Lara <laratollington@gmail.com>
2021-09-22 15:29:19 +0100

    update after dev review

--- src/main/steps/applicant2/hub-page/content.ts
+++ src/main/steps/applicant2/hub-page/content.ts
@@ -1,13 +1,10 @@
 import { TranslationFn } from '../../../app/controller/GetController';
-import { FormContent } from '../../../app/form/Form';
 import {
   form as applicant1Form,
   generateContent as applicant1GenerateContent,
 } from '../../applicant1/hub-page/content';
 
-export const form: FormContent = {
-  ...applicant1Form,
-};
+export const form = applicant1Form;
 
 export const generateContent: TranslationFn = content => {
   return applicant1GenerateContent(content);
