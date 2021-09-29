25a1ff0fd09e602868ce73b3ce37a2322d210fda
Linus Norton <linusnorton@gmail.com>
2021-09-29 13:31:57 +0100

    Make fees configurable (#1228)

--- src/main/steps/applicant1/how-to-apply-to-serve/content.ts
+++ src/main/steps/applicant1/how-to-apply-to-serve/content.ts
@@ -1,3 +1,5 @@
+import config from 'config';
+
 import { TranslationFn } from '../../../app/controller/GetController';
 import { FormContent } from '../../../app/form/Form';
 import { CommonContent } from '../../common/common.content';
@@ -6,7 +8,9 @@ const en = ({ isDivorce, divorce, endingCivilPartnership, formState }: CommonCon
   title: 'How to apply to serve (deliver) the papers another way',
   line1: `You have to make a separate application to serve the ${
     isDivorce ? divorce : endingCivilPartnership
-  } papers another way. You will be given a link to the form after you have submitted this application. It will be reviewed by a judge and costs an additional £50.`,
+  } papers another way. You will be given a link to the form after you have submitted this application. It will be reviewed by a judge and costs an additional ${config.get(
+    'fees.alternativeService'
+  )}.`,
   line3: `For example, you could apply to have the papers served (delivered) by ${
     formState?.applicant2EmailAddress ? 'the email address you entered earlier' : 'email, text message or social media'
   }.`,
@@ -17,7 +21,9 @@ const cy: typeof en = ({ isDivorce, formState }: CommonContent) => ({
   title: 'Sut i wneud cais i gyflwyno (danfon) y papurau drwy ddull arall',
   line1: `Rydych wedi gwneud cais ar wahân i gyflwyno'r ${
     isDivorce ? 'papurau ysgaru' : 'papurau diweddu eich partneriaeth sifil'
-  } drwy ddull arall. Fe gewch ddolen i'r ffurfen ar ôl ichi gyflwyno'r cais hwn. Bydd yn cael ei adolygu gan farnwr ac mae cost ychwanegol o £50.`,
+  } drwy ddull arall. Fe gewch ddolen i'r ffurfen ar ôl ichi gyflwyno'r cais hwn. Bydd yn cael ei adolygu gan farnwr ac mae cost ychwanegol o ${config.get(
+    'fees.alternativeService'
+  )}.`,
   line3: `Er enghraifft, gallwch wneud cais i'r papurau gael eu cyflwyno (danfon) ${
     formState?.applicant2EmailAddress
       ? "i'r cyfeiriad e-bost wnaethoch chi ei nodi yn gynharach"
