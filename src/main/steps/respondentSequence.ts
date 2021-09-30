8079f9b578a455efd1e759d9b522ed1b8304e4de
velimd <duzgunce.vd@gmail.com>
2021-09-30 15:26:57 +0100

    implementation

--- src/main/steps/respondentSequence.ts
+++ src/main/steps/respondentSequence.ts
@@ -1,7 +1,14 @@
 import { YesOrNo } from '../app/case/definition';
 
 import { Sections, Step } from './applicant1Sequence';
-import { DETAILS_OTHER_PROCEEDINGS, HOW_THE_COURTS_WILL_CONTACT_YOU, OTHER_COURT_CASES, RESPONDENT } from './urls';
+import {
+  CHECK_ANSWERS_URL,
+  DETAILS_OTHER_PROCEEDINGS,
+  ENGLISH_OR_WELSH,
+  HOW_THE_COURTS_WILL_CONTACT_YOU,
+  OTHER_COURT_CASES,
+  RESPONDENT,
+} from './urls';
 
 const sequences: Step[] = [
   {
@@ -14,6 +21,11 @@ const sequences: Step[] = [
     url: DETAILS_OTHER_PROCEEDINGS,
     showInSection: Sections.OtherCourtCases,
     getNextStep: () => HOW_THE_COURTS_WILL_CONTACT_YOU,
+  },
+  {
+    url: ENGLISH_OR_WELSH,
+    showInSection: Sections.ContactYou,
+    getNextStep: () => CHECK_ANSWERS_URL,
   },
 ];
 
