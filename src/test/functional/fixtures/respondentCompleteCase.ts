b7cbefcd5b9ff516606faab15a25563f584bfb4b
velimd <duzgunce.vd@gmail.com>
2021-10-12 17:06:08 +0100

    e2e

---
+++ src/test/functional/fixtures/respondentCompleteCase.ts
@@ -0,0 +1,10 @@
+import { Checkbox, LanguagePreference } from '../../../main/app/case/case';
+import { YesOrNo } from '../../../main/app/case/definition';
+import { BrowserCase } from '../../steps/common';
+
+export const respondentCompleteCase: Partial<BrowserCase> = {
+  applicant2LegalProceedings: YesOrNo.NO,
+  applicant2LegalProceedingsRelated: [],
+  applicant2AgreeToReceiveEmails: Checkbox.Checked,
+  applicant2EnglishOrWelsh: LanguagePreference.English,
+};
