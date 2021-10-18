7ee72bbdb35e4bafc726ca4c6857f84f0cb9ab02
Lara <laratollington@gmail.com>
2021-10-18 12:21:51 +0100

    Add confirmDisputeApplication field to the dispute application page

--- src/main/app/case/case.ts
+++ src/main/app/case/case.ts
@@ -91,6 +91,7 @@ export const formFieldsToCaseMapping: Partial<Record<keyof Case, keyof CaseData>
   applicationFeeOrderSummary: 'applicationFeeOrderSummary',
   payments: 'applicationPayments',
   disputeApplication: 'disputeApplication',
+  confirmDisputeApplication: 'confirmDisputeApplication',
   doYouAgreeCourtHasJurisdiction: 'doYouAgreeCourtHasJurisdiction',
   reasonCourtsOfEnglandAndWalesHaveNoJurisdiction: 'reasonCourtsOfEnglandAndWalesHaveNoJurisdiction',
   inWhichCountryIsYourLifeMainlyBased: 'inWhichCountryIsYourLifeMainlyBased',
@@ -224,6 +225,7 @@ export interface Case {
   applicant2Explanation: string;
   applicant1PcqId?: string;
   disputeApplication?: YesOrNo;
+  confirmDisputeApplication?: YesOrNo;
   confirmReadPetition?: Checkbox;
   doYouAgreeCourtHasJurisdiction?: YesOrNo;
   reasonCourtsOfEnglandAndWalesHaveNoJurisdiction?: string;
