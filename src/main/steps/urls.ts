71ff1bb22a9539403ba521720b0c98cabb7bf01a
Lara <52360278+lara-mpt@users.noreply.github.com>
2021-11-01 12:13:17 +0000

    NFDIV-1191 Alternate service page (#1336)
    * Initial implementation
    
    * Make fees dynamic
    
    * fix e2e tests
    
    * update templates
    
    * move html to template
    
    * Update content.ts
    
    * update after dev review
    
    * clean up

--- src/main/steps/urls.ts
+++ src/main/steps/urls.ts
@@ -109,3 +109,5 @@ export const REVIEW_CONTACT_DETAILS: PageLink = '/check-contact-details';
 export const HOW_DO_YOU_WANT_TO_RESPOND: PageLink = '/how-do-you-want-to-respond';
 export const DISPUTING_THE_APPLICATION: PageLink = '/disputing-the-application';
 export const LEGAL_JURISDICTION_OF_THE_COURTS: PageLink = '/legal-jurisdiction-of-the-courts';
+
+export const HOW_YOU_CAN_PROCEED: PageLink = '/how-you-can-proceed';
