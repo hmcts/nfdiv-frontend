6e20f6d2e71233c478052ec6878c3b2721fa32c9
Lara <laratollington@gmail.com>
2021-11-01 13:43:23 +0000

    content update

--- src/main/steps/applicant1/how-you-can-proceed/content.ts
+++ src/main/steps/applicant1/how-you-can-proceed/content.ts
@@ -1,11 +1,15 @@
 import config from 'config';
+import dayjs from 'dayjs';
+import advancedFormat from 'dayjs/plugin/advancedFormat';
 
 import { TranslationFn } from '../../../app/controller/GetController';
 import { FormContent } from '../../../app/form/Form';
 import { CommonContent } from '../../common/common.content';
 import { generateContent as columnGenerateContent } from '../hub-page/right-column/content';
 
-const en = ({ isDivorce, marriage, civilPartnership, partner }: CommonContent) => ({
+dayjs.extend(advancedFormat);
+
+const en = ({ isDivorce, marriage, civilPartnership, partner, formState }: CommonContent) => ({
   title: `How to proceed with ${isDivorce ? 'your divorce' : 'ending your civil partnership'}`,
   line1: `The court usually needs to hear from both parties in a ${
     isDivorce ? marriage : civilPartnership
@@ -28,6 +32,7 @@ const en = ({ isDivorce, marriage, civilPartnership, partner }: CommonContent) =
   thinkPartnerChoosingNotToRespond: `I think my ${partner} is receiving the application but is choosing not to respond`,
   evidencePartnerNotResponded: `I have evidence that my ${partner} has received the application, but will not or cannot respond`,
   triedEveryWayToDeliver: "I've tried every possible way of delivering the application",
+  dueDate: `${dayjs(formState?.issueDate).add(18, 'day').format('D MMMM YYYY')}`,
 });
 
 // @TODO translations
