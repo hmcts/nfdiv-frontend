5d041c37be47a1d2328ecafbb5598a443e9af0bb
velimd <duzgunce.vd@gmail.com>
2021-11-09 15:29:33 +0000

    alternative to setting values to null.

--- src/main/app/case/to-api-format.ts
+++ src/main/app/case/to-api-format.ts
@@ -1,7 +1,7 @@
 import { isInvalidHelpWithFeesRef } from '../form/validation';
 
 import { Case, CaseDate, Checkbox, LanguagePreference, formFieldsToCaseMapping, formatCase } from './case';
-import { CaseData, ChangedNameHow, DivorceOrDissolution, Gender, YesOrNo } from './definition';
+import { ApplicationType, CaseData, ChangedNameHow, DivorceOrDissolution, Gender, YesOrNo } from './definition';
 import { applicant1AddressToApi, applicant2AddressToApi } from './formatter/address';
 
 export type OrNull<T> = { [K in keyof T]: T[K] | null };
@@ -124,6 +124,92 @@ const fields: ToApiConverters = {
   confirmReadPetition: data => ({
     confirmReadPetition: checkboxConverter(data.confirmReadPetition),
   }),
+  applicant1LastNameChangedWhenRelationshipFormed: data => ({
+    applicant1LastNameChangedWhenMarried: data.applicant1LastNameChangedWhenRelationshipFormed,
+    ...(data.applicant1LastNameChangedWhenRelationshipFormed === YesOrNo.NO &&
+    data.applicant1NameChangedSinceRelationshipFormed === YesOrNo.NO
+      ? setUnreachableAnswersToNull(['applicant1NameChangedHow', 'applicant1NameChangedHowOtherDetails'])
+      : {}),
+  }),
+  applicant2LastNameChangedWhenRelationshipFormed: data => ({
+    applicant2LastNameChangedWhenMarried: data.applicant2LastNameChangedWhenRelationshipFormed,
+    ...(data.applicant2LastNameChangedWhenRelationshipFormed === YesOrNo.NO &&
+    data.applicant2NameChangedSinceRelationshipFormed === YesOrNo.NO
+      ? setUnreachableAnswersToNull(['applicant2NameChangedHow', 'applicant2NameChangedHowOtherDetails'])
+      : {}),
+  }),
+  applicant1HelpPayingNeeded: data => ({
+    applicant1HWFNeedHelp: data.applicant1HelpPayingNeeded,
+    ...(data.applicant1HelpPayingNeeded === YesOrNo.NO
+      ? setUnreachableAnswersToNull(['applicant1HWFAppliedForFees', 'applicant1HWFReferenceNumber'])
+      : {}),
+  }),
+  applicant2HelpPayingNeeded: data => ({
+    applicant2HWFNeedHelp: data.applicant2HelpPayingNeeded,
+    ...(data.applicant2HelpPayingNeeded === YesOrNo.NO
+      ? setUnreachableAnswersToNull(['applicant2HWFAppliedForFees', 'applicant2HWFReferenceNumber'])
+      : {}),
+  }),
+  applicant1KnowsApplicant2Address: data => ({
+    applicant1KnowsApplicant2Address: data.applicant1KnowsApplicant2Address,
+    ...(data.applicant1KnowsApplicant2Address === YesOrNo.NO
+      ? applicant2AddressToApi(
+          setUnreachableAnswersToNull([
+            'applicant2Address1',
+            'applicant2Address2',
+            'applicant2Address3',
+            'applicant2AddressCountry',
+            'applicant2AddressCounty',
+            'applicant2AddressPostcode',
+          ])
+        )
+      : setUnreachableAnswersToNull(['applicant1WantsToHavePapersServedAnotherWay'])),
+  }),
+  inTheUk: data => ({
+    marriageMarriedInUk: data.inTheUk,
+    ...(data.inTheUk === YesOrNo.YES
+      ? setUnreachableAnswersToNull([
+          'marriageCertificateInEnglish',
+          'marriageCertifiedTranslation',
+          'marriageCountryOfMarriage',
+          'marriagePlaceOfMarriage',
+        ])
+      : {}),
+  }),
+  certificateInEnglish: data => ({
+    marriageCertificateInEnglish: data.certificateInEnglish,
+    ...(data.certificateInEnglish === YesOrNo.NO ? setUnreachableAnswersToNull(['marriageCertifiedTranslation']) : {}),
+  }),
+  applicant1LegalProceedings: data => ({
+    applicant1LegalProceedings: data.applicant1LegalProceedings,
+    ...(data.applicant1LegalProceedings === YesOrNo.YES
+      ? setUnreachableAnswersToNull(['applicant1LegalProceedingsDetails'])
+      : {}),
+  }),
+  applicant2LegalProceedings: data => ({
+    applicant2LegalProceedings: data.applicant2LegalProceedings,
+    ...(data.applicant2LegalProceedings === YesOrNo.YES
+      ? setUnreachableAnswersToNull(['applicant2LegalProceedingsDetails'])
+      : {}),
+  }),
+  applicationType: data => ({
+    applicationType: data.applicationType,
+    ...(data.applicationType === ApplicationType.JOINT_APPLICATION
+      ? {
+          ...setUnreachableAnswersToNull(['applicant2FirstName', 'applicant2MiddleName', 'applicant2LastName']),
+          ...applicant2AddressToApi(
+            setUnreachableAnswersToNull([
+              'applicant2Address1',
+              'applicant2Address2',
+              'applicant2Address3',
+              'applicant2AddressCountry',
+              'applicant2AddressCounty',
+              'applicant2AddressPostcode',
+            ])
+          ),
+        }
+      : {}),
+  }),
 };
 
 const toApiDate = (date: CaseDate | undefined) => {
@@ -139,5 +225,8 @@ const languagePreferenceYesNoOrNull = (value: LanguagePreference | undefined) =>
   }
   return value === LanguagePreference.Welsh ? YesOrNo.YES : YesOrNo.NO;
 };
+
+const setUnreachableAnswersToNull = (properties: string[]): Record<string, null> =>
+  properties.reduce((arr: Record<string, null>, property: string) => ({ ...arr, [property]: null }), {});
 
 export const toApiFormat = (data: Partial<Case>): CaseData => formatCase(fields, data);
