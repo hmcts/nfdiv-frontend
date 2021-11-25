a2204094b0dcc76ad6556dc8644aeb668d043dd9
Linus Norton <linusnorton@gmail.com>
2021-11-25 07:55:58 +0000

    how about this

--- src/main/modules/session/index.ts
+++ src/main/modules/session/index.ts
@@ -35,7 +35,7 @@ export class SessionStorage {
     const redisHost = config.get<string>('session.redis.host');
     if (redisHost) {
       const client = redis.createClient({
-        url: 'http://' + redisHost + ':6380',
+        url: redisHost + ':6380,ssl=True,abortConnect=False',
         password: config.get<string>('session.redis.key'),
       });
 
