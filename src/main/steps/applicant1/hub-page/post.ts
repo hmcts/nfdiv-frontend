0b7d8955f3ad8bd4bc713abe453fb3374526b8b9
velimd <duzgunce.vd@gmail.com>
2021-12-03 18:10:12 +0000

    clean up

--- src/main/steps/applicant1/hub-page/post.ts
+++ src/main/steps/applicant1/hub-page/post.ts
@@ -1,11 +1,15 @@
 import autobind from 'autobind-decorator';
 
-import { APPLICANT_1_CONFIRM_RECEIPT } from '../../../app/case/definition';
+import { APPLICANT_1_CONFIRM_RECEIPT, ApplicationType, DRAFT_CONDITIONAL_ORDER } from '../../../app/case/definition';
+import { AppRequest } from '../../../app/controller/AppRequest';
 import { AnyObject, PostController } from '../../../app/controller/PostController';
 
 @autobind
 export default class HubPagePostController extends PostController<AnyObject> {
-  protected getEventName(): string {
-    return APPLICANT_1_CONFIRM_RECEIPT;
+  protected getEventName(req: AppRequest<AnyObject>): string {
+    return req.session.userCase.applicationType === ApplicationType.JOINT_APPLICATION &&
+      req.body.applicant1ConfirmReceipt
+      ? APPLICANT_1_CONFIRM_RECEIPT
+      : DRAFT_CONDITIONAL_ORDER;
   }
 }
