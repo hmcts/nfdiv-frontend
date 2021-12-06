c55fb0d58aa42ab99f4c1dcef96087344b8cfdfc
Lara <laratollington@gmail.com>
2021-12-06 15:34:20 +0000

    resolve conflicts

--- src/main/steps/applicant1/hub-page/sole/content.ts
+++ src/main/steps/applicant1/hub-page/sole/content.ts
@@ -67,7 +67,7 @@ const en = ({ isDivorce, partner, userCase }: CommonContent) => ({
       isDivorce ? 'divorce application' : 'application to end your civil partnership'
     }. <a class="govuk-link" href="https://www.gov.uk/money-property-when-relationship-ends" target="_blank">Find out about dividing money and property</a>`,
   },
-  holdingAndDeemedOrDispensedService: {
+  holdingAndDeemedOrDispensedAccepted: {
     line1: `Your application ${
       userCase.alternativeServiceOutcomes?.[0].value.alternativeServiceType === AlternativeServiceType.DISPENSED
         ? 'to dispense with service'
@@ -83,35 +83,6 @@ const en = ({ isDivorce, partner, userCase }: CommonContent) => ({
          ? 'dispensed'
          : 'deemed'
      } service</a>`,
-    line2: `The next step is for you to apply for a ‘conditional order’.
-    A conditional order is a document that says the court does not see any reason why you cannot ${
-      isDivorce ? 'get a divorce' : 'end your civil partnership'
-    }`,
-    line3: `You can apply for a conditional order on ${dayjs(userCase.issueDate)
-      .add(141, 'day')
-      .format('D MMMM YYYY')}. This is because you have to wait until 20 weeks from when the ${
-      isDivorce ? 'divorce application' : 'application to end your civil partnership'
-    } was issued.
-     You will receive an email to remind you.`,
-    readMore: 'Read more about the next steps',
-    line4: `You have to complete 2 more steps before ${
-      isDivorce ? 'you are legally divorced' : 'your civil partnership has ended'
-    }:`,
-    steps: {
-      step1: `
-        <strong>Apply for a conditional order</strong><br>
-        This shows that the court agrees that you’re entitled to ${
-          isDivorce ? 'get a divorce' : 'end your civil partnership'
-        }.`,
-      step2: `
-        <strong>Apply for a final order</strong><br>
-        This legally ends the ${
-          isDivorce ? 'marriage' : 'civil partnership'
-        }. You cannot apply for a final order until 6 weeks after the conditional order.`,
-    },
-    line5: `You can use the time to decide how your money and property will be divided. This is dealt with separately to the ${
-      isDivorce ? 'divorce application' : 'application to end your civil partnership'
-    }. <a class="govuk-link" href="https://www.gov.uk/money-property-when-relationship-ends" target="_blank">Find out about dividing money and property</a>`,
   },
   d8Awaiting: {
     line1: `Your ${partner} has responded to your application and said they want to defend the ${
