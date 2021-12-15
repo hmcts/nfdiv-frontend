fbe02ce654b6bb55372d22d6489069e19d74f936
Lara <laratollington@gmail.com>
2021-12-15 10:44:40 +0000

    fix e2e tests

--- src/test/steps/common.ts
+++ src/test/steps/common.ts
@@ -52,8 +52,8 @@ Given('I create a new user and login', () => {
   login('citizenSingleton');
 });
 
-Given('I login with applicant {number}', async (applicant: number) => {
-  autoLogin.login(I, testConfig.GetUser(applicant).username);
+Given('I login with applicant {string}', async (applicant: string) => {
+  autoLogin.login(I, testConfig.GetUser(parseInt(applicant)).username);
 });
 
 export const iClick = (text: string, locator?: CodeceptJS.LocatorOrString, wait?: number): void => {
