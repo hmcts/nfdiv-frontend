c56eab8d4b51ad32a81b84b726f24d6e393edbe3
velimd <duzgunce.vd@gmail.com>
2021-12-15 09:18:31 +0000

    pr fix considering joint application for CO post event

--- src/main/app/controller/PostController.ts
+++ src/main/app/controller/PostController.ts
@@ -85,7 +85,11 @@ export class PostController<T extends AnyObject> {
   }
 
   protected getEventName(req: AppRequest<T>): string {
-    if (req.session.userCase.state === State.ConditionalOrderDrafted && !req.session.isApplicant2) {
+    if (
+      req.session.userCase.state === State.ConditionalOrderDrafted &&
+      (req.session.userCase.applicationType === ApplicationType.JOINT_APPLICATION ||
+        (req.session.userCase.applicationType === ApplicationType.SOLE_APPLICATION && !req.session.isApplicant2))
+    ) {
       return UPDATE_CONDITIONAL_ORDER;
     } else if (req.session.userCase.applicationType === ApplicationType.SOLE_APPLICATION && req.session.isApplicant2) {
       return UPDATE_AOS;
