8208146dbf8dd8ca142ccc68d77bf4164f9bdd60
velimd <duzgunce.vd@gmail.com>
2021-12-17 13:01:32 +0000

    clean up content

--- src/main/steps/applicant1/read-the-response/content.ts
+++ src/main/steps/applicant1/read-the-response/content.ts
@@ -15,8 +15,8 @@ const en = ({ partner, isDivorce, userCase }: CommonContent) => ({
   subHeading3: 'How the respondent wants to respond',
   line4:
     userCase.disputeApplication === YesOrNo.YES
-      ? 'I want to dispute the divorce'
-      : 'Continue without disputing the divorce',
+      ? `I want to dispute the ${isDivorce ? 'divorce' : 'application to end your civil partnership'}`
+      : `Continue without disputing the ${isDivorce ? 'divorce' : 'application to end your civil partnership'}`,
   subHeading4: 'The legal power (jurisdiction) of the courts',
   line5:
     userCase.jurisdictionAgree === YesOrNo.YES
@@ -24,12 +24,13 @@ const en = ({ partner, isDivorce, userCase }: CommonContent) => ({
       : `The respondent does not agree the court has jurisdiction.<br> They provided the following reasons:<br> ${userCase.reasonCourtsOfEnglandAndWalesHaveNoJurisdiction}`,
   countryLifeMainlyBased: `They said their life was mainly based in the following country:<br> ${userCase.inWhichCountryIsYourLifeMainlyBased}`,
   subHeading5: 'Other court cases',
-  line6:
-    userCase.applicant2LegalProceedings === YesOrNo.NO
-      ? 'The respondent said there were no other court cases relating to the marriage'
-      : `The respondent said there were other court cases relating to the ${
-          isDivorce ? 'marriage' : 'civil partnership'
-        }. They provided the following details:<br> ${userCase.applicant2LegalProceedingsDetails}`,
+  line6: `The respondent said there were${
+    userCase.applicant2LegalProceedings === YesOrNo.NO ? ' no' : ''
+  } other court cases relating to the ${isDivorce ? 'marriage' : 'civil partnership'}.${
+    userCase.applicant2LegalProceedings === YesOrNo.YES
+      ? ` They provided the following details:<br> ${userCase.applicant2LegalProceedingsDetails}`
+      : ''
+  }`,
   subHeading6: 'Statement of truth',
   line7: '“I believe the facts stated in this response are true”',
   line8: `${userCase.applicant2FirstNames} ${userCase.applicant2LastNames}`,
