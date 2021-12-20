24a33f55cc1ccf0425dc03f109a0e5843163d5ce
Velican <31857663+velimd@users.noreply.github.com>
2021-12-20 10:44:47 +0000

    NFDIV 1173 (#1552)
    * implementation
    
    * clean up
    
    * update post controller to use update CO event
    
    * pr fix considering joint application for CO post event
    
    * pr clean up
    
    * clean post controller
    
    * add url redirect after master merge
    
    * clean up content
    
    * e2e test
    
    * merge clean up
    
    * fix e2e test
    
    * fix e2e test (2)

---
+++ src/main/steps/applicant1/read-the-response/content.ts
@@ -0,0 +1,61 @@
+import { YesOrNo } from '../../../app/case/definition';
+import { TranslationFn } from '../../../app/controller/GetController';
+import { FormContent } from '../../../app/form/Form';
+import { CommonContent } from '../../common/common.content';
+
+const en = ({ partner, isDivorce, userCase }: CommonContent) => ({
+  title: `Read your ${partner}'s response`,
+  line1: `You need to read your ${partner}'s response before you continue. These are the questions they were asked, including whether they want to dispute your application ${
+    isDivorce ? 'for divorce' : 'to end your civil partnership'
+  }. It’s legally known as an ‘acknowledgement of service’.`,
+  subHeading1: 'Respondent',
+  line2: `${userCase.applicant2FirstNames} ${userCase.applicant2MiddleNames} ${userCase.applicant2LastNames}`,
+  subHeading2: 'Applicant',
+  line3: `${userCase.applicant1FirstNames} ${userCase.applicant1MiddleNames} ${userCase.applicant1LastNames}`,
+  subHeading3: 'How the respondent wants to respond',
+  line4:
+    userCase.disputeApplication === YesOrNo.YES
+      ? `I want to dispute the ${isDivorce ? 'divorce' : 'application to end your civil partnership'}`
+      : `Continue without disputing the ${isDivorce ? 'divorce' : 'application to end your civil partnership'}`,
+  subHeading4: 'The legal power (jurisdiction) of the courts',
+  line5:
+    userCase.jurisdictionAgree === YesOrNo.YES
+      ? 'The respondent agrees the court has jurisdiction'
+      : `The respondent does not agree the court has jurisdiction.<br> They provided the following reasons:<br> ${userCase.reasonCourtsOfEnglandAndWalesHaveNoJurisdiction}`,
+  countryLifeMainlyBased: `They said their life was mainly based in the following country:<br> ${userCase.inWhichCountryIsYourLifeMainlyBased}`,
+  subHeading5: 'Other court cases',
+  line6: `The respondent said there were${
+    userCase.applicant2LegalProceedings === YesOrNo.NO ? ' no' : ''
+  } other court cases relating to the ${isDivorce ? 'marriage' : 'civil partnership'}.${
+    userCase.applicant2LegalProceedings === YesOrNo.YES
+      ? ` They provided the following details:<br> ${userCase.applicant2LegalProceedingsDetails}`
+      : ''
+  }`,
+  subHeading6: 'Statement of truth',
+  line7: '“I believe the facts stated in this response are true”',
+  line8: `${userCase.applicant2FirstNames} ${userCase.applicant2LastNames}`,
+});
+
+// @TODO translations
+const cy: typeof en = en;
+
+export const form: FormContent = {
+  fields: {},
+  submit: {
+    text: l => l.continue,
+  },
+};
+
+const languages = {
+  en,
+  cy,
+};
+
+export const generateContent: TranslationFn = content => {
+  const hasNotAgreedToJurisdiction = content.userCase.jurisdictionAgree === YesOrNo.NO;
+  return {
+    ...languages[content.language](content),
+    form,
+    hasNotAgreedToJurisdiction,
+  };
+};
--- src/main/steps/applicant1Sequence.ts
+++ src/main/steps/applicant1Sequence.ts
@@ -59,6 +59,7 @@ import {
   PAY_AND_SUBMIT,
   PAY_YOUR_FEE,
   PageLink,
+  READ_THE_RESPONSE,
   RELATIONSHIP_DATE_URL,
   RELATIONSHIP_NOT_BROKEN_URL,
   RELATIONSHIP_NOT_LONG_ENOUGH_URL,
@@ -482,6 +483,10 @@ export const applicant1Sequence: Step[] = [
   {
     url: CHECK_PHONE_NUMBER,
     getNextStep: () => ADDRESS_PRIVATE,
+  },
+  {
+    url: READ_THE_RESPONSE,
+    getNextStep: () => CONTINUE_WITH_YOUR_APPLICATION,
   },
   {
     url: CONTINUE_WITH_YOUR_APPLICATION,
