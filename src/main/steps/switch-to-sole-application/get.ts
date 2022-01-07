aaef76f93cfd5b7e16d79f888ad35a3cf0b52ffd
velimd <duzgunce.vd@gmail.com>
2022-01-07 14:35:16 +0000

    fix template path

--- src/main/steps/switch-to-sole-application/get.ts
+++ src/main/steps/switch-to-sole-application/get.ts
@@ -4,6 +4,6 @@ import { generateContent } from './content';
 
 export class SwitchToSoleApplicationGetController extends GetController {
   constructor() {
-    super(__dirname + './template.njk', generateContent);
+    super(__dirname + '/template.njk', generateContent);
   }
 }
