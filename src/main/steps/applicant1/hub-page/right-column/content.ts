e3eb080eece7026358cca4185b93db52cdae9309
Aaron Moriarty <95974886+amoriartyMOJ@users.noreply.github.com>
2022-03-07 09:34:44 +0000

    NFDIV-1677 Update postal and email addresses (#1830)
    * Update email for divorce cases as per NFDIV-1677
    
    * Update po-box details
    
    * Update postal code to new details
    
    * use common contactEmail var as its cleaner
    
    * remove logic thats no longer needed as email can be static and address too
    
    * All logic will stay as is. Only email / address will change.
    
    If in future we need to update this a new ticket will be made.
    
    Co-authored-by: danielbe11 <70265071+danielbe11@users.noreply.github.com>

--- src/main/steps/applicant1/hub-page/right-column/content.ts
+++ src/main/steps/applicant1/hub-page/right-column/content.ts
@@ -3,7 +3,7 @@ import { TranslationFn } from '../../../../app/controller/GetController';
 import { CommonContent } from '../../../common/common.content';
 import { APPLICANT_2, CHECK_CONTACT_DETAILS, RESPONDENT } from '../../../urls';
 
-const en = ({ isDivorce, isApplicant2, userCase }: CommonContent) => ({
+const en = ({ isDivorce, isApplicant2, userCase, contactEmail }: CommonContent) => ({
   applicationDownload: {
     reference: 'Divorce-Application',
     link: `/downloads/${isDivorce ? 'divorce-application' : 'application-to-end-civil-partnership'}`,
@@ -48,16 +48,14 @@ const en = ({ isDivorce, isApplicant2, userCase }: CommonContent) => ({
   gettingHelp: 'Getting help',
   telephone: '<strong>Phone</strong></br> 0300 303 0642</br> (Monday to Friday, 8am to 8PM, Saturday 8AM to 2PM)',
   email: `<strong>Email</strong><br>
-    <a class="govuk-link" href="mailto:${
-      isDivorce ? 'contactdivorce@justice.gov.uk' : 'civilpartnership.case@justice.gov.uk'
-    }">${isDivorce ? 'contactdivorce@justice.gov.uk' : 'civilpartnership.case@justice.gov.uk'}</a>`,
+    <a class="govuk-link" href="mailto:${contactEmail}">${contactEmail}</a>`,
   post: `
     <strong>Post</strong></br>
     Courts and Tribunals Service Centre</br>
     Digital Divorce</br>
-    PO Box 12706</br>
+    PO Box 13226</br>
     Harlow</br>
-    CM20 9QT`,
+    CM20 9UG`,
   whatHappensNext: 'What happens next',
 });
 
