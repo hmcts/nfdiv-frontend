193143f621465ef2841471fcceb2bec2335351c0
danielbe11 <daniel.bell@solirius.com>
2022-03-07 13:10:56 +0000

    Add isCountryUk test

--- src/main/steps/applicant1Sequence.test.ts
+++ src/main/steps/applicant1Sequence.test.ts
@@ -1,9 +1,9 @@
 import { ApplicationType } from '../app/case/definition';
 
-import { applicant1Sequence } from './applicant1Sequence';
+import { applicant1Sequence, isCountryUk } from './applicant1Sequence';
 import { ENTER_THEIR_ADDRESS, OTHER_COURT_CASES, YOU_NEED_TO_SERVE } from './urls';
 
-describe('Applicant 1 Sequence', () => {
+describe('Applicant 1 Sequence test', () => {
   describe('ENTER_THEIR_ADDRESS', () => {
     const caseData = {
       applicationType: ApplicationType.SOLE_APPLICATION,
@@ -26,5 +26,23 @@ describe('Applicant 1 Sequence', () => {
       const nextStepMethod = applicant1Sequence.find(obj => obj.url === ENTER_THEIR_ADDRESS)!.getNextStep;
       expect(nextStepMethod(caseData)).toBe(YOU_NEED_TO_SERVE);
     });
+  });
+});
+
+describe('isCountryUk test', () => {
+  test('True UK terms', () => {
+    const ukTermsTest = ['uk', 'unitedkingdom', 'u.k', 'u.k.'];
+    for (const term of ukTermsTest) {
+      expect(isCountryUk(term)).toBe(true);
+    }
+  });
+
+  test('False UK term', () => {
+    const nonUkCountry = 'France';
+    expect(isCountryUk(nonUkCountry)).toBe(false);
+  });
+
+  test('Undefined country', () => {
+    expect(isCountryUk(undefined)).toBe(false);
   });
 });
