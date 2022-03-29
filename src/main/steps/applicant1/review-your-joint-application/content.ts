e8ee32926848aabaad6e7625b4a18bb58b74007e
taleb <taleb.benouaer@hmcts.net>
2022-03-29 13:11:50 +0100

    nfdiv-1949: tidyup content and template

--- src/main/steps/applicant1/review-your-joint-application/content.ts
+++ src/main/steps/applicant1/review-your-joint-application/content.ts
@@ -115,17 +115,17 @@ const en = ({ isDivorce, userCase, partner, required, userEmail }: CommonContent
   heading17: 'Statement of truth',
   factsTrue: 'I believe that the facts stated in this application are true.',
   confirmInformationStillCorrect: 'Is the information in this application still correct?',
-  reasonInformationNotCorrect: `<strong>Changing your contact details</strong>
-    <br>
-    You can update your email address, phone number and postal address in the <a class="govuk-link" href="${CHECK_CONTACT_DETAILS}">‘contact details’ section of your ${
-    isDivorce ? 'divorce' : ''
-  } account.</a> There is no cost for this.</p>
-    <br>
-    <p class="govuk-body"><strong>Changing any other information</strong>
-    <br>
-    If you want to change any other information then you should provide details below. You may need to pay a ${getFee(
+  reasonInformationNotCorrect: {
+    heading1: 'Changing your contact details',
+    part1: 'You can update your email address, phone number and postal address in the ',
+    link: CHECK_CONTACT_DETAILS,
+    linkText: `'contact details' section of your ${isDivorce ? 'divorce' : ''} account.`,
+    part2: 'There is no cost for this.',
+    heading2: 'Changing any other information',
+    part3: `If you want to change any other information then you should provide details below. You may need to pay a ${getFee(
       config.get('fees.updateApplication')
     )} fee. This is because the application will need to be updated and sent to your ${partner} again.`,
+  },
   reasonInformationNotCorrectHint:
     'Provide details of any other information that needs updating. Do not tell the court about updates to contact details here.',
   errors: {
@@ -144,9 +144,7 @@ const cy = en;
 export const form: FormContent = {
   fields: {
     applicant1ConfirmInformationStillCorrect: {
-      type: 'radios',
-      classes: 'govuk-radios',
-      label: l => l.confirmInformationStillCorrect,
+      type: 'hidden',
       values: [
         { label: l => l.yes, value: YesOrNo.YES },
         {
@@ -154,14 +152,8 @@ export const form: FormContent = {
           value: YesOrNo.NO,
           subFields: {
             applicant1ReasonInformationNotCorrect: {
-              type: 'textarea',
+              type: 'hidden',
               label: l => l.title,
-              labelHidden: true,
-              hint: l => `
-                <p class="govuk-body">
-                  ${l.reasonInformationNotCorrect}
-                </p>
-                ${l.reasonInformationNotCorrectHint}`,
               validator: value => isFieldFilledIn(value),
             },
           },
