40d41833bcddf037cbddd0673e3d4fe329b27264
danielbe11 <daniel.bell@solirius.com>
2022-05-18 13:14:48 +0100

    Check for content.userCase.applicant2StatementOfTruth || content.userCase.aosStatementOfTruth

--- src/main/steps/home/get.ts
+++ src/main/steps/home/get.ts
@@ -78,7 +78,8 @@ const applicant1RedirectPageSwitch = (userCase: Partial<CaseWithId>, isFirstQues
         return CHECK_CONDITIONAL_ORDER_ANSWERS_URL;
       } else if (userCase.applicant1ApplyForConditionalOrderStarted) {
         return userCase.applicationType === ApplicationType.SOLE_APPLICATION &&
-          userCase.applicant2StatementOfTruth === Checkbox.Checked
+          (userCase.applicant2StatementOfTruth === Checkbox.Checked ||
+            userCase.aosStatementOfTruth === Checkbox.Checked)
           ? READ_THE_RESPONSE
           : CONTINUE_WITH_YOUR_APPLICATION;
       } else {
