254059755d3e23a61f90aea12279dc15e426e5ac
Lara <laratollington@gmail.com>
2022-05-18 11:02:35 +0100

    add back in code to try to fix build

--- src/main/assets/js/cookie.ts
+++ src/main/assets/js/cookie.ts
@@ -33,6 +33,9 @@ cookieManager.init({
   userPreferences: {
     cookieName: 'nfdiv-cookie-preferences',
   },
+  preferencesForm: {
+    class: 'cookie-preferences-form',
+  },
   cookieManifest: [
     {
       categoryName: 'essential',
@@ -48,6 +51,28 @@ cookieManager.init({
       cookies: ['dtCookie', 'dtLatC', 'dtPC', 'dtSa', 'dtValidationCookie', 'dtDisabled', 'rxVisitor', 'rxvt'],
     },
   ],
+  cookieBanner: {
+    class: 'cookie-banner',
+    showWithPreferencesForm: false,
+    actions: [
+      {
+        name: 'accept',
+        buttonClass: 'cookie-banner-accept-button',
+        confirmationClass: 'cookie-banner-accept-message',
+        consent: true,
+      },
+      {
+        name: 'reject',
+        buttonClass: 'cookie-banner-reject-button',
+        confirmationClass: 'cookie-banner-reject-message',
+        consent: false,
+      },
+      {
+        name: 'hide',
+        buttonClass: 'cookie-banner-hide-button',
+      },
+    ],
+  },
 });
 
 declare global {
