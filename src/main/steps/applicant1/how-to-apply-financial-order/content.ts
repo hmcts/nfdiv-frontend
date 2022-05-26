a77bb13cfce2f4a01f4bdba8cb20245dcc84de65
Lara <laratollington@gmail.com>
2022-05-26 12:20:13 +0100

    NFDIV-1547 english content update

--- src/main/steps/applicant1/how-to-apply-financial-order/content.ts
+++ src/main/steps/applicant1/how-to-apply-financial-order/content.ts
@@ -4,7 +4,7 @@ import { CommonContent } from '../../common/common.content';
 
 const en = ({ partner, isDivorce }: CommonContent) => ({
   title: 'How to apply for a financial order',
-  line1: `You will need to complete another form (Form A or Form A1) and pay an additional fee. You can apply at any time, so long as your ${partner} is still alive.`,
+  line1: `You will need to complete another form (Form A or Form A1) and pay a fee. You can apply at any time, so long as your ${partner} is still alive.`,
   line2: `You will be given a link to the relevant forms and more guidance after you have submitted this ${
     isDivorce ? 'divorce' : 'ending your civil partnership'
   } application. You can get legal advice or ask a solicitor to draft a financial order for you.`,
@@ -18,7 +18,7 @@ const cy: typeof en = ({ partner, isDivorce }: CommonContent) => ({
     isDivorce ? 'am ysgariad' : 'i ddod â’ch partneriaeth sifil i ben'
   }. Gallwch geisio cyngor cyfreithiol neu ofyn i gyfreithiwr ddrafftio gorchymyn ariannol ar eich cyfer.`,
   line3: `Parhau gyda’ch ${isDivorce ? 'cais am ysgariad' : 'cais i ddod â’ch partneriaeth sifil i ben'}.`,
-}); // todo nfdiv-1547
+});
 
 export const form: FormContent = {
   fields: {},
