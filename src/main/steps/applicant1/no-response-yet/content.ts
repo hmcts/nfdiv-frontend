2032cfd0144a65b3e083f6245c54cddb92455400
Tom Elliott <tomelliott@Toms-MBP.home>
2022-05-27 16:48:35 +0100

    NFDIV-1517: Add translations for Welsh. Awaiting one line of translation in order to complete

--- src/main/steps/applicant1/no-response-yet/content.ts
+++ src/main/steps/applicant1/no-response-yet/content.ts
@@ -10,7 +10,14 @@ const en = ({ partner }) => ({
   line4: `You can <a href="${SWITCH_TO_SOLE_APPLICATION}" class="govuk-link">create a new application here.</a>`,
 });
 
-const cy: typeof en = en;
+const cy: typeof en = ({ partner }) => ({
+  title: `Nid yw eich ${partner} wedi ymateb`,
+  line1: `Nid yw eich ${partner} wedi ymateb. Os ydych eisiau gwneud cais ar y cyd yna mae’n rhaid iddynt ymuno a chadarnhau’r cais cyn iddo gael ei gyflwyno. Dylech gysylltu â nhw a gofyn iddynt wirio eu negeseuon e-bost a chadarnhau’r cais, os yw’n ddiogel i wneud hynny.`,
+  readMore: 'Os ydych yn meddwl na fyddant yn ymateb',
+  line2: `Os ydych yn meddwl na fydd eich ${partner} yn ymuno â’r cais yna gallwch greu ‘cais unigol’ newydd.`,
+  line3: `Mae hyn yn golygu y byddwch yn cyflwyno’r cais ar eich pen eich hun, a bydd eich ${partner} yn ymateb iddo ar ôl iddo gael ei gyflwyno a’i wirio gan y llys.`,
+  line4: `You can <a href="${SWITCH_TO_SOLE_APPLICATION}" class="govuk-link">create a new application here.</a>`,
+});
 
 const languages = {
   en,
