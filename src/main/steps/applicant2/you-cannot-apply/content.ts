74a8df8735f68b1f0e90f38cfede173e02e74566
aramchurn21 <Adesh.Ramchurn@justice.gov.uk>
2022-06-01 11:05:25 +0100

    update english and welsh content for you-cannot-apply page

--- src/main/steps/applicant2/you-cannot-apply/content.ts
+++ src/main/steps/applicant2/you-cannot-apply/content.ts
@@ -7,10 +7,9 @@ const en = ({ isDivorce, partner }) => ({
       for you to ${isDivorce ? 'get a divorce' : 'end your civil partnership'}. This is the law in England and Wales.`,
   line2: `If you end this joint application then your ${partner} will be notified by email. If they still want to ${
     isDivorce ? 'get a divorce' : 'end your civil partnership'
-  } then they can apply as a sole applicant. This would mean you would have to respond to ${
-    isDivorce ? 'divorce application' : 'application to end your civil partnership'
-  }. You can also launch a sole application, if you want.`,
-  line3: 'If you are not sure what to do then you can save and sign out and get legal advice.',
+  } then they can start a new sole application. If they do, then you will be asked to respond to it.`,
+  line3: `If you are not sure what to do then you can save and sign out and get legal advice. or talk to your ${partner},
+  if it’s safe to do so.`,
   continue: 'End joint application',
 });
 
@@ -22,10 +21,9 @@ const cy: typeof en = ({ isDivorce, partner }) => ({
        ${isDivorce ? 'cael ysgariad ' : 'dod â’r bartneriaeth sifil i ben'}. Dyma’r gyfraith yng Nghymru a Lloegr.`,
   line2: `Os byddwch yn dod â’r cais ar y cyd hwn i ben, bydd eich ${partner} yn cael eu hysbysu drwy e-bost. Os ydynt dal eisiau ${
     isDivorce ? 'cael ysgariad' : 'dod â’r bartneriaeth sifil i ben'
-  } yna gallant gychwyn cais unigol newydd. Os ydynt yn gwneud hynny, yna gofynnir ichi ymateb iddo. ${
-    isDivorce ? 'divorce application' : 'application to end your civil partnership'
-  }. You can also launch a sole application, if you want.`,
-  line3: 'Os ydych yn ansicr am beth i wneud, yna gallwch gadw’r cais ac allgofnodi a cheisio cyngor cyfreithiol.',
+  } yna gallant gychwyn cais unigol newydd. Os ydynt yn gwneud hynny, yna gofynnir ichi ymateb iddo.`,
+  line3: `Os ydych yn ansicr am beth i wneud, yna gallwch gadw’r cais ac allgofnodi a cheisio cyngor cyfreithiol, neu siarad gyda’ch
+  ${partner}, os yw’n ddiogel i wneud hynny.`,
   continue: 'Dod â chais ar y cyd i ben',
 });
 
