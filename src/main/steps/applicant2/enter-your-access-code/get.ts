f6461e39c1c1aac1f8bc38916a19ebda3d6fb735
taleb <20416858+benouaer@users.noreply.github.com>
2022-06-06 09:55:48 +0100

    Bug/nfdiv 2245 fix linking url issues (#2152)
    * nfdiv-2245: search for cases using email
    
    * nfdiv-2245: tidyup case search results
    
    * nfdiv-2245: tidyup routes
    
    * nfdiv-2245: only search for linked user cases
    
    * nfdiv-2245: fix broken tests
    
    * nfdiv-2245: fix failing tests
    
    * nfdiv-2245: minor tidyup
    
    * nfdiv-2245: update redirect tests
    
    * nfdiv-2245: rename variable for clarity
    
    * nfdiv-2245: fix saveAndSignout issue
    
    * nfdiv-2245: enable posting to all linking urls
    
    Co-authored-by: Lara <52360278+lara-mpt@users.noreply.github.com>

--- src/main/steps/applicant2/enter-your-access-code/get.ts
+++ src/main/steps/applicant2/enter-your-access-code/get.ts
@@ -1,9 +1,23 @@
+import autobind from 'autobind-decorator';
+import { Response } from 'express';
+
+import { AppRequest } from '../../../app/controller/AppRequest';
 import { GetController } from '../../../app/controller/GetController';
+import { HOME_URL } from '../../urls';
 
 import { generateContent } from './content';
 
+@autobind
 export class Applicant2AccessCodeGetController extends GetController {
   constructor() {
     super(__dirname + '/../../common/template.njk', generateContent);
+  }
+
+  public async get(req: AppRequest, res: Response): Promise<void> {
+    if (await req.locals.api.isAlreadyLinked(res.locals.serviceType, req.session.user)) {
+      return res.redirect(HOME_URL);
+    }
+
+    await super.get(req, res);
   }
 }
