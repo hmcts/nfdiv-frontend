9258ed1e9b9d5181779136e15b084a427302c160
Lara <52360278+lara-mpt@users.noreply.github.com>
2022-06-14 12:47:51 +0100

    Apply suggestions from code review
    Co-authored-by: Daniel Bell <70265071+danielbe11@users.noreply.github.com>

--- src/main/app/jurisdiction/bulletedPointsContent.ts
+++ src/main/app/jurisdiction/bulletedPointsContent.ts
@@ -125,17 +125,17 @@ export const cyConnectionUserReads = (
     [JurisdictionConnections.APP_1_APP_2_LAST_RESIDENT]: `roeddech chi a’ch ${partner} yn preswylio’n arferol ddiwethaf yng Nghymru a Lloegr, ac mae un ohonoch yn parhau i breswylio yno`,
     [JurisdictionConnections.APP_2_RESIDENT_SOLE]: `mae eich ${partner} yn preswylio’n arferol yng Nghymru a Lloegr`,
     [JurisdictionConnections.APP_2_RESIDENT_JOINT]: `mae eich ${partner} yn preswylio’n arferol yng Nghymru a Lloegr`,
-    [JurisdictionConnections.APP_1_RESIDENT_TWELVE_MONTHS]: `Rydych ${isJointApplication ? `chi neu'ch ${partner}` : ''}
+    [JurisdictionConnections.APP_1_RESIDENT_TWELVE_MONTHS]: `rydych ${isJointApplication ? `chi neu'ch ${partner}` : ''}
     yn preswylio'n arferol yng Nghymru a Lloegr ac wedi preswylio yno am o leiaf blwyddyn yn union cyn gwneud y cais hwn`,
-    [JurisdictionConnections.APP_1_RESIDENT_SIX_MONTHS]: `Rydych ${
+    [JurisdictionConnections.APP_1_RESIDENT_SIX_MONTHS]: `rydych ${
       isJointApplication ? `chi neu'ch ${partner} ` : ''
     }â'ch domisil, ac yn preswylio'n arferol, yng Nghymru a Lloegr ac wedi preswylio yno am o leiaf chwe mis yn union cyn gwneud y cais hwn`,
     [JurisdictionConnections.APP_1_APP_2_DOMICILED]: `mae eich domisil chi a domisil eich ${partner} yng Nghymru a Lloegr`,
     [JurisdictionConnections.APP_1_DOMICILED]: 'dim ond eich domisil chi sydd yng Nghymru a Lloegr',
-    [JurisdictionConnections.APP_2_DOMICILED]: `Dim ond domisil eich ${partner} sydd yng Nghymru a Lloegr`,
+    [JurisdictionConnections.APP_2_DOMICILED]: `dim ond domisil eich ${partner} sydd yng Nghymru a Lloegr`,
     [JurisdictionConnections.RESIDUAL_JURISDICTION_CP]:
-      "Mi wnaethoch chi a’ch partner sifil gofrestru eich partneriaeth sifil yng Nghymru a Lloegr, a byddai er budd cyfiawnder i'r llys ysgwyddo awdurdodaeth yn yr achos hwn",
-    [JurisdictionConnections.RESIDUAL_JURISDICTION_D]: `Mi wnaethoch chi a’ch ${partner} briodi eich gilydd yng Nghymru a Lloegr, a byddai er budd cyfiawnder i’r llys ysgwyddo awdurdodaeth yn yr achos hwn`,
+      "mi wnaethoch chi a’ch partner sifil gofrestru eich partneriaeth sifil yng Nghymru a Lloegr, a byddai er budd cyfiawnder i'r llys ysgwyddo awdurdodaeth yn yr achos hwn",
+    [JurisdictionConnections.RESIDUAL_JURISDICTION_D]: `mi wnaethoch chi a’ch ${partner} briodi eich gilydd yng Nghymru a Lloegr, a byddai er budd cyfiawnder i’r llys ysgwyddo awdurdodaeth yn yr achos hwn`,
     [JurisdictionConnections.APP_1_RESIDENT_JOINT]: 'rydych yn preswylio’n arferol yng Nghymru a Lloegr',
   };
 };
