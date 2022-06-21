3912db7e27e9db9799a6196b2c13671aef06b5ef
Lara <laratollington@gmail.com>
2022-06-21 11:35:37 +0100

    Revert "Upgrade to @hmcts/cookie-manage v1.0.0 (#2084)"
    This reverts commit d5e6ee5e99ddf462b5150f5cbc662134f0eeec79.

--- src/main/assets/js/cookie.ts
+++ src/main/assets/js/cookie.ts
@@ -1,18 +1,41 @@
-import cookieManager from '@hmcts/cookie-manager';
+import * as cookieManager from '@hmcts/cookie-manager';
 
-cookieManager.on('UserPreferencesLoaded', preferences => {
-  const dataLayer = window.dataLayer || [];
-  dataLayer.push({ event: 'Cookie Preferences', cookiePreferences: preferences });
-});
+import { qs } from './selectors';
 
-cookieManager.on('UserPreferencesSaved', preferences => {
+const cookieBanner = qs('#cm-cookie-banner');
+const cookieBannerDecision = cookieBanner?.querySelector('.govuk-cookie-banner__decision') as HTMLInputElement;
+const cookieBannerConfirmation = cookieBanner?.querySelector('.govuk-cookie-banner__confirmation') as HTMLInputElement;
+
+function cookieBannerAccept() {
+  const confirmationMessage = cookieBannerConfirmation?.querySelector('p') as HTMLInputElement;
+  confirmationMessage.innerHTML = 'You’ve accepted additional cookies. ' + confirmationMessage.innerHTML;
+}
+
+function cookieBannerReject() {
+  const confirmationMessage = cookieBannerConfirmation?.querySelector('p') as HTMLInputElement;
+  confirmationMessage.innerHTML = 'You’ve rejected additional cookies. ' + confirmationMessage.innerHTML;
+}
+
+function cookieBannerSaved() {
+  cookieBannerDecision.hidden = true;
+  cookieBannerConfirmation.hidden = false;
+}
+
+function preferenceFormSaved() {
+  const message = qs('.cookie-preference-success') as HTMLInputElement;
+  message.style.display = 'block';
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+function cookiePreferencesUpdated(cookieStatus) {
   const dataLayer = window.dataLayer || [];
   const dtrum = window.dtrum;
 
-  dataLayer.push({ event: 'Cookie Preferences', cookiePreferences: preferences });
+  dataLayer.push({ event: 'Cookie Preferences', cookiePreferences: cookieStatus });
 
   if (dtrum !== undefined) {
-    if (preferences.apm === 'on') {
+    if (cookieStatus.apm === 'on') {
       dtrum.enable();
       dtrum.enableSessionReplay();
     } else {
@@ -20,59 +43,37 @@ cookieManager.on('UserPreferencesSaved', preferences => {
       dtrum.disableSessionReplay();
     }
   }
-});
-
-cookieManager.on('PreferenceFormSubmitted', () => {
-  const message = document.querySelector('.cookie-preference-success') as HTMLElement;
-  message.style.display = 'block';
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-});
+}
 
 cookieManager.init({
-  userPreferences: {
-    cookieName: 'nfdiv-cookie-preferences',
-  },
-  preferencesForm: {
-    class: 'cookie-preferences-form',
-  },
-  cookieManifest: [
+  'user-preference-cookie-name': 'nfdiv-cookie-preferences',
+  'user-preference-saved-callback': cookiePreferencesUpdated,
+  'preference-form-id': 'cm-preference-form',
+  'preference-form-saved-callback': preferenceFormSaved,
+  'set-checkboxes-in-preference-form': true,
+  'cookie-banner-id': 'cm-cookie-banner',
+  'cookie-banner-visible-on-page-with-preference-form': false,
+  'cookie-banner-reject-callback': cookieBannerReject,
+  'cookie-banner-accept-callback': cookieBannerAccept,
+  'cookie-banner-saved-callback': cookieBannerSaved,
+  'cookie-banner-auto-hide': false,
+  'cookie-manifest': [
     {
-      categoryName: 'essential',
+      'category-name': 'essential',
       optional: false,
       cookies: ['nfdiv-cookie-preferences'],
     },
     {
-      categoryName: 'analytics',
+      'category-name': 'analytics',
+      optional: true,
       cookies: ['_ga', '_gid', 'gat'],
     },
     {
-      categoryName: 'apm',
+      'category-name': 'apm',
+      optional: true,
       cookies: ['dtCookie', 'dtLatC', 'dtPC', 'dtSa', 'dtValidationCookie', 'dtDisabled', 'rxVisitor', 'rxvt'],
     },
   ],
-  cookieBanner: {
-    class: 'cookie-banner',
-    showWithPreferencesForm: false,
-    actions: [
-      {
-        name: 'accept',
-        buttonClass: 'cookie-banner-accept-button',
-        confirmationClass: 'cookie-banner-accept-message',
-        consent: true,
-      },
-      {
-        name: 'reject',
-        buttonClass: 'cookie-banner-reject-button',
-        confirmationClass: 'cookie-banner-reject-message',
-        consent: false,
-      },
-      {
-        name: 'hide',
-        buttonClass: 'cookie-banner-hide-button',
-      },
-    ],
-  },
 });
 
 declare global {
