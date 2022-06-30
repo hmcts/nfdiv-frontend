e166f68ccb4be40d73c7bc9fa9dcac015291f95a
Daniel Bell <70265071+danielbe11@users.noreply.github.com>
2022-06-30 16:14:09 +0100

    NFDIV-2482 - Fix XB tests V3 (#2287)
    * Enable XB
    
    * Increase timeout
    
    * Lower Edge version
    
    * Accident
    
    * Try doubleClick
    
    * Click away before Continue
    
    * Change to double click

--- src/test/steps/happy-path.ts
+++ src/test/steps/happy-path.ts
@@ -162,8 +162,7 @@ Given("I've completed all happy path questions correctly", async () => {
   I.waitInUrl('/upload-your-documents');
   iClearTheForm();
   iClick('I cannot upload my original marriage certificate');
-  I.click('#uploadGroup');
-  iClick('Continue');
+  I.doubleClick('Continue');
 
   I.waitForText('Equality and diversity questions');
   iClick("I don't want to answer these questions");
