e1bda4cd70084fbf7a748adbd845839917378170
Lara <laratollington@gmail.com>
2022-07-26 11:50:45 +0100

    fix confirmYourJointApplication post controller

--- src/main/steps/applicant1/confirm-your-joint-application/post.ts
+++ src/main/steps/applicant1/confirm-your-joint-application/post.ts
@@ -13,7 +13,7 @@ export default class Applicant1ConfirmYourJointApplicationPostController extends
       formData.applicant1UsedWelshTranslationOnSubmission = YesOrNo.YES;
     }
 
-    return req.locals.api.triggerEvent(req.session.userCase.id, formData, eventName);
+    return super.save(req, formData, eventName);
   }
 
   protected getEventName(): string {
