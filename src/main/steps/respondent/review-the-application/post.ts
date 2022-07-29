bf6fe1984c347434465314a205eb972d239f21ad
danielbe11 <daniel.bell@solirius.com>
2022-07-29 14:18:20 +0100

    Add logic to ReviewTheApplicationPostController

--- src/main/steps/respondent/review-the-application/post.ts
+++ src/main/steps/respondent/review-the-application/post.ts
@@ -1,11 +1,16 @@
 import autobind from 'autobind-decorator';
 
-import { DRAFT_AOS } from '../../../app/case/definition';
+import { Checkbox } from '../../../app/case/case';
+import { DRAFT_AOS, UPDATE_AOS } from '../../../app/case/definition';
+import { AppRequest } from '../../../app/controller/AppRequest';
 import { AnyObject, PostController } from '../../../app/controller/PostController';
 
 @autobind
 export default class ReviewTheApplicationPostController extends PostController<AnyObject> {
-  protected getEventName(): string {
+  protected getEventName(req: AppRequest<AnyObject>): string {
+    if (req.session.userCase.confirmReadPetition === Checkbox.Checked) {
+      return UPDATE_AOS;
+    }
     return DRAFT_AOS;
   }
 }
