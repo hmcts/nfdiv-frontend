6b3240ba9b295bef3e68e78b5232b5471de22812
Lara <laratollington@gmail.com>
2022-08-04 10:44:34 +0100

    clean up

--- src/main/steps/url-utils.ts
+++ src/main/steps/url-utils.ts
@@ -34,13 +34,15 @@ export const isLinkingUrl = (reqPath: string): boolean =>
 
 export const isAosStep = (reqPath: string): boolean => {
   return [
-    `${RESPONDENT}${CHECK_ANSWERS_URL}`,
-    `${RESPONDENT}${DETAILS_OTHER_PROCEEDINGS}`,
-    `${RESPONDENT}${DISPUTING_THE_APPLICATION}`,
-    `${RESPONDENT}${HOW_DO_YOU_WANT_TO_RESPOND}`,
-    `${RESPONDENT}${HOW_THE_COURTS_WILL_CONTACT_YOU}`,
-    `${RESPONDENT}${LEGAL_JURISDICTION_OF_THE_COURTS}`,
-    `${RESPONDENT}${OTHER_COURT_CASES}`,
-    `${RESPONDENT}${REVIEW_THE_APPLICATION}`,
-  ].includes(reqPath as PageLink);
+    CHECK_ANSWERS_URL,
+    DETAILS_OTHER_PROCEEDINGS,
+    DISPUTING_THE_APPLICATION,
+    HOW_DO_YOU_WANT_TO_RESPOND,
+    HOW_THE_COURTS_WILL_CONTACT_YOU,
+    LEGAL_JURISDICTION_OF_THE_COURTS,
+    OTHER_COURT_CASES,
+    REVIEW_THE_APPLICATION,
+  ]
+    .map(url => RESPONDENT + url)
+    .includes(reqPath as PageLink);
 };
--- src/test/functional/features/respondent.feature
+++ src/test/functional/features/respondent.feature
@@ -14,7 +14,7 @@ Feature: Respondent
   Scenario: They fill out a happy path respondent journey
     And I enter my valid case reference and valid access code
     Then the page URL should be "/respondent/hub-page"
-    When I click "Respond to the apswwsplication"
+    When I click "Respond to the application"
     Then the page URL should be "/respondent/review-the-application"
     And the page should include "Review the divorce application"
 
