3c4627c0542179f3780509cd31798eddd8dc6834
danielbe11 <daniel.bell@solirius.com>
2022-08-04 12:43:05 +0100

    Increase coverage

--- src/main/steps/respondent/review-the-application/post.test.ts
+++ src/main/steps/respondent/review-the-application/post.test.ts
@@ -1,12 +1,15 @@
 import { mockRequest } from '../../../../test/unit/utils/mockRequest';
 import { mockResponse } from '../../../../test/unit/utils/mockResponse';
 import { Checkbox } from '../../../app/case/case';
-import { DRAFT_AOS, UPDATE_AOS } from '../../../app/case/definition';
+import { DRAFT_AOS, Gender, UPDATE_AOS } from '../../../app/case/definition';
+import { PostController } from '../../../app/controller/PostController';
 import { FormContent } from '../../../app/form/Form';
+import * as steps from '../../index';
 
 import ReviewTheApplicationPostController from './post';
 
 describe('ReviewTheApplicationPostController', () => {
+  const getNextStepUrlMock = jest.spyOn(steps, 'getNextStepUrl');
   global.structuredClone = jest.fn(val => {
     return JSON.parse(JSON.stringify(val));
   });
@@ -59,5 +62,32 @@ describe('ReviewTheApplicationPostController', () => {
     await reviewTheApplicationPostController.post(req, res);
 
     expect(req.locals.api.triggerEvent).toHaveBeenCalledWith('1234', body, UPDATE_AOS);
+  });
+
+  test('rejects with an error when unable to save session data in ReviewTheApplicationPostController', async () => {
+    const mockFormContent = {
+      fields: {
+        confirmReadPetition: {},
+      },
+    } as unknown as FormContent;
+
+    getNextStepUrlMock.mockReturnValue('/next-step-url');
+    const body = { gender: Gender.FEMALE };
+    const controller = new PostController(mockFormContent.fields);
+
+    const mockSave = jest.fn(done => done('An error while saving session'));
+    const req = mockRequest({ body, session: { save: mockSave } });
+    (req.locals.api.triggerEvent as jest.Mock).mockResolvedValueOnce({ gender: Gender.FEMALE });
+    const res = mockResponse();
+    await expect(controller.post(req, res)).rejects.toEqual('An error while saving session');
+
+    const userCase = {
+      ...req.session.userCase,
+      ...body,
+    };
+    expect(mockSave).toHaveBeenCalled();
+    expect(getNextStepUrlMock).toBeCalledWith(req, userCase);
+    expect(res.redirect).not.toHaveBeenCalled();
+    expect(req.session.errors).toStrictEqual([]);
   });
 });
