d24d17e94f246dc7f9df15cc8daa3cd1f57d69cf
FaisalMoJ <faisal.amin@justice.gov.uk>
2022-08-10 14:18:59 +0100

    NFDIV-2384: Add Welsh translation for joint application

--- src/main/steps/applicant1/withdrawing-your-application/content.ts
+++ src/main/steps/applicant1/withdrawing-your-application/content.ts
@@ -74,7 +74,7 @@ const en = ({ isDivorce, partner, isApplicant2 }: CommonContent) => ({
   exitLink: 'Exit service',
 });
 
-const cy = ({ isDivorce }: CommonContent) => ({
+const cy = ({ isDivorce, partner, isApplicant2 }: CommonContent) => ({
   title: `Tynnu eich ${isDivorce ? 'cais am ysgariad' : 'cais i ddod â’ch partneriaeth sifil i ben'} yn ôl`,
   sole: {
     line1: {
@@ -89,6 +89,61 @@ const cy = ({ isDivorce }: CommonContent) => ({
     line2:
       'Os oes arnoch angen help gallwch gysylltu â’r llys gan ddefnyddio’r manylion isod. Ni all y staff cynorthwyol roi cyngor cyfreithiol i chi.',
   },
+  joint: {
+    line1: {
+      part1: `Rydych wedi dweud nad ydych eisiau bwrw ymlaen â‘ch ${
+        isDivorce ? 'ysgariad' : 'cais i ddod â’ch partneriaeth sifil i ben'
+      }.
+      Os ydych eisiau tynnu eich cais yn ôl, yna mae angen i chi lenwi `,
+      part2: 'D11 form',
+      part3: 'ar y cyd a’i hanfon i’r llys. Mae’r ffurflen yn egluro i ble y dylid ei hanfon.',
+      link: 'https://www.gov.uk/government/publications/form-d11-application-notice',
+    },
+    continueAsSole: 'Os ydych am fwrw ymlaen fel yr unig geisydd',
+    firstInTimeApplicant: {
+      line1: `Os ydych yn dal i fod eisiau ${
+        isDivorce ? 'ysgaru' : 'dod â’ch partneriaeth sifil i ben'
+      } ond nad ydych yn meddwl y bydd eich
+      ${partner} yn cadarnhau ei fod/bod eisiau bwrw ymlaen, yna gallwch newid i gyflwyno cais unigol. Golyga hyn y bydd eich.
+      This means your ${partner} yn dod yn atebydd${isDivorce ? ' ar gyfer gweddill yr ysgariad' : ''}.`,
+      line2: {
+        part1: 'Dylech',
+        part2: 'barhau i wneud cais am orchymyn amodol ar y cyd.',
+        link: `${isApplicant2 ? '/applicant2' : ''}/continue-with-your-application`,
+      },
+      line3: `Os na fydd eich ${partner} hefyd yn gwneud cais o fewn 2 wythnos yna byddwch yn gallu newid i gais unigol ar-lein.
+       Byddwch yn cael e-bost gyda gwybodaeth am sut i newid eich cais, os na fydd yn gwneud cais.`,
+      line4: {
+        part1: 'Os ydych eisiau gwneud cais am orchymyn amodol fel yr unig geisydd nawr, yna bydd angen ichi',
+        part2: 'lwytho a llenwi ffurflen D84 bapur.',
+        link: 'https://www.gov.uk/government/publications/form-d84-application-for-a-decree-nisi-conditional-order-or-judicial-separation-decreeorder',
+      },
+      line5: `Mae manylion ar y ffurflen yn egluro i ble y dylid ei hanfon. Bydd eich cais yn cael ei gofnodi cyn gynted ag y bydd y llys yn derbyn y
+       ffurflen ond gallai gymryd hyd at dair wythnos i brosesu’r cais. Os ydych eisiau dewis yr opsiwn hwn, dylech adael y gwasanaeth.`,
+    },
+    secondInTimeApplicant: {
+      line1: {
+        part1: `Mae eich ${partner} eisoes wedi cadarnhau ei fod/bod am fwrw ymlaen â’ch cais ar y cyd. 
+        Y ffordd gyflymaf o fwrw ymlaen â’r ${isDivorce ? 'ysgariad' : 'cais i ddod â’ch partneriaeth sifil i ben'}
+        yw i chithau hefyd `,
+        part2: 'gadarnhau eich bod am fwrw ymlaen â’ch cais ar y cyd.',
+        link: `${isApplicant2 ? '/applicant2' : ''}/continue-with-your-application`,
+      },
+      line2: {
+        part1: `Os ydych eisiau newid i gais unigol, bydd hynny’n achosi oedi i’r ${
+          isDivorce ? 'ysgariad' : 'cais i ddod â’ch partneriaeth sifil i ben'
+        }. Bydd yn rhaid ichi wneud cais ar wahân drwy`,
+        part2: 'lwytho ffurflen bapur D84 a’i llenwi.',
+        link: 'https://www.gov.uk/government/publications/form-d84-application-for-a-decree-nisi-conditional-order-or-judicial-separation-decreeorder',
+      },
+      line3:
+        'Mae manylion ar y ffurflen yn egluro i ble y dylid ei hanfon. Dylech adael y gwasanaeth ac anfon y ffurflen i mewn os ydych eisiau newid i gais unigol.',
+      line4: `Y ffordd gyflymaf o fwrw ymlaen â’r 
+      ${isDivorce ? 'ysgariad' : 'cais i ddod â’ch partneriaeth sifil i ben'}
+      cadarnhau eich bod am fwrw ymlaen â’ch cais ar y cyd.`,
+    },
+  },
+  exitLink: 'Exit service',
 });
 
 const languages = {
