e866a30a7628f3c7d24854d18d78e5ec5089ab7e
Lara <laratollington@gmail.com>
2022-08-23 09:55:40 +0100

    Revert Welsh

--- src/main/steps/applicant1Sequence.ts
+++ src/main/steps/applicant1Sequence.ts
@@ -36,7 +36,6 @@ import {
   DO_THEY_HAVE_A_SOLICITOR,
   DO_YOU_HAVE_ADDRESS,
   EMAIL_RESENT,
-  ENGLISH_OR_WELSH,
   ENTER_SOLICITOR_DETAILS,
   ENTER_THEIR_ADDRESS,
   ENTER_YOUR_ADDRESS,
@@ -280,10 +279,6 @@ export const applicant1PreSubmissionSequence: Step[] = [
   },
   {
     url: HOW_THE_COURTS_WILL_CONTACT_YOU,
-    getNextStep: () => ENGLISH_OR_WELSH,
-  },
-  {
-    url: ENGLISH_OR_WELSH,
     getNextStep: () => ADDRESS_PRIVATE,
   },
   {
