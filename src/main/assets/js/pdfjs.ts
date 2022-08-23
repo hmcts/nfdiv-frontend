b0986cd6f01618b5a4827b2ca0b3c10e3a8a30db
taleb <taleb.benouaer@hmcts.net>
2022-08-23 13:02:11 +0100

    nfdiv-2588: show prev/next page

--- src/main/assets/js/pdfjs.ts
+++ src/main/assets/js/pdfjs.ts
@@ -1,37 +1,89 @@
-import { Logger } from '@hmcts/nodejs-logging';
 import * as pdfjsLib from 'pdfjs-dist';
 import 'pdfjs-dist/build/pdf.worker';
 
 pdfjsLib.GlobalWorkerOptions.workerSrc = '/assets/pdf/pdf.worker.js';
 
-const logger = Logger.getLogger('pdfjs');
-
 pdfjsLib.getDocument('/downloads/respondent-answers').promise.then(
   async pdfDocument => {
-    logger.info('PDF loaded', pdfDocument.numPages);
+    const pdf = new PdfDocument(pdfDocument);
+    await pdf.load();
+  },
+  err => console.log(err)
+);
 
-    const page = await pdfDocument.getPage(1);
-    logger.info('Page loaded');
+class PdfDocument {
+  container: HTMLDivElement | undefined;
+  currentPage = 1;
+  pageAwaitingRender: number | undefined;
+  renderInProgress = false;
+
+  constructor(private pdf) {}
+
+  async load() {
+    console.log('PDF loaded', this.pdf.numPages);
+    this.container = document.getElementById('pdf-container') as HTMLDivElement;
+    if (this.container) {
+      if (this.pdf.numPages > 1) {
+        const prevLink = this.container.querySelector('#prev-page') as HTMLButtonElement;
+        prevLink.removeAttribute('hidden');
+        prevLink.onclick = () => this.renderPrevPage();
+
+        const nextLink = this.container.querySelector('#next-page') as HTMLButtonElement;
+        nextLink.removeAttribute('hidden');
+        nextLink.onclick = () => this.renderNextPage();
+      }
+      await this.renderPage();
+    }
+  }
+
+  async renderPrevPage(): Promise<void> {
+    if (this.currentPage <= 1) {
+      return;
+    }
+    this.currentPage--;
+    await this.queuePageForRender();
+  }
+
+  async renderNextPage(): Promise<void> {
+    if (this.currentPage >= this.pdf.numPages) {
+      return;
+    }
+    this.currentPage++;
+    await this.queuePageForRender();
+  }
+
+  async queuePageForRender(): Promise<void> {
+    if (this.renderInProgress) {
+      this.pageAwaitingRender = this.currentPage;
+    } else {
+      await this.renderPage();
+    }
+  }
+
+  async renderPage() {
+    this.renderInProgress = true;
+    const page = await this.pdf.getPage(this.currentPage);
+    const canvas = this.container?.getElementsByTagName('canvas').item(0) as HTMLCanvasElement;
 
     const viewport = page.getViewport({ scale: 1.5 });
-    const canvas = document.getElementById('pdf-canvas') as HTMLCanvasElement;
-    if (canvas) {
-      const context = canvas.getContext('2d');
-      canvas.height = viewport.height;
-      canvas.width = viewport.width;
+    canvas.height = viewport.height;
+    canvas.width = viewport.width;
 
-      if (context) {
-        const renderContext = {
-          canvasContext: context,
-          viewport,
-        };
-
-        await page.render(renderContext);
-        logger.info('Page rendered');
+    const context = canvas.getContext('2d');
+    if (context) {
+      const renderContext = {
+        canvasContext: context,
+        viewport,
+      };
+      await page.render(renderContext);
+      this.renderInProgress = false;
+      if (this.pageAwaitingRender) {
+        this.currentPage = this.pageAwaitingRender;
+        await this.renderPage();
       }
+      this.container?.removeAttribute('hidden');
+
+      console.log('Page rendered', this.currentPage);
     }
-  },
-  err => {
-    logger.error(err);
   }
-);
+}
