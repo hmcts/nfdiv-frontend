042d007684a8eb70fdb889fdc89ac3a8e731f04e
taleb <tech@benouaer.com>
2022-09-05 12:38:19 +0100

    nfdiv-2794: add feature toggle

--- src/main/app/case/case-api.ts
+++ src/main/app/case/case-api.ts
@@ -40,13 +40,15 @@ export class CaseApi {
     logger: LoggerInstance
   ): Promise<{ existingUserCase: CaseWithId | false; newInviteUserCase: CaseWithId | false }> {
     const existingUserCase: CaseWithId | false = await this.getExistingUserCase(serviceType);
-
     const newInviteUserCase = await this.getNewInviteCase(email, serviceType, logger);
-    if (existingUserCase && newInviteUserCase) {
-      return {
-        existingUserCase,
-        newInviteUserCase: newInviteUserCase.id !== existingUserCase.id ? newInviteUserCase : false,
-      };
+
+    if (process.env.ENV_TYPE !== 'PROD') {
+      if (existingUserCase && newInviteUserCase) {
+        return {
+          existingUserCase,
+          newInviteUserCase: newInviteUserCase.id !== existingUserCase.id ? newInviteUserCase : false,
+        };
+      }
     }
     return { existingUserCase, newInviteUserCase };
   }
