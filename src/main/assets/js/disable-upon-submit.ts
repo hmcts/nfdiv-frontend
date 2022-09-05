d9178db6d3d7b44335136125b6ccbb0d6018c907
Lara <laratollington@gmail.com>
2022-09-05 17:05:43 +0100

    increase timeout to 5 seconds

--- src/main/assets/js/disable-upon-submit.ts
+++ src/main/assets/js/disable-upon-submit.ts
@@ -1,17 +1,19 @@
 import { DISABLE_UPON_SUBMIT } from '../../steps/common/content.utils';
 
-const DEBOUNCE_TIMEOUT_IN_SECONDS = 3;
+const DEBOUNCE_TIMEOUT_IN_SECONDS = 5;
 let debounceFormSubmitTimer: number | ReturnType<typeof setTimeout> = 0;
 
-const submitButton = document.getElementsByClassName(DISABLE_UPON_SUBMIT)[0] as Element;
+const submitButtons = document.getElementsByClassName(DISABLE_UPON_SUBMIT);
 
-submitButton?.addEventListener('click', event => {
-  if (debounceFormSubmitTimer && debounceFormSubmitTimer > 0) {
-    event.preventDefault();
-    return false;
-  }
+Array.from(submitButtons).forEach(button => {
+  button?.addEventListener('click', event => {
+    if (debounceFormSubmitTimer && debounceFormSubmitTimer > 0) {
+      event.preventDefault();
+      return false;
+    }
 
-  debounceFormSubmitTimer = setTimeout(function () {
-    debounceFormSubmitTimer = 0;
-  }, DEBOUNCE_TIMEOUT_IN_SECONDS * 1000);
+    debounceFormSubmitTimer = setTimeout(function () {
+      debounceFormSubmitTimer = 0;
+    }, DEBOUNCE_TIMEOUT_IN_SECONDS * 1000);
+  });
 });
