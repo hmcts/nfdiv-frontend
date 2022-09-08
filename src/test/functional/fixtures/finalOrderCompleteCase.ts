d535c306181343b11c2979051d0d635cf6857040
FaisalMoJ <faisal.amin@justice.gov.uk>
2022-09-08 13:49:33 +0100

    NFDIV-1620: Fix functional test

--- src/test/functional/fixtures/finalOrderCompleteCase.ts
+++ src/test/functional/fixtures/finalOrderCompleteCase.ts
@@ -1,3 +1,4 @@
+import { YesOrNo } from '../../../main/app/case/definition';
 import { BrowserCase } from '../../steps/common';
 
 import { completeCase } from './completeCase';
@@ -5,4 +6,5 @@ import { completeCase } from './completeCase';
 export const finalOrderCompleteCase: Partial<BrowserCase> = {
   ...completeCase,
   dateFinalOrderNoLongerEligible: '2030-10-01',
+  applicant1AppliedForFinalOrderFirst: YesOrNo.YES,
 };
