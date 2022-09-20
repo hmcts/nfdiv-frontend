7acaada84a0ef335d89458821d7ffa284c7b32b4
Lara <laratollington@gmail.com>
2022-09-20 14:37:17 +0100

    fix eslint errors

--- src/main/app/access-code/AccessCodePostController.test.ts
+++ src/main/app/access-code/AccessCodePostController.test.ts
@@ -197,7 +197,7 @@ describe('AccessCodePostController', () => {
         propertyName: 'accessCode',
       },
     ]);
-    expect(req.locals.logger.error).toBeCalled();
+    expect(req.locals.logger.error).toHaveBeenCalled();
   });
 
   test('Should return error when case reference is invalid and should redirect to the same page', async () => {
