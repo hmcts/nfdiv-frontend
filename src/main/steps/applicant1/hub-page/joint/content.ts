ad68c96f609121873b36fa8a9824160632f14ae5
FaisalMoJ <faisal.amin@justice.gov.uk>
2022-09-29 11:49:48 +0100

    NFDIV-1790: Fix text when dissolution

--- src/main/steps/applicant1/hub-page/joint/content.ts
+++ src/main/steps/applicant1/hub-page/joint/content.ts
@@ -196,8 +196,8 @@ const cy: typeof en = ({ isDivorce, userCase, partner, isApplicant2 }: CommonCon
     line1: 'Your final order has been granted. You are now legally divorced.',
     line2: {
       part1: 'Download a copy of your ‘final order’',
-      part2:
-        '. This is the document that shows your marriage is legally ended. You may need it so you should save a copy for your records.',
+      part2: `. This is the document that shows your ${isDivorce ? 'marriage' : 'civil partnership'} is legally ended. 
+        You may need it so you should save a copy for your' records.`,
       link: '/downloads/final-order-granted',
       reference: 'Final-Order-Granted',
     },
