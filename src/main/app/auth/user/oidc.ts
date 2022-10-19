3ce11715fcd184d29cb14390dca5b7d2286f1d27
danielbe11 <daniel.bell@solirius.com>
2022-10-19 14:12:09 +0100

    Fix

--- src/main/app/auth/user/oidc.ts
+++ src/main/app/auth/user/oidc.ts
@@ -49,7 +49,7 @@ export const getSystemUser = async (): Promise<UserDetails> => {
   const systemPassword: string = config.get('services.idam.systemPassword');
 
   let response;
-  const isCachingEnabled = Boolean(config.get('service.idam.caching'));
+  const isCachingEnabled = Boolean(config.get('services.idam.caching'));
   if (isCachingEnabled && idamTokenCache.get(systemUsername)) {
     logger.info('Fetching systemUsername from cache...');
     response = idamTokenCache.get(systemUsername);
