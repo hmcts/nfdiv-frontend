d090a3edac6fefef31e5f6bb7849cc6ee2bd09bf
danielbe11 <daniel.bell@solirius.com>
2022-10-19 11:37:29 +0100

    Add oidc tests

--- src/main/app/auth/user/oidc.test.ts
+++ src/main/app/auth/user/oidc.test.ts
@@ -1,14 +1,19 @@
 import Axios, { AxiosStatic } from 'axios';
 
+import { IdamUserManager, idamTokenCache } from '../../../../test/steps/IdamUserManager';
 import { APPLICANT_2_SIGN_IN_URL, CALLBACK_URL, SIGN_IN_URL } from '../../../steps/urls';
 
 import { getRedirectUrl, getSystemUser, getUserDetails } from './oidc';
 
 jest.mock('axios');
+jest.mock('node-cache');
+
+const getAccessTokenFromIdamMock = jest.spyOn(IdamUserManager, 'getAccessTokenFromIdam');
+const idamCacheGetMock = jest.spyOn(idamTokenCache, 'get');
 
 const mockedAxios = Axios as jest.Mocked<AxiosStatic>;
 
-const token =
+let token =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0ZXN0QHRlc3QuY29tIiwiZ2l2ZW5fbmFtZSI6IkpvaG4iLCJmYW1pbHlfbmFtZSI6IkRvcmlhbiIsInVpZCI6IjEyMyIsInJvbGVzIjpbImNpdGl6ZW4iXX0.rxjx6XsSNNYavVppwKAqWiNWT_GxN4vjVzdLRe6q14I';
 
 describe('getRedirectUrl', () => {
@@ -64,5 +69,88 @@ describe('getCaseWorkerUser', () => {
       id: '123',
       roles: ['citizen'],
     });
+  });
+});
+
+token =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0ZXN0QHRlc3QuY29tIiwiZ2l2ZW5fbmFtZSI6IkpvaG4iLCJmYW1pbHlfbmFtZSI6IkRvcmlhbiIsInVpZCI6IjEyMyIsInJvbGVzIjpbImNhc2V3b3JrZXItZGl2b3JjZS1zeXN0ZW11cGRhdGUiLCJjYXNld29ya2VyLWNhYSIsImNhc2V3b3JrZXIiLCJjYXNld29ya2VyLWRpdm9yY2UiXX0.NDab3XAV8NWQTuuxBQ9mpwTIdw4KMWWiJ37Dp3EHG7s';
+
+describe('getSystemUser', () => {
+  test('In non-production mode sets cache when user token has not been saved', async () => {
+    process.env.NODE_ENV = 'development';
+    (idamCacheGetMock as jest.Mock).mockReturnValue(undefined);
+    getAccessTokenFromIdamMock.mockResolvedValue({
+      status: 200,
+      data: {
+        id_token: token,
+        access_token: token,
+      },
+      statusText: 'wsssw',
+      headers: { test: 'now' },
+      config: {},
+    });
+
+    const expected = await getSystemUser();
+    expect(expected).toEqual({
+      accessToken: token,
+      email: 'test@test.com',
+      givenName: 'John',
+      familyName: 'Dorian',
+      id: '123',
+      roles: ['caseworker-divorce-systemupdate', 'caseworker-caa', 'caseworker', 'caseworker-divorce'],
+    });
+    expect(idamTokenCache.get).toHaveBeenCalledWith('dummy_user');
+    expect(idamTokenCache.get).toHaveBeenCalledTimes(1);
+
+    expect(idamTokenCache.set).toHaveBeenCalledWith('dummy_user', { data: { access_token: token, id_token: token } });
+    expect(idamTokenCache.set).toHaveBeenCalledTimes(1);
+  });
+
+  test('In non-production mode uses cache when user token has already been saved', async () => {
+    process.env.NODE_ENV = 'development';
+    (idamCacheGetMock as jest.Mock).mockReturnValue({
+      data: {
+        id_token: token,
+        access_token: token,
+      },
+    });
+
+    const expected = await getSystemUser();
+    expect(expected).toEqual({
+      accessToken: token,
+      email: 'test@test.com',
+      givenName: 'John',
+      familyName: 'Dorian',
+      id: '123',
+      roles: ['caseworker-divorce-systemupdate', 'caseworker-caa', 'caseworker', 'caseworker-divorce'],
+    });
+    expect(idamTokenCache.get).toHaveBeenCalledWith('dummy_user');
+    expect(idamTokenCache.get).toHaveBeenCalledTimes(2);
+  });
+
+  test('Creates a new token when in production mode', async () => {
+    process.env.NODE_ENV = 'production';
+    getAccessTokenFromIdamMock.mockResolvedValue({
+      status: 200,
+      data: {
+        id_token: token,
+        access_token: token,
+      },
+      statusText: 'wsssw',
+      headers: { test: 'now' },
+      config: {},
+    });
+
+    const expected = await getSystemUser();
+    expect(expected).toEqual({
+      accessToken: token,
+      email: 'test@test.com',
+      givenName: 'John',
+      familyName: 'Dorian',
+      id: '123',
+      roles: ['caseworker-divorce-systemupdate', 'caseworker-caa', 'caseworker', 'caseworker-divorce'],
+    });
+    expect(idamTokenCache.get).toHaveBeenCalledTimes(1);
+    expect(idamTokenCache.set).toHaveBeenCalledTimes(0);
   });
 });
