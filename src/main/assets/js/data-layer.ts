32e3e5a808c4d4f0edc25321e368715462ce0918
taleb <tech@benouaer.com>
2022-10-27 18:01:58 +0100

    nfdiv-2362: add errors to dataLayer for analytics

--- src/main/assets/js/data-layer.ts
+++ src/main/assets/js/data-layer.ts
@@ -1,2 +1,14 @@
 const dataLayer = window.dataLayer || [];
 dataLayer.push({ event: 'Site language', language: document.documentElement.lang });
+
+const errorList: HTMLUListElement | null = document.querySelector('.govuk-error-summary__list');
+if (errorList) {
+  const listElements = errorList.getElementsByTagName('li');
+  for (let i = 0; i < listElements.length; i++) {
+    dataLayer.push({
+      event: 'Error shown',
+      errormsg: listElements.item(i)?.firstElementChild?.textContent,
+    });
+    console.log('error shown', listElements.item(i)?.firstElementChild?.textContent);
+  }
+}
