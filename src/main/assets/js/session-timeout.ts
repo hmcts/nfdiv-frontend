4b31077a31541cc55a054cbb31eca40c626fb735
taleb <tech@benouaer.com>
2022-11-09 19:27:00 +0000

    nfdiv-2521: content update

--- src/main/assets/js/session-timeout.ts
+++ src/main/assets/js/session-timeout.ts
@@ -3,79 +3,87 @@ import { throttle } from 'lodash';
 import { PageLink, TIMED_OUT_URL, WEBCHAT_URL } from '../../steps/urls';
 
 const eventTimer = 5 * 60 * 1000; // 5 minutes
-const TIMEOUT_NOTICE = 120 * 1000; // 2 minutes
-const timeoutDialog = document.getElementById('timeout-modal-container');
 
-let timeout, noticeTimeout;
+class SessionTimeout {
+  TWELVE_HOURS = 12 * 60 * 60 * 1000;
+  TWENTY_MINUTES = 20 * 60 * 1000;
+  TIMEOUT_NOTICE = 120 * 1000; // 2 minutes
+  sessionTimeoutInterval: number = this.getSessionTimeoutInterval();
+  timeout;
+  notificationTimer;
 
-const scheduleSessionTimeout = () => {
-  clearTimeout(timeout);
-  timeout = setTimeout(() => {
-    saveSessionAndRedirect();
-  }, getSessionTimeoutInterval());
-};
+  notificationPopup = document.getElementById('timeout-modal-container');
+  popupCloseBtn = this.notificationPopup?.querySelector('#timeout-modal-close-button');
+  form = document.getElementById('main-form');
 
-const scheduleTimeoutNotice = () => {
-  clearTimeout(noticeTimeout);
-  noticeTimeout = setTimeout(() => {
-    timeoutDialog?.removeAttribute('hidden');
-  }, getSessionTimeoutInterval() - TIMEOUT_NOTICE);
-};
-
-const scheduleTimeouts = () => {
-  clearTimeout(noticeTimeout);
-  document.getElementById('timeout-modal-close-button')?.addEventListener('click', () => {
-    timeoutDialog?.setAttribute('hidden', 'hidden');
-    scheduleTimeoutNotice();
-    scheduleSessionTimeout();
-    pingUserActive();
-  });
-  scheduleTimeoutNotice();
-  scheduleSessionTimeout();
-};
-
-const saveSessionAndRedirect = async () => {
-  const form = document.getElementById('main-form');
-  if (form) {
-    const formData = new FormData(form as HTMLFormElement);
-    formData.append('saveBeforeSessionTimeout', 'true');
-    const url = window.location.pathname;
-    const csrf = formData.get('_csrf') as string;
-    await fetch(url, {
-      method: 'POST',
-      headers: {
-        'csrf-token': csrf,
-      },
-      body: new URLSearchParams(formData as unknown as Record<string, string>),
+  schedule(): void {
+    this.popupCloseBtn?.addEventListener('click', () => {
+      this.showNotificationPopup(false);
+      this.scheduleSignOut();
+      this.pingUserActive();
     });
-  }
-  window.location.href = `${TIMED_OUT_URL}?lng=${document.documentElement.lang}`;
-};
-
-const pingUserActive = throttle(
-  () => {
-    fetch('/active').then(() => {
-      scheduleSessionTimeout();
-    });
-  },
-  eventTimer,
-  { trailing: false }
-);
-
-const getSessionTimeoutInterval = (): number => {
-  const twelveHours: number = 12 * 60 * 60 * 1000;
-  const twentyMinutes: number = 20 * 60 * 1000;
-
-  const timeoutParam = new URL(location.href).searchParams.get('timeout');
-  if (process.env.NODE_ENV === 'development' && timeoutParam && !isNaN(parseInt(timeoutParam))) {
-    return parseInt(timeoutParam) > twentyMinutes ? twentyMinutes : Math.abs(parseInt(timeoutParam));
+    this.scheduleSignOut();
   }
 
-  return [WEBCHAT_URL, TIMED_OUT_URL].includes(window.location.pathname as PageLink) ? twelveHours : twentyMinutes;
-};
+  scheduleSignOut(): void {
+    this.scheduleNotificationPopup();
+    clearTimeout(this.timeout);
+    this.timeout = setTimeout(this.saveSessionAndRedirect, this.sessionTimeoutInterval);
+  }
+
+  scheduleNotificationPopup(): void {
+    clearTimeout(this.notificationTimer);
+    this.notificationTimer = setTimeout(
+      () => this.showNotificationPopup(true),
+      this.sessionTimeoutInterval - this.TIMEOUT_NOTICE
+    );
+  }
+
+  showNotificationPopup(visible: boolean): void {
+    if (visible) {
+      this.notificationPopup?.removeAttribute('hidden');
+    } else {
+      this.notificationPopup?.setAttribute('hidden', 'hidden');
+    }
+  }
+
+  async saveSessionAndRedirect(): Promise<void> {
+    if (this.form) {
+      const formData = new FormData(this.form as HTMLFormElement);
+      formData.append('saveBeforeSessionTimeout', 'true');
+      const url = window.location.pathname;
+      const csrf = formData.get('_csrf') as string;
+      await fetch(url, {
+        method: 'POST',
+        headers: { 'csrf-token': csrf },
+        body: new URLSearchParams(formData as unknown as Record<string, string>),
+      });
+    }
+    window.location.href = `${TIMED_OUT_URL}?lng=${document.documentElement.lang}`;
+  }
+
+  pingUserActive(): void {
+    throttle(() => fetch('/active').then(this.scheduleSignOut), eventTimer, { trailing: false });
+  }
+
+  getSessionTimeoutInterval(): number {
+    const timeoutParam = new URL(location.href).searchParams.get('timeout');
+    if (process.env.NODE_ENV === 'development' && timeoutParam && !isNaN(parseInt(timeoutParam))) {
+      return parseInt(timeoutParam) > this.TWENTY_MINUTES ? this.TWENTY_MINUTES : Math.abs(parseInt(timeoutParam));
+    }
+
+    return [WEBCHAT_URL, TIMED_OUT_URL].includes(window.location.pathname as PageLink)
+      ? this.TWELVE_HOURS
+      : this.TWENTY_MINUTES;
+  }
+}
+
+const sessionTimeout = new SessionTimeout();
 
 setTimeout(() => {
-  ['click', 'touchstart', 'mousemove', 'keypress'].forEach(evt => document.addEventListener(evt, pingUserActive));
+  ['click', 'touchstart', 'mousemove', 'keypress'].forEach(evt =>
+    document.addEventListener(evt, sessionTimeout.pingUserActive)
+  );
 }, eventTimer);
 
-scheduleTimeouts();
+sessionTimeout.schedule();
