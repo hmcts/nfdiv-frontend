048b07b86285cb123d0f22a05afa0900c26f9ca3
Barry Weeks <barry.weeks@justice.gov.uk>
2022-11-30 10:57:42 +0000

    NFDiv-2697 Refactor finalising-your-application/post.ts

--- src/main/steps/applicant1/finalising-your-application/post.ts
+++ src/main/steps/applicant1/finalising-your-application/post.ts
@@ -1,5 +1,4 @@
 import autobind from 'autobind-decorator';
-import { Response } from 'express';
 
 import { Case, CaseWithId } from '../../../app/case/case';
 import {
@@ -10,13 +9,11 @@ import {
 } from '../../../app/case/definition';
 import { AppRequest } from '../../../app/controller/AppRequest';
 import { AnyObject, PostController } from '../../../app/controller/PostController';
-import { Form } from '../../../app/form/Form';
 import { getSwitchToSoleFoStatus } from '../../common/switch-to-sole-content.utils';
 import { APPLICANT_2, FINALISING_YOUR_APPLICATION } from '../../urls';
 
 @autobind
 export default class FinalisingYourApplicationPostController extends PostController<AnyObject> {
-  private formData: Partial<Case> = {};
   protected async save(req: AppRequest<AnyObject>, formData: Partial<Case>, eventName: string): Promise<CaseWithId> {
     if (req.session.lang === 'cy') {
       if (req.session.isApplicant2) {
@@ -34,8 +31,7 @@ export default class FinalisingYourApplicationPostController extends PostControl
       (req.session.errors === undefined || req.session.errors.length === 0) &&
       req.session.isApplicant2 &&
       req.originalUrl === APPLICANT_2 + FINALISING_YOUR_APPLICATION &&
-      req.session.userCase.finalOrderSwitchedToSole === YesOrNo.YES &&
-      this.formData.doesApplicant2WantToApplyForFinalOrder;
+      req.session.userCase.finalOrderSwitchedToSole === YesOrNo.YES;
 
     if (hasApplicant2SwitchedToSoleFo) {
       req.session.isApplicant2 = false;
@@ -43,17 +39,6 @@ export default class FinalisingYourApplicationPostController extends PostControl
     }
 
     return super.getNextUrl(req);
-  }
-
-  protected async saveAndContinue(
-    req: AppRequest<AnyObject>,
-    res: Response,
-    form: Form,
-    formData: Partial<Case>
-  ): Promise<void> {
-    this.formData = formData;
-
-    return super.saveAndContinue(req, res, form, formData);
   }
 
   protected getEventName(req: AppRequest<AnyObject>): string {
