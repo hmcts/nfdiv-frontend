6918d9e33538d06e066d3536e8a39af622a5d07f
danielbe11 <daniel.bell@solirius.com>
2022-12-20 15:15:43 +0000

    Add more tests

--- src/main/app/controller/controller.utils.test.ts
+++ src/main/app/controller/controller.utils.test.ts
@@ -1,7 +1,9 @@
-import { CaseWithId } from '../case/case';
-import { DivorceOrDissolution, State } from '../case/definition';
+import { mockRequest } from '../../../test/unit/utils/mockRequest';
+import { SupportedLanguages } from '../../modules/i18n';
+import { Case, CaseWithId } from '../case/case';
+import { DivorceOrDissolution, State, YesOrNo } from '../case/definition';
 
-import { needsToExplainDelay } from './controller.utils';
+import { addWelshTranslationUponSubmissionFormData, needsToExplainDelay } from './controller.utils';
 
 describe('Controller utils', () => {
   describe('needsToExplainDelay', () => {
@@ -24,6 +26,37 @@ describe('Controller utils', () => {
       userCase.state = State.AwaitingFinalOrder;
       const result = needsToExplainDelay(userCase);
       expect(result).toBe(false);
+    });
+  });
+
+  describe('addWelshTranslationUponSubmissionFormData', () => {
+    let formData;
+    beforeEach(() => {
+      formData = {} as Partial<Case>;
+    });
+
+    it('adds app1 relevant form data (English)', () => {
+      const req = mockRequest({ session: { isApplicant2: false, lang: SupportedLanguages.En } });
+      const result = addWelshTranslationUponSubmissionFormData(formData, req.session);
+      expect(result).toEqual({ applicant1UsedWelshTranslationOnSubmission: YesOrNo.NO });
+    });
+
+    it('adds app1 relevant form data (Welsh)', () => {
+      const req = mockRequest({ session: { isApplicant2: false, lang: SupportedLanguages.Cy } });
+      const result = addWelshTranslationUponSubmissionFormData(formData, req.session);
+      expect(result).toEqual({ applicant1UsedWelshTranslationOnSubmission: YesOrNo.YES });
+    });
+
+    it('adds app2 relevant form data (English)', () => {
+      const req = mockRequest({ session: { isApplicant2: true, lang: SupportedLanguages.En } });
+      const result = addWelshTranslationUponSubmissionFormData(formData, req.session);
+      expect(result).toEqual({ applicant2UsedWelshTranslationOnSubmission: YesOrNo.NO });
+    });
+
+    it('adds app2 relevant form data (Welsh)', () => {
+      const req = mockRequest({ session: { isApplicant2: true, lang: SupportedLanguages.Cy } });
+      const result = addWelshTranslationUponSubmissionFormData(formData, req.session);
+      expect(result).toEqual({ applicant2UsedWelshTranslationOnSubmission: YesOrNo.YES });
     });
   });
 });
