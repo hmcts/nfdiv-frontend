afb1ef006f34738e84ddb51f43093caace48cda9
Aaron Moriarty <95974886+amoriartyMOJ@users.noreply.github.com>
2023-06-02 08:30:07 +0100

    Revert "Revert "NFDIV 3294: Remove use of docstore CDAM flag" (#2978)" (#2979)
    This reverts commit 8347705ace3ddd91371552a95f708b08a0648bc9.

--- src/main/app/document/DocumentManagementController.test.ts
+++ src/main/app/document/DocumentManagementController.test.ts
@@ -5,9 +5,9 @@ import { CITIZEN_APPLICANT2_UPDATE, CITIZEN_UPDATE, State } from '../case/defini
 
 import { DocumentManagerController } from './DocumentManagementController';
 
-const { mockCreate, mockDelete } = require('./DocumentManagementClient');
+const { mockCreate, mockDelete } = require('./CaseDocumentManagementClient');
 
-jest.mock('../document/DocumentManagementClient');
+jest.mock('../document/CaseDocumentManagementClient');
 
 describe('DocumentManagerController', () => {
   const documentManagerController = new DocumentManagerController();
