42001dd440ddf4c4cb21c203e4698fa696c9eb8b
Linus Norton <linusnorton@gmail.com>
2023-06-21 10:22:09 +0100

    fix redis6 secret name

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -44,7 +44,7 @@ module "nfdiv-frontend-redis6" {
   common_tags  = var.common_tags
 }
 
-resource "azurerm_key_vault_secret" "redis_access_key" {
+resource "azurerm_key_vault_secret" "redis6_access_key" {
   name         = "redis6-access-key"
   value        = module.nfdiv-frontend-redis6.access_key
   key_vault_id = data.azurerm_key_vault.key_vault.id
