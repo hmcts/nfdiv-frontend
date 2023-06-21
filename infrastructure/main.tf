ccc0bf80f4d678dd718f14dc497988d5ee75e544
Linus Norton <linusnorton@gmail.com>
2023-06-21 10:14:29 +0100

    Add side-by-side redis

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -31,3 +31,21 @@ resource "azurerm_key_vault_secret" "redis_access_key" {
   value        = module.nfdiv-frontend-session-storage.access_key
   key_vault_id = data.azurerm_key_vault.key_vault.id
 }
+
+module "nfdiv-frontend-redis6" {
+  source   = "git@github.com:hmcts/cnp-module-redis?ref=master"
+  product  = "${var.product}-${var.component}-redis"
+  location = var.location
+  env      = var.env
+  private_endpoint_enabled = true
+  redis_version = "6"
+  business_area = "cft"
+  public_network_access_enabled = false
+  common_tags  = var.common_tags
+}
+
+resource "azurerm_key_vault_secret" "redis_access_key" {
+  name         = "redis6-access-key"
+  value        = module.nfdiv-frontend-redis6.access_key
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+}
