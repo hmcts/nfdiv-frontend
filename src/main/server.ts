0157ff48e2ffa6638c9782eca15865196dfc0819
Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
2023-09-25 13:43:39 +0100

    Move body-parser after express-http-proxy

--- src/main/server.ts
+++ src/main/server.ts
@@ -33,8 +33,6 @@ const app = express();
 
 app.locals.developmentMode = process.env.NODE_ENV !== 'production';
 app.use(favicon(path.join(__dirname, '/public/assets/images/favicon.ico')));
-app.use(bodyParser.json() as RequestHandler);
-app.use(bodyParser.urlencoded({ extended: false }) as RequestHandler);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   res.setHeader('Cache-Control', 'no-cache, max-age=0, must-revalidate, no-store');
@@ -52,8 +50,10 @@ new AppInsights().enable();
 new SessionStorage().enableFor(app, logger);
 new TooBusy().enableFor(app);
 new HealthCheck().enableFor(app);
-new CSRFToken().enableFor(app);
 new DocumentDownloadMiddleware().enableFor(app);
+app.use(bodyParser.json() as RequestHandler);
+app.use(bodyParser.urlencoded({ extended: false }) as RequestHandler);
+new CSRFToken().enableFor(app);
 new LanguageToggle().enableFor(app);
 new AuthProvider().enable();
 new FeesRegister().enable();
