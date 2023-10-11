bf079249f50768f5d6ffb8d759bfc1166f847c40
mokainos <72137340+mokainos@users.noreply.github.com>
2023-10-11 15:22:38 +0100

    update (#3280)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -7,21 +7,25 @@ locals {
 }
 
 data "azurerm_key_vault" "key_vault" {
-  name = local.vaultName
+  name                = local.vaultName
   resource_group_name = local.vaultName
 }
 
 module "nfdiv-frontend-redis6" {
-  source   = "git@github.com:hmcts/cnp-module-redis?ref=master"
-  product  = var.product
-  name     = "${var.product}-${var.component}-${var.env}"
-  location = var.location
-  env      = var.env
-  private_endpoint_enabled = true
-  redis_version = "6"
-  business_area = "cft"
+  source                        = "git@github.com:hmcts/cnp-module-redis?ref=master"
+  product                       = var.product
+  name                          = "${var.product}-${var.component}-${var.env}"
+  location                      = var.location
+  env                           = var.env
+  private_endpoint_enabled      = true
+  redis_version                 = "6"
+  business_area                 = "cft"
   public_network_access_enabled = false
-  common_tags  = var.common_tags
+  common_tags                   = var.common_tags
+  sku_name                      = var.sku_name
+  family                        = var.family
+  capacity                      = var.capacity
+
 }
 
 resource "azurerm_key_vault_secret" "redis6_access_key" {
