acdc1e61f4f4a4b82da5b857d6ef0e678031818d
Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
2023-10-27 19:49:39 +0100

    NFDIV-3751: Address review comment

--- src/main/app/case/case-api-client.ts
+++ src/main/app/case/case-api-client.ts
@@ -128,10 +128,7 @@ export class CaseApiClient {
     } catch (err) {
       if (
         retries < this.maxRetries &&
-        (err?.response.status === 409 ||
-          err?.response.status === 422 ||
-          err?.response.status === 502 ||
-          err?.response.status === 504)
+        ([409, 422, 502, 504].includes(err?.response.status))
       ) {
         ++retries;
         this.logger.info(`retrying send event due to ${err.response.status}. this is retry no (${retries})`);
