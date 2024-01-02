07180fb4d995c080ce7c6ac85237c25b6f6c6b51
FaisalMoJ <faisal.amin@justice.gov.uk>
2024-01-02 10:17:15 +0000

    NFDIV-3847: Reset statementt of truth on save and sign out

--- src/main/steps/respondent/check-your-answers/post.ts
+++ src/main/steps/respondent/check-your-answers/post.ts
@@ -1,9 +1,11 @@
 import autobind from 'autobind-decorator';
+import { Response } from 'express';
 
-import { Case, CaseWithId } from '../../../app/case/case';
+import { Case, CaseWithId, Checkbox } from '../../../app/case/case';
 import { SUBMIT_AOS, YesOrNo } from '../../../app/case/definition';
 import { AppRequest } from '../../../app/controller/AppRequest';
 import { AnyObject, PostController } from '../../../app/controller/PostController';
+import { Form } from '../../../app/form/Form';
 
 @autobind
 export default class RespondentCheckYourAnswersPostController extends PostController<AnyObject> {
@@ -13,6 +15,19 @@ export default class RespondentCheckYourAnswersPostController extends PostContro
     }
 
     return super.save(req, formData, eventName);
+  }
+
+  protected async saveAndSignOut(
+    req: AppRequest<AnyObject>,
+    res: Response,
+    form: Form,
+    formData: Partial<Case>
+  ): Promise<void> {
+    // If statement of truth checkbox is selected and they click save and sign out
+    // then force it to uncheck to allow them to submit AoS
+    formData.aosStatementOfTruth = Checkbox.Unchecked;
+
+    return super.saveAndSignOut(req, res, form, formData);
   }
 
   protected getEventName(): string {
