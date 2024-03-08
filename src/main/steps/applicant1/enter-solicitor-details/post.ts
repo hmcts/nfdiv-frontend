28cbbe14bb80f9b516442e3d0116c64d8514399b
Barry Weeks <barry.weeks@justice.gov.uk>
2024-03-08 15:43:30 +0000

    NFDiv-3905 Add new field to enter-solicitor-details

--- src/main/steps/applicant1/enter-solicitor-details/post.ts
+++ src/main/steps/applicant1/enter-solicitor-details/post.ts
@@ -14,8 +14,12 @@ export default class EnterSolicitorDetailsPostController extends PostController<
       (formData.applicant2SolicitorAddressPostcode && formData.applicant2SolicitorAddress1)
     ) {
       formData.applicant2SolicitorRepresented = YesOrNo.YES;
+      if (formData.applicant2SolicitorAddressOverseas === undefined) {
+        formData.applicant2SolicitorAddressOverseas = YesOrNo.NO;
+      }
     } else {
       formData.applicant2SolicitorRepresented = YesOrNo.NO;
+      formData.applicant2SolicitorAddressOverseas = undefined;
     }
 
     return req.locals.api.triggerEvent(req.session.userCase.id, formData, eventName);
