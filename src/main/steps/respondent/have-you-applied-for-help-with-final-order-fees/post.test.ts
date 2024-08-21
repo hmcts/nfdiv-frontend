8c964828a740ad57f9b7b91221b46eaca6dc6200
adamg-hmcts <adam.goddard@justice.gov.uk>
2024-08-21 16:20:56 +0100

    Add post test for hwf controller

---
+++ src/main/steps/respondent/have-you-applied-for-help-with-final-order-fees/post.test.ts
@@ -0,0 +1,38 @@
+import { mockRequest } from '../../../../test/unit/utils/mockRequest';
+import { mockResponse } from '../../../../test/unit/utils/mockResponse';
+import { ApplicationType, CITIZEN_APPLICANT2_UPDATE, RESPONDENT_APPLY_FOR_FINAL_ORDER } from '../../../app/case/definition';
+import { FormContent } from '../../../app/form/Form';
+
+import HaveYouAppliedForHelpWithFinalOrderFeesPostController from './post';
+
+describe('HaveYouAppliedForHelpWithFinalOrderFeesPostController', () => {
+  const mockFormContent = {
+    fields: { applicant2FoHelpWithFeesRefNo: null },
+  } as unknown as FormContent;
+
+  it('triggers RESPONDENT_APPLY_FOR_FINAL_ORDER when HWF Ref has been provided', async () => {
+    const body = {
+        applicant2FoHelpWithFeesRefNo: "Dummy Ref",
+    };
+    const haveYouAppliedForHelpPostController = new HaveYouAppliedForHelpWithFinalOrderFeesPostController(mockFormContent.fields);
+
+    const req = mockRequest({ body, userCase: { applicationType: ApplicationType.SOLE_APPLICATION } });
+    const res = mockResponse();
+    await haveYouAppliedForHelpPostController.post(req, res);
+
+    expect(req.locals.api.triggerEvent).toHaveBeenCalledWith('1234', body, RESPONDENT_APPLY_FOR_FINAL_ORDER);
+  });
+
+  it('triggers CITIZEN_APPLICANT2_UPDATE when HWF Ref has not been provided', async () => {
+    const body = {
+        applicant2FoHelpWithFeesRefNo: null,
+    };
+    const haveYouAppliedForHelpPostController = new HaveYouAppliedForHelpWithFinalOrderFeesPostController(mockFormContent.fields);
+
+    const req = mockRequest({ body, userCase: { applicationType: ApplicationType.SOLE_APPLICATION } });
+    const res = mockResponse();
+    await haveYouAppliedForHelpPostController.post(req, res);
+
+    expect(req.locals.api.triggerEvent).toHaveBeenCalledWith('1234', body, CITIZEN_APPLICANT2_UPDATE);
+  });
+});
