88dcf9314c2da0b6242125726b9ae9a9c093076e
FaisalMoJ <100577377+FaisalMoJ@users.noreply.github.com>
2024-09-24 09:47:18 +0100

    NFDIV-4206: Add logic for represented respondent and HWF (#3880)
    * NFDIV-4206: Add logic for represented respondent and HWF
    
    * NFDIV-4207: Add state to template selector for HWF cannot upload documents
    
    * NFDIV-4210: Update hub page when respondent represented and international address (#3884)
    
    * NFDIV-4210: Update hub page when respodent represented and international address
    
    * NFDIV-4210: Fix issue with progress bar
    
    * NFDIV-4210: Use correct function

--- src/main/steps/applicant1/hub-page/sole/soleTemplateSelector.test.ts
+++ src/main/steps/applicant1/hub-page/sole/soleTemplateSelector.test.ts
@@ -351,4 +351,14 @@ describe('SoleTemplateSelector test', () => {
     const jointTemplate = getJointHubTemplate(theState, userCase);
     expect(jointTemplate).toBe(HubTemplate.AwaitingDocuments);
   });
+
+  test('should show /awaiting-documents.njk for state AwaitingHWFDecision and reason is "cannot upload documents"', () => {
+    const userCaseWithApplicant1CannotUploadDocuments = {
+      ...userCase,
+      applicant1CannotUpload: Checkbox.Checked,
+    };
+    const theState = displayState.at(State.AwaitingHWFDecision);
+    const soleTemplate = getSoleHubTemplate(theState, userCaseWithApplicant1CannotUploadDocuments, false, false);
+    expect(soleTemplate).toBe(HubTemplate.AwaitingDocuments);
+  });
 });
