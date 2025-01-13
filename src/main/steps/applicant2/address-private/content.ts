6fa30decaf7622c489893df0e2cfa538c3d5d90c
DelythJustice <97962563+DelythJustice@users.noreply.github.com>
2025-01-13 09:19:36 +0000

    NFDIV-4011 (#4004)
    * NFDIV-4011
    
    * NFDIV-4011
    
    * NFDIV-4011 address type errors
    
    * NFDIV-4011 take out the 3rd param I added trying to get rid of error
    
    * NFDIV-4011 omg it complained I had left in spaces
    
    * NFDIV-4011 somehow messed up format
    
    * NFDIV-4011 somehow messed up import order
    
    * NFDIV-4011
    
    * NFDIV-4011
    
    * NFDIV-4011 change isComplete to behave differently for refuge field
    
    * NFDIV-4011 try get ts-ignore to work
    
    * NFDIV-4011 make sonar happier
    
    * NFDIV-4011 make sonar happier
    
    * NFDIV-4011 make sonar happier
    
    * NFDIV-4011 remove accidental comit of local dev run change
    
    * NFDIV-4011 point at case api pr
    
    * NFDIV-4011 point at case api pr
    
    * Bumping chart version/ fixing aliases
    
    * NFDIV-4011 point at case api pr in template too
    
    * Bumping chart version/ fixing aliases
    
    * Bumping chart version/ fixing aliases
    
    * point to case api properly
    
    * and point to case api properly here
    
    * Bumping chart version/ fixing aliases
    
    * NFDIV-4011 jenkins formatting
    
    * Update values.yaml
    
    * Update values.yaml
    
    * Update default.yaml
    
    * NFDIV-4011
    
    * NFDIV-4011 funny business necessary for the welsh to work
    
    * NFDIV-4011 funny business necessary for the welsh to work
    
    * NFDIV-4011 added missed mapping
    
    * NFDIV-4011 added missed mapping
    
    * NFDIV-4011 stop pointing to case api pr as ready for release
    
    ---------
    
    Co-authored-by: hmcts-jenkins-j-to-z <61242337+hmcts-jenkins-j-to-z[bot]@users.noreply.github.com>
    Co-authored-by: adamg-hmcts <159780146+adamg-hmcts@users.noreply.github.com>

--- src/main/steps/applicant2/address-private/content.ts
+++ src/main/steps/applicant2/address-private/content.ts
@@ -1,21 +1,58 @@
+import { YesOrNo } from '../../../app/case/definition';
 import { TranslationFn } from '../../../app/controller/GetController';
-import { FormContent, FormFields } from '../../../app/form/Form';
-import {
-  form as applicant1Form,
-  generateContent as applicant1GenerateContent,
-} from '../../applicant1/address-private/content';
+import type { FormContent } from '../../../app/form/Form';
+import { isFieldFilledIn } from '../../../app/form/validation';
+import { generateContent as applicant1GenerateContent } from '../../applicant1/address-private/content';
 
 const labels = content => ({
   errors: {
     applicant2AddressPrivate: content.errors.applicant1AddressPrivate,
   },
+  title: content.title, // Reuse the title from applicant1
+  inRefugeLabel: content.inRefugeLabel, // Reuse the refuge label
+  detailsPrivate: content.detailsPrivate, // Reuse the "details private" wording
+  detailsPrivateMoreDetails: content.detailsPrivateMoreDetails,
+  supportAvailable: content.supportAvailable,
+  detailsNotPrivate: content.detailsNotPrivate,
 });
 
-const applicant1FormFields = applicant1Form.fields as FormFields;
 export const form: FormContent = {
-  ...applicant1Form,
   fields: {
-    applicant2AddressPrivate: applicant1FormFields.applicant1AddressPrivate,
+    applicant2AddressPrivate: {
+      type: 'radios',
+      classes: 'govuk-radios',
+      label: l => l.title,
+      labelHidden: true,
+      values: [
+        {
+          label: l => l.detailsPrivate,
+          value: YesOrNo.YES,
+          conditionalText: l =>
+            `<p class="govuk-label">${l.detailsPrivateMoreDetails} <a class="govuk-link" href="https://www.gov.uk/guidance/domestic-abuse-how-to-get-help">${l.supportAvailable}</a></p>`,
+          subFields: {
+            applicant2InRefuge: {
+              id: 'applicant2InRefuge',
+              type: 'radios',
+              classes: 'govuk-radios--inline',
+              label: l => l.inRefugeLabel,
+              values: [
+                { label: l => (l.language === 'cy' ? 'Yndw' : l.yes), value: YesOrNo.YES },
+                { label: l => (l.language === 'cy' ? 'Nac ydw' : l.no), value: YesOrNo.NO },
+              ],
+              value: YesOrNo.NO, // Default value
+            },
+          },
+        },
+        {
+          label: l => l.detailsNotPrivate,
+          value: YesOrNo.NO,
+        },
+      ],
+      validator: value => isFieldFilledIn(value),
+    },
+  },
+  submit: {
+    text: l => l.continue,
   },
 };
 
