d21b966f2446ac66e2e2e81dac93e4fb1bba1abd
adamg-hmcts <159780146+adamg-hmcts@users.noreply.github.com>
2025-01-27 13:37:46 +0000

    [NFDIV-4433] Allow card payments after HWF refused (#3993)
    * Add new state and allow payments from it
    
    * Lint corrections
    
    * Update homepage tests
    
    * Move awaiting payment state arrays into constants
    
    * Lint corrections
    
    * Capitalize new HWF state name to match other HWF states
    
    * Link backend PR
    
    * Set up new awaiting response to hwf decision page for app 2
    
    * Remove HWF references from pages if paid in full
    
    * Lint corrections
    
    * Use safe navigation operator to handle undefined user case
    
    * Make awaiting response to HWF decision available to app2
    
    * Welsh correction
    
    * Lint corrections
    
    * Update state sequence test
    
    * Remove references to backend PR
    
    * Lint correction

--- src/main/steps/state-sequence.test.ts
+++ src/main/steps/state-sequence.test.ts
@@ -42,12 +42,12 @@ describe('StateSequence', () => {
   });
 
   test('preSubmittedStatePrioritySequence should be appropriate', async () => {
-    expect(preSubmittedStatePrioritySequence).toHaveLength(12);
+    expect(preSubmittedStatePrioritySequence).toHaveLength(13);
     expect(preSubmittedStatePrioritySequence).not.toContain(State.Submitted);
   });
 
   test('orderedStateSequence should be appropriate', async () => {
-    expect(orderedStateSequence).toHaveLength(59);
+    expect(orderedStateSequence).toHaveLength(60);
   });
 
   describe('getHighestPriorityPreSubmissionCases', () => {
