0a41a019868ca0c433be02de3a1cc4926e9d4d24
DelythJustice <delyth.jones11@justice.gov.uk>
2025-02-11 12:23:29 +0000

    NFDIV-4676 revert back to using az cli for local env

--- src/main/modules/properties-volume/index.ts
+++ src/main/modules/properties-volume/index.ts
@@ -1,36 +1,24 @@
 import { execSync } from 'child_process';
 
-import { DefaultAzureCredential } from '@azure/identity';
-import { SecretClient } from '@azure/keyvault-secrets';
 import * as propertiesVolume from '@hmcts/properties-volume';
 import config from 'config';
 import { Application } from 'express';
 import { get, set } from 'lodash';
 
 export class PropertiesVolume {
-  private client: SecretClient;
-
-  constructor() {
-    const vaultName = 'nfdiv-aat';
-    const url = `https://${vaultName}.vault.azure.net/`;
-    const credential = new DefaultAzureCredential();
-
-    this.client = new SecretClient(url, credential);
-  }
-
-  enableFor = async (app: Application): Promise<void> => {
+  enableFor(app: Application): void {
     if (!app.locals.developmentMode) {
       propertiesVolume.addTo(config);
 
-      await this.setSecret('secrets.nfdiv.AppInsightsInstrumentationKey', 'appInsights.instrumentationKey');
-      await this.setSecret('secrets.nfdiv.redis-access-key', 'session.redis.key');
-      await this.setSecret('secrets.nfdiv.redis-access-key', 'session.secret');
-      await this.setSecret('secrets.nfdiv.idam-secret', 'services.idam.clientSecret');
-      await this.setSecret('secrets.nfdiv.frontend-secret', 'services.authProvider.secret');
-      await this.setSecret('secrets.nfdiv.os-places-token', 'services.postcodeLookup.token');
-      await this.setSecret('secrets.nfdiv.idam-systemupdate-username', 'services.idam.systemUsername');
-      await this.setSecret('secrets.nfdiv.idam-systemupdate-password', 'services.idam.systemPassword');
-      await this.setSecret('secrets.nfdiv.pcq-token-key', 'services.equalityAndDiversity.tokenKey');
+      this.setSecret('secrets.nfdiv.AppInsightsInstrumentationKey', 'appInsights.instrumentationKey');
+      this.setSecret('secrets.nfdiv.redis-access-key', 'session.redis.key');
+      this.setSecret('secrets.nfdiv.redis-access-key', 'session.secret');
+      this.setSecret('secrets.nfdiv.idam-secret', 'services.idam.clientSecret');
+      this.setSecret('secrets.nfdiv.frontend-secret', 'services.authProvider.secret');
+      this.setSecret('secrets.nfdiv.os-places-token', 'services.postcodeLookup.token');
+      this.setSecret('secrets.nfdiv.idam-systemupdate-username', 'services.idam.systemUsername');
+      this.setSecret('secrets.nfdiv.idam-systemupdate-password', 'services.idam.systemPassword');
+      this.setSecret('secrets.nfdiv.pcq-token-key', 'services.equalityAndDiversity.tokenKey');
     } else {
       this.setLocalSecret('idam-secret', 'services.idam.clientSecret');
       this.setLocalSecret('frontend-secret', 'services.authProvider.secret');
@@ -40,9 +28,9 @@ export class PropertiesVolume {
       this.setLocalSecret('e2e-test-user-password', 'e2e.userTestPassword');
       this.setLocalSecret('pcq-token-key', 'services.equalityAndDiversity.tokenKey');
     }
-  };
+  }
 
-  private async setSecret(fromPath: string, toPath: string): Promise<void> {
+  private setSecret(fromPath: string, toPath: string): void {
     if (config.has(fromPath)) {
       set(config, toPath, get(config, fromPath));
     }
