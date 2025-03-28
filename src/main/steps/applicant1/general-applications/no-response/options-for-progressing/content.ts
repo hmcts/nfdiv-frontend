7b5c350119fea328cf03fc5dfeee6b01c523c47d
Barry Weeks <barry.weeks@justice.gov.uk>
2025-03-28 15:50:31 +0000

    NFDIV-4714 add no-response/options-for-progressing

---
+++ src/main/steps/applicant1/general-applications/no-response/options-for-progressing/content.ts
@@ -0,0 +1,42 @@
+import config from 'config';
+
+import { TranslationFn } from '../../../../../app/controller/GetController';
+import { CommonContent } from '../../../../common/common.content';
+import { HAVE_THEY_RECEIVED, HAVE_THEY_RECEIVED_REPRESENTED } from '../../../../urls';
+
+const en = ({ isDivorce, partner, isApp2Represented }: CommonContent) => ({
+  title: `Options for progressing with ${isDivorce ? 'your divorce' : 'ending your civil partnership'}`,
+  line1: `There are a number of ways to progress your ${
+    isDivorce ? 'divorce application' : 'application to end your civil partnership'
+  } without a response from your partner.`,
+  line2: `The court has sent the ${
+    isDivorce ? 'divorce papers' : 'papers to end your civil partnership'
+  } to your ${partner}'s postal address, as well as their email address if your provided one. If you want to progress your ${
+    isDivorce ? 'divorce' : 'application to end your civil partnership'
+  } in a different way, you will need to apply to the court to do so.`,
+  line3: `Depending on the application you make, it could cost either £45 or £58, but you may be able to <a class="govuk-link" target="_blank" href="${config.get(
+    'govukUrls.getHelpWithCourtFees'
+  )}">get help paying this fee (opens in a new tab)</a>.`,
+  line4: `We will ask you some questions so that we can show you which options are available for you for proceeding with your ${
+    isDivorce ? 'divorce' : 'application to end your civil partnership'
+  }`,
+  startButton: {
+    text: 'Start now',
+    url: isApp2Represented ? HAVE_THEY_RECEIVED_REPRESENTED : HAVE_THEY_RECEIVED,
+  },
+});
+
+// @TODO translations
+const cy: typeof en = en;
+
+const languages = {
+  en,
+  cy,
+};
+
+export const generateContent: TranslationFn = content => {
+  const translations = languages[content.language](content);
+  return {
+    ...translations,
+  };
+};
