df78314190dd14070f28ffa15f155e14f60d2962
pallavijustice <pallavi.saha@justice.gov.uk>
2025-04-22 12:59:47 +0100

    NFDIV-4707 - Fix lint issues

--- src/main/steps/applicant1/pay-your-fee/content.ts
+++ src/main/steps/applicant1/pay-your-fee/content.ts
@@ -4,7 +4,6 @@ import { isEmpty } from 'lodash';
 import { TranslationFn } from '../../../app/controller/GetController';
 import { getFee } from '../../../app/fees/service/get-fee';
 import { FormContent } from '../../../app/form/Form';
-import { CommonContent } from '../../common/common.content';
 import { VIEW_YOUR_ANSWERS } from '../../urls';
 
 const en = ({ isDivorce }, applicationFee) => ({
