8636cecc3afb87d2a333f17ded794918d16ab398
adamg-hmcts <adam.goddard@justice.gov.uk>
2025-06-11 14:32:42 +0100

    Set application type in check answers

--- src/main/steps/applicant1/interim-applications/deemed-service/check-answers/post.ts
+++ src/main/steps/applicant1/interim-applications/deemed-service/check-answers/post.ts
@@ -1,10 +1,19 @@
 import autobind from 'autobind-decorator';
 
-import { CITIZEN_SERVICE_APPLICATION } from '../../../../../app/case/definition';
+import { Case, CaseWithId } from '../../../../../app/case/case';
+import { CITIZEN_SERVICE_APPLICATION, GeneralApplicationType } from '../../../../../app/case/definition';
+import { AppRequest } from '../../../../../app/controller/AppRequest';
 import { AnyObject, PostController } from '../../../../../app/controller/PostController';
 
 @autobind
 export default class CheckYourAnswersPostController extends PostController<AnyObject> {
+  protected async save(req: AppRequest<AnyObject>, formData: Partial<Case>, eventName: string): Promise<CaseWithId> {
+    if (!req.session.isApplicant2) {
+      formData.applicant1InterimApplicationType = GeneralApplicationType.DEEMED_SERVICE;
+    }
+    return super.save(req, formData, eventName);
+  }
+
   protected getEventName(): string {
     return CITIZEN_SERVICE_APPLICATION;
   }
