ed8fddb88fcb776eefa24cb0fa5f1db553d98714
Barry Weeks <barry.weeks@justice.gov.uk>
2025-06-11 10:19:10 +0100

    NFDIV-4734 Add dispenseServiceApplicationSequence and test

---
+++ src/main/steps/dispenseServiceApplicationSequence.test.ts
@@ -0,0 +1,14 @@
+import { Step } from './applicant1Sequence';
+import { DISPENSE_SERVICE_APPLICATION, HELP_WITH_FEES_DISPENSE } from './urls';
+import { dispenseServiceApplicationSequence } from './dispenseServiceApplicationSequence';
+
+describe('Dispense With Service Application Sequence test', () => {
+  describe('DISPENSE_SERVICE_APPLICATION', () => {
+    test('DISPENSE_SERVICE_APPLICATION', () => {
+      const step = dispenseServiceApplicationSequence.find(
+        obj => obj.url === DISPENSE_SERVICE_APPLICATION
+      ) as Step;
+      expect(step.getNextStep({})).toBe(HELP_WITH_FEES_DISPENSE);
+    });
+  });
+});
