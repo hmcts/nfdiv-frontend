ff2b39b56e626d7bfdfa45f4b9d389ae2f46fcc9
adamg-hmcts <adam.goddard@justice.gov.uk>
2025-06-16 13:25:27 +0100

    Remove reference to removed GeneralApplicationType

--- src/main/steps/applicant1/interim-applications/deemed-service/check-answers/post.ts
+++ src/main/steps/applicant1/interim-applications/deemed-service/check-answers/post.ts
@@ -1,7 +1,7 @@
 import autobind from 'autobind-decorator';
 
 import { Case, CaseWithId } from '../../../../../app/case/case';
-import { CITIZEN_SERVICE_APPLICATION, GeneralApplicationType } from '../../../../../app/case/definition';
+import { CITIZEN_SERVICE_APPLICATION, InterimApplicationType } from '../../../../../app/case/definition';
 import { AppRequest } from '../../../../../app/controller/AppRequest';
 import { AnyObject, PostController } from '../../../../../app/controller/PostController';
 
@@ -9,7 +9,7 @@ import { AnyObject, PostController } from '../../../../../app/controller/PostCon
 export default class CheckYourAnswersPostController extends PostController<AnyObject> {
   protected async save(req: AppRequest<AnyObject>, formData: Partial<Case>, eventName: string): Promise<CaseWithId> {
     if (!req.session.isApplicant2) {
-      formData.applicant1InterimApplicationType = GeneralApplicationType.DEEMED_SERVICE;
+      formData.applicant1InterimApplicationType = InterimApplicationType.DEEMED_SERVICE;
     }
     return super.save(req, formData, eventName);
   }
