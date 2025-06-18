645a31bea54ae20250d26cea399faa3bc8d9a501
Barry Weeks <barry.weeks@justice.gov.uk>
2025-06-18 18:36:41 +0100

    NFDIV-4763 Add conditional date input

--- src/main/steps/applicant1/interim-applications/dispense-service/last-date-dispense/content.ts
+++ src/main/steps/applicant1/interim-applications/dispense-service/last-date-dispense/content.ts
@@ -1,11 +1,21 @@
+import { CaseDate } from '../../../../../app/case/case';
 import { YesOrNo } from '../../../../../app/case/definition';
 import { TranslationFn } from '../../../../../app/controller/GetController';
-import { FormContent } from '../../../../../app/form/Form';
-import { isFieldFilledIn } from '../../../../../app/form/validation';
+import { FormContent, FormFieldsFn } from '../../../../../app/form/Form';
+import { covertToDateObject } from '../../../../../app/form/parser';
+import {
+  areDateFieldsFilledIn,
+  isDateInputInvalid,
+  isFieldFilledIn,
+  isFutureDate,
+} from '../../../../../app/form/validation';
+import { SupportedLanguages } from '../../../../../modules/i18n';
 import { CommonContent } from '../../../../common/common.content';
 
 const en = ({ partner }: CommonContent) => ({
   title: `Did you and your ${partner} live together?`,
+  lastLivedTogether:
+    "Give the date of when you last lived together. If you're not sure of the day you can enter the last day of the month.",
   errors: {
     applicant1DispenseLiveTogether: {
       required: 'You must select an option before continuing.',
@@ -16,6 +26,8 @@ const en = ({ partner }: CommonContent) => ({
 // @TODO translations
 const cy = ({ partner }: CommonContent) => ({
   title: `Did you and your ${partner} live together?`,
+  lastLivedTogether:
+    "Give the date of when you last lived together. If you're not sure of the day you can enter the last day of the month.",
   errors: {
     applicant1DispenseLiveTogether: {
       required: 'You must select an option before continuing.',
@@ -29,7 +41,7 @@ const languages = {
 };
 
 export const form: FormContent = {
-  fields: {
+  fields: (userCase, language) => ({
     applicant1DispenseLiveTogether: {
       type: 'radios',
       classes: 'govuk-radios',
@@ -40,6 +52,41 @@ export const form: FormContent = {
           label: l => l.yes,
           id: 'yes',
           value: YesOrNo.YES,
+          subFields: {
+            applicant1DispenseLastLivedTogetherDate: {
+              type: 'date',
+              classes: 'govuk-date-input',
+              label: l => l.lastLivedTogether,
+              labelHidden: false,
+              hint: l => l.hint,
+              values: [
+                {
+                  label: language === SupportedLanguages.Cy ? 'Diwrnod' : 'Day',
+                  name: 'day',
+                  classes: 'govuk-input--width-2',
+                  attributes: { maxLength: 2 },
+                },
+                {
+                  label: language === SupportedLanguages.Cy ? 'Mis' : 'Month',
+                  name: 'month',
+                  classes: 'govuk-input--width-2',
+                  attributes: { maxLength: 2 },
+                },
+                {
+                  label: language === SupportedLanguages.Cy ? 'Blwyddyn' : 'Year',
+                  name: 'year',
+                  classes: 'govuk-input--width-4',
+                  attributes: { maxLength: 4 },
+                },
+              ],
+              parser: body =>
+                covertToDateObject('applicant1DispenseLastLivedTogetherDate', body as Record<string, unknown>),
+              validator: value =>
+                areDateFieldsFilledIn(value as CaseDate) ||
+                isDateInputInvalid(value as CaseDate) ||
+                isFutureDate(value as CaseDate),
+            },
+          },
         },
         {
           label: l => l.no,
@@ -49,7 +96,7 @@ export const form: FormContent = {
       ],
       validator: value => isFieldFilledIn(value),
     },
-  },
+  }),
   submit: {
     text: l => l.continue,
   },
@@ -59,6 +106,6 @@ export const generateContent: TranslationFn = content => {
   const translations = languages[content.language](content);
   return {
     ...translations,
-    form,
+    form: { ...form, fields: (form.fields as FormFieldsFn)(content.userCase || {}, content.language) },
   };
 };
