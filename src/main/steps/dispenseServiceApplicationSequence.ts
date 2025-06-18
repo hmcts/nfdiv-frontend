c14dedd0779c72e3dea17b0d579a27983c8a82c0
Barry Weeks <barry.weeks@justice.gov.uk>
2025-06-18 15:38:12 +0100

    NFDIV-4763 Add step sequence

--- src/main/steps/dispenseServiceApplicationSequence.ts
+++ src/main/steps/dispenseServiceApplicationSequence.ts
@@ -1,9 +1,42 @@
+import { YesOrNo } from '../app/case/definition';
 import { Step } from './applicant1Sequence';
-import { DISPENSE_SERVICE_APPLICATION, HELP_WITH_FEES_DISPENSE } from './urls';
+import {
+  APPLY_FOR_HWF_DISPENSE,
+  DISPENSE_SERVICE_APPLICATION,
+  HELP_WITH_FEES_DISPENSE,
+  HUB_PAGE,
+  HWF_REFERENCE_NUMBER_DISPENSE,
+  HWF_REFERENCE_NUMBER_INPUT_DISPENSE,
+  LAST_DATE_DISPENSE
+} from './urls';
 
 export const dispenseServiceApplicationSequence: Step[] = [
   {
     url: DISPENSE_SERVICE_APPLICATION,
     getNextStep: () => HELP_WITH_FEES_DISPENSE,
+  },
+  {
+    url: HELP_WITH_FEES_DISPENSE,
+    getNextStep: data =>
+      data?.applicant1InterimAppsUseHelpWithFees === YesOrNo.YES ? HWF_REFERENCE_NUMBER_DISPENSE : LAST_DATE_DISPENSE,
+  },
+  {
+    url: HWF_REFERENCE_NUMBER_DISPENSE,
+    getNextStep: data =>
+      data?.applicant1InterimAppsHaveHwfReference === YesOrNo.YES
+        ? HWF_REFERENCE_NUMBER_INPUT_DISPENSE
+        : APPLY_FOR_HWF_DISPENSE,
+  },
+  {
+    url: HWF_REFERENCE_NUMBER_INPUT_DISPENSE,
+    getNextStep: () => LAST_DATE_DISPENSE,
+  },
+  {
+    url: APPLY_FOR_HWF_DISPENSE,
+    getNextStep: () => HWF_REFERENCE_NUMBER_INPUT_DISPENSE,
+  },
+  {
+    url: LAST_DATE_DISPENSE,
+    getNextStep: () => HUB_PAGE,
   },
 ];
