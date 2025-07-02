0e91da5e3a9a100c1a4135c15123d566deffa213
BWeeks-JusticeGovUK <92433491+BWeeks-JusticeGovUK@users.noreply.github.com>
2025-07-02 11:51:54 +0100

    NFDIV-4733 Remove unnecessary condition

--- src/main/steps/applicant1/interim-applications/no-response/process-server/post.ts
+++ src/main/steps/applicant1/interim-applications/no-response/process-server/post.ts
@@ -1,6 +1,6 @@
 import autobind from 'autobind-decorator';
-import { Case, CaseWithId } from '../../../../../app/case/case';
 
+import { Case, CaseWithId } from '../../../../../app/case/case';
 import { CITIZEN_GENERATE_D10, InterimApplicationType } from '../../../../../app/case/definition';
 import { AppRequest } from '../../../../../app/controller/AppRequest';
 import { AnyObject, PostController } from '../../../../../app/controller/PostController';
@@ -8,9 +8,8 @@ import { AnyObject, PostController } from '../../../../../app/controller/PostCon
 @autobind
 export default class ProcessServerPostController extends PostController<AnyObject> {
   protected async save(req: AppRequest<AnyObject>, formData: Partial<Case>, eventName: string): Promise<CaseWithId> {
-    if (!req.session.isApplicant2) {
-      formData.applicant1InterimApplicationType = InterimApplicationType.PROCESS_SERVER_SERVICE;
-    }
+    formData.applicant1InterimApplicationType = InterimApplicationType.PROCESS_SERVER_SERVICE;
+
     return super.save(req, formData, eventName);
   }
 
