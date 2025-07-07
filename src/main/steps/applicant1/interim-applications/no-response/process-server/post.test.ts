2ee29c6ec176e5ffc3d111382874d3cbed233320
BWeeks-JusticeGovUK <92433491+BWeeks-JusticeGovUK@users.noreply.github.com>
2025-07-07 16:31:40 +0100

    NFDIV-4733 Update test

--- src/main/steps/applicant1/interim-applications/no-response/process-server/post.test.ts
+++ src/main/steps/applicant1/interim-applications/no-response/process-server/post.test.ts
@@ -1,6 +1,6 @@
 import { mockRequest } from '../../../../../../test/unit/utils/mockRequest';
 import { mockResponse } from '../../../../../../test/unit/utils/mockResponse';
-import { CITIZEN_GENERATE_D10, InterimApplicationType } from '../../../../../app/case/definition';
+import { CITIZEN_GENERATE_PROCESS_SERVER_DOCS, InterimApplicationType } from '../../../../../app/case/definition';
 
 import ProcessServerPostController from './post';
 
@@ -16,6 +16,10 @@ describe('ProcessServerPostController', () => {
     const res = mockResponse();
     await processServerPostController.post(req, res);
 
-    expect(req.locals.api.triggerEvent).toHaveBeenCalledWith('1234', expectedBody, CITIZEN_GENERATE_D10);
+    expect(req.locals.api.triggerEvent).toHaveBeenCalledWith(
+      '1234',
+      expectedBody,
+      CITIZEN_GENERATE_PROCESS_SERVER_DOCS
+    );
   });
 });
