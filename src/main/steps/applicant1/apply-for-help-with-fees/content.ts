679d38679f581effde56aec9841df4155839d789
adamg-hmcts <adam.goddard@justice.gov.uk>
2025-07-11 10:04:55 +0100

    Add new HWF guidance text

--- src/main/steps/applicant1/apply-for-help-with-fees/content.ts
+++ src/main/steps/applicant1/apply-for-help-with-fees/content.ts
@@ -11,6 +11,10 @@ const en = ({ isDivorce, isApplicant2 }) => ({
   line3: `After you have applied then you will receive a Help With Fees reference number. You should enter the reference number when you return to this ${
     isDivorce ? 'divorce' : 'ending a civil partnership'
   } application.`,
+  sendEvidenceGuidanceLine1: 'If you receive any benefit that qualifies you for Help with Fees, you must include evidence of it when you apply online for Help with Fees.',
+  sendEvidenceGuidanceLine2: `If you cannot include your benefits evidence when you apply online for Help with Fees, you must send your evidence to the court when you submit this ${isDivorce ? 'divorce application' : 'application to end your civil partnership'}.`,
+  sendEvidenceGuidanceLine3: `You will get your case reference number after you submit your ${isDivorce ? 'divorce application' : 'application to end your civil partnership'}.`,
+  sendEvidenceGuidanceLine4: `Email your benefits evidence, including your case reference number, to <a href="mailto:contactdivorce@justice.gov.uk">contactdivorce@justice.gov.uk</a>.`,
   line4: `If you have a Help With Fees reference number then you can <a class="govuk-link" href="${
     isApplicant2 ? APPLICANT_2 : ''
   }${HELP_WITH_YOUR_FEE_URL}">enter it here.</a>`,
@@ -28,6 +32,10 @@ const cy: typeof en = ({ isDivorce, isApplicant2 }) => ({
   line3: `Ar ôl ichi wneud cais byddwch yn cael cyfeirnod Help i Dalu Ffioedd. Dylech nodi’r cyfeirnod pan fyddwch yn dychwelyd i’r cais hwn ${
     isDivorce ? 'am ysgariad' : 'i ddod â’ch partneriaeth sifil i ben'
   }.`,
+  sendEvidenceGuidanceLine1: 'If you receive any benefit that qualifies you for help with fees, you must include evidence of it when you apply online for Help with Fees.',
+  sendEvidenceGuidanceLine2: `If you cannot include your benefits evidence when you apply online for Help with fees, you must send your evidence to the court when you submit this ${isDivorce ? 'divorce application' : 'application to end your civil partnership'}.`,
+  sendEvidenceGuidanceLine3: `You will get your case reference number after you submit your ${isDivorce ? 'divorce application' : 'application to end your civil partnership'}.`,
+  sendEvidenceGuidanceLine4: `Email your benefits evidence, including your case reference number, to <a href="mailto:contactdivorce@justice.gov.uk">contactdivorce@justice.gov.uk</a>.`,
   line4: `Os oes gennych gyfeirnod Help i Dalu Ffioedd, yna gallwch <a class="govuk-link" href="${
     isApplicant2 ? APPLICANT_2 : ''
   }${HELP_WITH_YOUR_FEE_URL}">ei nodi yma.</a>`,
