b915c0962963c7de6da72976ae941c591d403ee9
BWeeks-JusticeGovUK <92433491+BWeeks-JusticeGovUK@users.noreply.github.com>
2025-07-31 10:38:17 +0100

    Reapply "NFDIV-4955 Move deployment Id to content to handle language switching"
    This reverts commit 0a2ec0bb4728d22f5b0e455c0de8d5a53d61a07a.

--- src/main/modules/nunjucks/index.ts
+++ src/main/modules/nunjucks/index.ts
@@ -84,7 +84,6 @@ export class Nunjucks {
         avayaClientUrl: config.get('webchat.avayaClientUrl'),
         avayaService: config.get('webchat.avayaService'),
         genesysReferrerPage: config.get('webchat.genesysReferrerPage'),
-        genesysDeploymentId: config.get('webchat.genesysDeploymentId'),
         genesysBaseUrl: config.get('webchat.genesysBaseUrl'),
         genesysEnvironment: config.get('webchat.genesysEnvironment'),
         genesysKervBaseUrl: config.get('webchat.genesysKervBaseUrl'),
