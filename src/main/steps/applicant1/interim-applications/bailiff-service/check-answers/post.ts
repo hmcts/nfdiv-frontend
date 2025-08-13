a24bd2444dcff28899fdd24269dec8f09ca8abac
adamg-hmcts <159780146+adamg-hmcts@users.noreply.github.com>
2025-08-13 09:44:29 +0100

    [NFDIV-4983] Validation to prevent early service applicaton submission (#4287)
    * Refactor post controllers and add validation method
    
    * Simplify new steps function
    
    * Linter corrections
    
    * Rename method for clarity
    
    * Add tests for new post controllers
    
    * Add steps tests for new function
    
    * Block use of the back button after service app submission
    
    * Linter corrections
    
    * Remove comment

--- src/main/steps/applicant1/interim-applications/bailiff-service/check-answers/post.ts
+++ src/main/steps/applicant1/interim-applications/bailiff-service/check-answers/post.ts
@@ -1,20 +1,21 @@
 import autobind from 'autobind-decorator';
 
-import { Case, CaseWithId } from '../../../../../app/case/case';
 import { CITIZEN_SERVICE_APPLICATION, InterimApplicationType } from '../../../../../app/case/definition';
-import { AppRequest } from '../../../../../app/controller/AppRequest';
-import { AnyObject, PostController } from '../../../../../app/controller/PostController';
+import { Step } from '../../../../../steps/applicant1Sequence';
+import { bailiffServiceApplicationSequence as bailiffServiceSequence } from '../../../../bailiffServiceApplicationSequence';
+import CheckAnswersPostController from '../../common/check-answers/post';
 
 @autobind
-export default class CheckYourAnswersPostController extends PostController<AnyObject> {
-  protected async save(req: AppRequest<AnyObject>, formData: Partial<Case>, eventName: string): Promise<CaseWithId> {
-    if (!req.session.isApplicant2) {
-      formData.applicant1InterimApplicationType = InterimApplicationType.BAILIFF_SERVICE;
-    }
-    return super.save(req, formData, eventName);
-  }
-
+export default class CheckBailiffServiceAnswersPostController extends CheckAnswersPostController {
   protected getEventName(): string {
     return CITIZEN_SERVICE_APPLICATION;
+  }
+
+  protected interimApplicationType(): InterimApplicationType {
+    return InterimApplicationType.BAILIFF_SERVICE;
+  }
+
+  protected interimApplicationSequence(): Step[] {
+    return bailiffServiceSequence;
   }
 }
