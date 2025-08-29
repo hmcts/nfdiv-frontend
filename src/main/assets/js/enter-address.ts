5ad09c6157d07040c167897d644aeb44257330b8
adamg-hmcts <adam.goddard@justice.gov.uk>
2025-08-29 13:41:42 +0100

    Load last known address when user returns to the address input page

--- src/main/assets/js/enter-address.ts
+++ src/main/assets/js/enter-address.ts
@@ -17,7 +17,10 @@ if (form && getById('enterPostcode')) {
   const uk = 'UK';
   const applicant1AddressCountry = formData.get('applicant1AddressCountry');
   const applicant2AddressCountry = formData.get('applicant2AddressCountry');
-  const addressCountry = applicant1AddressCountry || applicant2AddressCountry;
+  const applicant1SearchGovRecordsPartnerLastKnownAddressCounty = formData.get(
+    'applicant1SearchGovRecordsPartnerLastKnownAddressCounty'
+  );
+  const addressCountry = applicant1AddressCountry || applicant2AddressCountry || applicant1SearchGovRecordsPartnerLastKnownAddressCounty;
   const hasBackendError = qsa('.govuk-error-summary').length > 1;
 
   if (addressCountry || hasBackendError) {
