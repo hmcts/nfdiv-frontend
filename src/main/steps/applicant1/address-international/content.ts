805728f474fd4e77bea660b52f726084a474a470
FaisalMoJ <100577377+FaisalMoJ@users.noreply.github.com>
2025-11-21 09:24:15 +0000

    Apply suggestion from @adamg-hmcts
    Co-authored-by: adamg-hmcts <159780146+adamg-hmcts@users.noreply.github.com>

--- src/main/steps/applicant1/address-international/content.ts
+++ src/main/steps/applicant1/address-international/content.ts
@@ -4,8 +4,8 @@ import { FormContent } from '../../../app/form/Form';
 const en = ({ isDivorce, partner }) => ({
   title: `${isDivorce ? 'Divorcing' : 'Ending civil partnership with'} someone who lives outside of England and Wales`,
   line1:
-    'The address you have provided is outside of England and Wales. That means you are responsbile for serving the papers.',
-  line2: `You will recieve the documents that you need to send to your ${partner}.`,
+    'The address you have provided is outside of England and Wales. That means you are responsible for serving the papers.',
+  line2: `You will receive the documents that you need to send to your ${partner}.`,
 });
 
 //TODO Welsh translation required
