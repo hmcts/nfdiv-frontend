a13d9c4e0bdc7826563057b8911143a13ba2b234
adamg-hmcts <adam.goddard@justice.gov.uk>
2025-11-28 14:21:29 +0000

    Linter corrections

--- src/main/modules/axios-logger/index.ts
+++ src/main/modules/axios-logger/index.ts
@@ -1,10 +1,11 @@
+import axios, { AxiosResponse, InternalAxiosRequestConfig } from 'axios';
 import { Application } from 'express';
-import axios, { InternalAxiosRequestConfig, AxiosResponse } from 'axios';
 
 export class AxiosLogger {
   enableFor(app: Application): void {
-
-    if (!app.locals.developmentMode) return;
+    if (!app.locals.developmentMode) {
+      return;
+    }
 
     axios.interceptors.request.use((config: InternalAxiosRequestConfig) => {
       console.debug(
@@ -17,15 +18,19 @@ export class AxiosLogger {
     axios.interceptors.response.use(
       (response: AxiosResponse) => {
         console.debug(
-          `Received response "${response.status} ${response.statusText}" from: "${response.config.baseURL || ''}${response.config.url}" content type: "${response.headers['content-type']}" data:`,
+          `Received response "${response.status} ${response.statusText}" from: "${response.config.baseURL || ''}${
+            response.config.url
+          }" content type: "${response.headers['content-type']}" data:`,
           response.data ? JSON.stringify(response.data, null, 2) : '{}'
         );
         return response;
       },
-      (error) => {
+      error => {
         if (error.response) {
           console.debug(
-            `Received error "${error.response.status} ${error.response.statusText}" from: "${error.response.config.baseURL || ''}${error.response.config.url}" content type: "${error.response.headers['content-type']}" data:`,
+            `Received error "${error.response.status} ${error.response.statusText}" from: "${
+              error.response.config.baseURL || ''
+            }${error.response.config.url}" content type: "${error.response.headers['content-type']}" data:`,
             error.response.data ? JSON.stringify(error.response.data, null, 2) : '{}'
           );
         } else {
@@ -36,4 +41,3 @@ export class AxiosLogger {
     );
   }
 }
-
