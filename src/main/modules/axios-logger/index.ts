bb2969860db9e575c0fd87b5aad2b8a8e2f3cb54
adamg-hmcts <adam.goddard@justice.gov.uk>
2025-12-02 17:18:47 +0000

    Add back axios logger config

--- src/main/modules/axios-logger/index.ts
+++ src/main/modules/axios-logger/index.ts
@@ -1,43 +1,22 @@
-import axios, { AxiosResponse, InternalAxiosRequestConfig } from 'axios';
 import { Application } from 'express';
 
 export class AxiosLogger {
   enableFor(app: Application): void {
-    if (!app.locals.developmentMode) {
-      return;
+    if (app.locals.developmentMode) {
+      require('axios-debug-log')({
+        request: (debug, req) =>
+          debug(
+            `Sending "${req.method}" request to: "${req.baseURL || ''}${req.url}" data:`,
+            JSON.stringify(req.data, null, 2)
+          ),
+        response: (debug, res) =>
+          debug(
+            `Received response "${res.status} ${res.statusText}" from: "${res.config.baseURL || ''}${
+              res.config.url
+            }" content type: "${res.headers['content-type']}" data:`,
+            JSON.stringify(res.data, null, 2)
+          ),
+      });
     }
-
-    axios.interceptors.request.use((config: InternalAxiosRequestConfig) => {
-      console.debug(
-        `Sending "${config.method?.toUpperCase()}" request to: "${config.baseURL || ''}${config.url}" data:`,
-        config.data ? JSON.stringify(config.data, null, 2) : '{}'
-      );
-      return config;
-    });
-
-    axios.interceptors.response.use(
-      (response: AxiosResponse) => {
-        console.debug(
-          `Received response "${response.status} ${response.statusText}" from: "${response.config.baseURL || ''}${
-            response.config.url
-          }" content type: "${response.headers['content-type']}" data:`,
-          response.data ? JSON.stringify(response.data, null, 2) : '{}'
-        );
-        return response;
-      },
-      error => {
-        if (error.response) {
-          console.debug(
-            `Received error "${error.response.status} ${error.response.statusText}" from: "${
-              error.response.config.baseURL || ''
-            }${error.response.config.url}" content type: "${error.response.headers['content-type']}" data:`,
-            error.response.data ? JSON.stringify(error.response.data, null, 2) : '{}'
-          );
-        } else {
-          console.debug('Axios request failed:', error.message);
-        }
-        return Promise.reject(error);
-      }
-    );
   }
 }
