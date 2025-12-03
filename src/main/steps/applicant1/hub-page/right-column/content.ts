6920e9cf2da396bcfba8e67e1460ba289ac58c88
BWeeks-JusticeGovUK <92433491+BWeeks-JusticeGovUK@users.noreply.github.com>
2025-12-03 14:31:42 +0000

    NFDIV-4923 Add missing content line

--- src/main/steps/applicant1/hub-page/right-column/content.ts
+++ src/main/steps/applicant1/hub-page/right-column/content.ts
@@ -3,12 +3,14 @@ import { TranslationFn } from '../../../../app/controller/GetController';
 const en = () => ({
   iWantTo: 'I want to...',
   gettingHelp: 'Getting help',
+  moneyAndProperty: 'Find out about dividing money and property',
 });
 
 // @TODO translations
 const cy: typeof en = () => ({
   iWantTo: 'Rwyf eisiau...',
   gettingHelp: 'Cael help',
+  moneyAndProperty: 'Rhagor o wybodaeth am rannu arian ac eiddo',
 });
 
 const languages = {
