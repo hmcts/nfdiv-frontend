6f4ec48a4780b38dcfc9b68ce622ed239c681664
pallavijustice <pallavi.saha@justice.gov.uk>
2025-12-04 09:35:15 +0000

    NFDIV-4991 - direct to hub_page

--- src/main/steps/withdrawApplicationSequence.ts
+++ src/main/steps/withdrawApplicationSequence.ts
@@ -1,12 +1,12 @@
 import { YesOrNo } from '../app/case/definition';
 
 import { Step } from './applicant1Sequence';
-import { CHECK_ANSWERS_WITHDRAW, HOME_URL, WITHDRAW_CONFIRMATION, WITHDRAW_THIS_APPLICATION } from './urls';
+import { CHECK_ANSWERS_WITHDRAW, HUB_PAGE, WITHDRAW_CONFIRMATION, WITHDRAW_THIS_APPLICATION } from './urls';
 
 export const withdrawApplicationSequence: Step[] = [
   {
     url: WITHDRAW_THIS_APPLICATION,
-    getNextStep: data => (data?.confirmWithdrawApplication === YesOrNo.YES ? CHECK_ANSWERS_WITHDRAW : HOME_URL),
+    getNextStep: data => (data?.confirmWithdrawApplication === YesOrNo.YES ? CHECK_ANSWERS_WITHDRAW : HUB_PAGE),
   },
   {
     url: CHECK_ANSWERS_WITHDRAW,
