c4d695f7b9aa69e105c45e1ab13c5e91d6cbce58
BWeeks-JusticeGovUK <92433491+BWeeks-JusticeGovUK@users.noreply.github.com>
2025-12-17 14:17:08 +0000

    NFDIV-4923 Hide download link if no downloads available

--- src/main/steps/applicant1/hub-page/hub-links/content.ts
+++ src/main/steps/applicant1/hub-page/hub-links/content.ts
@@ -10,6 +10,7 @@ import {
   WITHDRAW_APPLICATION,
   WITHDRAW_THIS_APPLICATION,
 } from '../../../urls';
+import { downloadsAvailable } from '../../downloads/content';
 
 const en = ({ caseHasBeenIssued, isDivorce }: CommonContent, app2OrRespondent: string) => ({
   reviewContactDetails: {
@@ -66,5 +67,6 @@ export const generateContent: TranslationFn = content => {
   return {
     ...languages[content.language](content, getApp2OrRespondent(content)),
     caseHasBeenIssued: content.caseHasBeenIssued,
+    showDownloadLink: downloadsAvailable(content),
   };
 };
