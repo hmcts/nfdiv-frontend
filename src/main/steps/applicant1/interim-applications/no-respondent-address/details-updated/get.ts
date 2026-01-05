8521e4ed7a62a52eda064b61fa2419576d77350a
pallavijustice <pallavi.saha@justice.gov.uk>
2026-01-05 14:41:32 +0000

    NFDIV-4993 - add autobind decorator

--- src/main/steps/applicant1/interim-applications/no-respondent-address/details-updated/get.ts
+++ src/main/steps/applicant1/interim-applications/no-respondent-address/details-updated/get.ts
@@ -1,7 +1,10 @@
+import autobind from 'autobind-decorator';
+
 import { GetController } from '../../../../../app/controller/GetController';
 
 import { generateContent } from './content';
 
+@autobind
 export class PartnerContactDetailsUpdatedGetController extends GetController {
   constructor() {
     super(__dirname + '/template.njk', generateContent);
