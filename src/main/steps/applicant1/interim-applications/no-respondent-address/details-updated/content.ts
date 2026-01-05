9e157cac1252138e9cd33daf480b933fe22ade61
pallavijustice <pallavi.saha@justice.gov.uk>
2026-01-05 14:19:11 +0000

    NFDIV-4993 - add link for serving outside UK

--- src/main/steps/applicant1/interim-applications/no-respondent-address/details-updated/content.ts
+++ src/main/steps/applicant1/interim-applications/no-respondent-address/details-updated/content.ts
@@ -2,10 +2,10 @@ import config from 'config';
 import dayjs from 'dayjs';
 
 import { getFormattedDate } from '../../../../../app/case/answers/formatDate';
+import { YesOrNo } from '../../../../../app/case/definition';
 import { TranslationFn } from '../../../../../app/controller/GetController';
 import { CommonContent } from '../../../../common/common.content';
 import { HUB_PAGE } from '../../../../urls';
-import { YesOrNo } from '../../../../../app/case/definition';
 
 const en = ({ partner, telephoneNumber, userCase }: CommonContent) => ({
   title: 'Details updated',
@@ -17,6 +17,8 @@ const en = ({ partner, telephoneNumber, userCase }: CommonContent) => ({
   line3: `Your ${partner} will then be sent a copy of the application. They will be asked to check the information and respond. If they do not respond then we’ll tell you what you can do next to progress your application.`,
   line4: `The court will send (serve) the documents to your ${partner}. If you want to do this yourself you can call ${telephoneNumber} to request it.`,
   line5: `You will need to send your ${partner} a copy of the application. They will need to check the information and respond. If they do not respond then we'll tell you what you can do next to progress your application.`,
+  line6:
+    'Further information concerning service out of the United Kingdom can be obtained from the <a class="govuk-link" target="_blank" href="https://www.gov.uk/guidance/service-of-documents-and-taking-of-evidence">Foreign Process Section (opens in a new tab)</a>.',
   returnToYourAccount: `<a href=${HUB_PAGE} class="govuk-link">Return to your account</a>`,
 });
 
@@ -30,6 +32,8 @@ const cy: typeof en = ({ partner, telephoneNumber, userCase }: CommonContent) =>
   line3: `Your ${partner} will then be sent a copy of the application. They will be asked to check the information and respond. If they do not respond then we’ll tell you what you can do next to progress your application.`,
   line4: `The court will send (serve) the documents to your ${partner}. If you want to do this yourself you can call ${telephoneNumber} to request it.`,
   line5: `You will need to send your ${partner} a copy of the application. They will need to check the information and respond. If they do not respond then we'll tell you what you can do next to progress your application.`,
+  line6:
+    'Further information concerning service out of the United Kingdom can be obtained from the <a class="govuk-link" target="_blank" href="https://www.gov.uk/guidance/service-of-documents-and-taking-of-evidence">Foreign Process Section (opens in a new tab)</a>.',
   returnToYourAccount: `<a href=${HUB_PAGE} class="govuk-link">Return to your account</a>`,
 });
 
