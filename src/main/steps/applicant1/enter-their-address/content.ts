67e3a16f75161ec319687c141173d30af343a8ed
pallavijustice <pallavi.saha@justice.gov.uk>
2026-01-15 14:55:32 +0000

    NFDIV-4993 - Rework address fields

--- src/main/steps/applicant1/enter-their-address/content.ts
+++ src/main/steps/applicant1/enter-their-address/content.ts
@@ -5,7 +5,7 @@ import { isFieldFilledIn, isInvalidPostcode } from '../../../app/form/validation
 import { isCountryUk } from '../../applicant1Sequence';
 import type { CommonContent } from '../../common/common.content';
 
-const en = ({ partner }: Partial<CommonContent>) => {
+const en = ({ partner, isDivorce }: Partial<CommonContent>) => {
   const addressPostcode = {
     required: `You have not entered your ${partner}’s postcode. Enter their postcode before continuing.`,
     invalid: 'You have not entered a valid UK postcode. Enter a valid UK postcode before continuing.',
@@ -35,6 +35,11 @@ const en = ({ partner }: Partial<CommonContent>) => {
     no: 'No',
     addressesFound: (addressesFound: number) => `${addressesFound} address${addressesFound !== 1 ? 'es' : ''} found`,
     cannotFindAddress: 'I cannot find the address in the list',
+    serviceOfDocumentsLine1: `For international addresses you will need to send them the ${
+      isDivorce ? 'divorce papers' : 'papers to dissolve the civil partnership'
+    } yourself. You will need to check how to legally serve papers in the country your ${partner} is living in.`,
+    serviceOfDocumentsLine2:
+      'We will post the documents to you so that you can arrange this. You can also download them from your hub.',
     errors: {
       applicant2Address1: {
         required: `You have not entered your ${partner}’s building and street address. Enter their building and street address before continuing.`,
@@ -51,7 +56,7 @@ const en = ({ partner }: Partial<CommonContent>) => {
   };
 };
 
-const cy = ({ partner }: CommonContent) => {
+const cy = ({ partner, isDivorce }: CommonContent) => {
   const addressPostcode = {
     required: `Nid ydych wedi nodi cod post eich ${partner}. Nodwch ei god post cyn parhau.`,
     invalid: 'Nid ydych wedi nodi cod post DU dilys. Nodwch god post DU dilys cyn parhau.',
@@ -82,6 +87,11 @@ const cy = ({ partner }: CommonContent) => {
     addressesFound: (addressesFound: number) =>
       `Wedi canfod ${addressesFound} ${addressesFound !== 1 ? 'gyfeiriad' : 'cyfeiriad'}`,
     cannotFindAddress: "Ni allaf ddod o hyd i'r cyfeiriad yn y rhestr",
+    serviceOfDocumentsLine1: `For international addresses you will need to send them the ${
+      isDivorce ? 'divorce papers' : 'papers to dissolve the civil partnership'
+    } yourself. You will need to check how to legally serve papers in the country your ${partner} is living in.`,
+    serviceOfDocumentsLine2:
+      'We will post the documents to you so that you can arrange this. You can also download them from your hub.',
     errors: {
       applicant2Address1: {
         required: `Nid ydych wedi nodi adeilad a chyfeiriad stryd eich ${partner}. Nodwch ei adeilad a'i gyfeiriad stryd cyn parhau.`,
@@ -128,12 +138,7 @@ export const form: FormContent = {
       classes: 'govuk-label govuk-!-width-two-thirds',
       label: l => l.town,
       labelSize: null,
-      validator: (value, formData) => {
-        if (!isCountryUk(formData.applicant2AddressCountry)) {
-          return;
-        }
-        return isFieldFilledIn(value);
-      },
+      validator: isFieldFilledIn,
     },
     applicant2AddressCounty: {
       id: 'addressCounty',
@@ -154,7 +159,7 @@ export const form: FormContent = {
       },
       validator: (value, formData) => {
         if (!isCountryUk(formData.applicant2AddressCountry)) {
-          return;
+          return isFieldFilledIn(value);
         }
         return isInvalidPostcode(value);
       },
