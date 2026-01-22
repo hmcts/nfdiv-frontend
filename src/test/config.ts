2bab008e5edea85b2cd7e737ba0f43c4e585bd9e
adamg-hmcts <adam.goddard@justice.gov.uk>
2026-01-22 09:42:17 +0000

    Weaken functional page load requirements

--- src/test/config.ts
+++ src/test/config.ts
@@ -172,7 +172,7 @@ config.helpers = {
     waitForAction: 350,
     timeout: config.WaitForTimeout,
     retries: 3,
-    waitForNavigation: 'load',
+    waitForNavigation: 'domcontentloaded',
     ignoreHTTPSErrors: true,
     bypassCSP: true,
     chromium: process.env.PLAYWRIGHT_SERVICE_ACCESS_TOKEN && {
