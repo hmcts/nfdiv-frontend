06fa2b86940412d614d49caee238e71f3d262d85
BWeeks-JusticeGovUK <92433491+BWeeks-JusticeGovUK@users.noreply.github.com>
2026-02-17 16:53:07 +0000

    NFDIV-4757 Refactor radioButtonAnswers into habitually-resident-england-wales

--- src/main/steps/applicant1/habitually-resident-england-wales/content.ts
+++ src/main/steps/applicant1/habitually-resident-england-wales/content.ts
@@ -3,6 +3,7 @@ import { TranslationFn } from '../../../app/controller/GetController';
 import { FormContent } from '../../../app/form/Form';
 import { isFieldFilledIn } from '../../../app/form/validation';
 import type { CommonContent } from '../../common/common.content';
+import { defaultYesOrNoRadioAnswers } from '../../common/input-labels.content';
 
 const en = ({ required }: CommonContent) => ({
   title: 'Were you both last habitually resident in England or Wales and does one of you still live here?',
@@ -48,8 +49,8 @@ export const form: FormContent = {
       label: l => l.title,
       labelHidden: true,
       values: [
-        { label: l => l.yes, value: YesOrNo.YES },
-        { label: l => l.no, value: YesOrNo.NO },
+        { label: l => l[YesOrNo.YES], value: YesOrNo.YES },
+        { label: l => l[YesOrNo.NO], value: YesOrNo.NO },
       ],
       validator: value => isFieldFilledIn(value),
     },
@@ -64,10 +65,14 @@ const languages = {
   cy,
 };
 
+export const radioButtonAnswers = defaultYesOrNoRadioAnswers;
+
 export const generateContent: TranslationFn = content => {
   const translations = languages[content.language](content);
+  const radioAnswers = radioButtonAnswers[content.language];
   return {
     ...translations,
+    ...radioAnswers,
     form,
   };
 };
