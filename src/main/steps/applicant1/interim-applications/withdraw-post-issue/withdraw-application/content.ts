ceee9670f17af3ec3b7c680e1a85c23576c6a79c
pallavijustice <pallavi.saha@justice.gov.uk>
2026-02-25 13:23:52 +0000

    NFDIV-5180 - Change to content in landing page

--- src/main/steps/applicant1/interim-applications/withdraw-post-issue/withdraw-application/content.ts
+++ src/main/steps/applicant1/interim-applications/withdraw-post-issue/withdraw-application/content.ts
@@ -2,10 +2,11 @@ import config from 'config';
 
 import { getFormattedDate } from '../../../../../app/case/answers/formatDate';
 import { TranslationFn } from '../../../../../app/controller/GetController';
+import { getFee } from '../../../../../app/fees/service/get-fee';
 import { FormContent } from '../../../../../app/form/Form';
 import { CommonContent } from '../../../../common/common.content';
 
-const en = ({ partner, isDivorce, referenceNumber, userCase }: CommonContent) => ({
+const en = ({ partner, isDivorce, referenceNumber, userCase }: CommonContent, serviceFee : string) => ({
   title: `Apply to withdraw your ${isDivorce ? 'divorce application' : 'application to end your civil partnership'}`,
   line1: `Reference number: ${referenceNumber}`,
   line2: `${
@@ -14,13 +15,13 @@ const en = ({ partner, isDivorce, referenceNumber, userCase }: CommonContent) =>
   line3: `Use this form to apply to the court to withdraw this ${isDivorce ? 'divorce application' : 'application to end your civil partnership'}.`,
   line4: `We have now sent (issued) the ${
     isDivorce ? 'divorce papers' : 'papers to end your civil partnership'
-  } to your ${partner}. That means there is a fee to withdraw this application, but you may be able to <a class="govuk-link" target="_blank" href="${config.get(
+  } to your ${partner}. That means there is a fee of ${serviceFee}, but you may be able to <a class="govuk-link" target="_blank" href="${config.get(
     'govukUrls.getHelpWithCourtFees'
   )}">get help paying this fee (opens in a new tab)</a>.`,
   buttonText: 'Start now',
 });
 
-const cy = ({ partner, isDivorce, referenceNumber, userCase }: CommonContent) => ({
+const cy = ({ partner, isDivorce, referenceNumber, userCase }: CommonContent, serviceFee: string) => ({
   title: `Apply to withdraw your ${isDivorce ? 'divorce application' : 'application to end your civil partnership'}`,
   line1: `Reference number: ${referenceNumber}`,
   line2: `${
@@ -29,7 +30,7 @@ const cy = ({ partner, isDivorce, referenceNumber, userCase }: CommonContent) =>
   line3: `Use this form to apply to the court to withdraw this ${isDivorce ? 'divorce application' : 'application to end your civil partnership'}.`,
   line4: `We have now sent (issued) the ${
     isDivorce ? 'divorce papers' : 'papers to end your civil partnership'
-  } to your ${partner}. That means there is a fee to withdraw this application, but you may be able to <a class="govuk-link" target="_blank" href="${config.get(
+  } to your ${partner}. That means there is a fee of ${serviceFee}, but you may be able to <a class="govuk-link" target="_blank" href="${config.get(
     'govukUrls.getHelpWithCourtFees'
   )}">get help paying this fee (opens in a new tab)</a>.`,
   buttonText: 'Start now',
@@ -49,7 +50,8 @@ export const form: FormContent = {
 };
 
 export const generateContent: TranslationFn = content => {
-  const translations = languages[content.language](content);
+  const serviceFee = getFee(config.get('fees.generalAppWithoutHearing'));
+  const translations = languages[content.language](content, serviceFee);
   return {
     ...translations,
     form,
