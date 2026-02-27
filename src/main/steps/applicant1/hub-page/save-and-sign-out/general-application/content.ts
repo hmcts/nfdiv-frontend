816ddb7eba8f9bac216827aadd757f6df9500fa1
adamg-hmcts <adam.goddard@justice.gov.uk>
2026-02-27 12:21:38 +0000

    Make save/sign out button text conditional on payment journey

--- src/main/steps/applicant1/hub-page/save-and-sign-out/general-application/content.ts
+++ src/main/steps/applicant1/hub-page/save-and-sign-out/general-application/content.ts
@@ -13,7 +13,7 @@ const en = ({ isDivorce }: CommonContent, hasPaymentInProgress) => ({
     line2: 'You can continue and complete the application or withdraw the application if you no longer want to continue.',
     line3: `If you withdraw the application, your draft will be deleted. This will not affect your ongoing ${isDivorce ? 'divorce application' : 'application to end your civil partnership'}.`,
     line4: `You can make another application to the court at any time.`,
-    continueLinkText: 'Complete payment',
+    continueLinkText: hasPaymentInProgress ? 'Complete payment' : 'Continue application',
     continueLinkUrl: hasPaymentInProgress ? PAY_YOUR_GENERAL_APPLICATION_FEE : MAKE_AN_APPLICATION,
     withdrawLinkText: 'Withdraw application',
     withdrawLinkUrl: '/withdraw'
@@ -29,7 +29,7 @@ const cy: typeof en = ({ isDivorce }: CommonContent, hasPaymentInProgress) => ({
     line2: 'You can continue and complete the application or withdraw the application if you no longer want to continue.',
     line3: `If you withdraw the application, your draft will be deleted. This will not affect your ongoing ${isDivorce ? 'divorce application' : 'application to end your civil partnership'}.`,
     line4: `You can make another application to the court at any time.`,
-    continueLinkText: 'Complete payment',
+    continueLinkText: hasPaymentInProgress ? 'Complete payment' : 'Continue application',
     continueLinkUrl: hasPaymentInProgress ? PAY_YOUR_GENERAL_APPLICATION_FEE : MAKE_AN_APPLICATION,
     withdrawLinkText: 'Withdraw application',
     withdrawLinkUrl: '/withdraw'
