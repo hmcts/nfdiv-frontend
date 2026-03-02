0b027836557fcef05a5f4388ad24e98a07d083aa
pallavijustice <pallavi.saha@justice.gov.uk>
2026-03-02 11:24:15 +0000

    NFDIV-5086 - Added additional translations

--- src/main/steps/applicant1/interim-applications/no-response/process-server-documents/content.ts
+++ src/main/steps/applicant1/interim-applications/no-response/process-server-documents/content.ts
@@ -20,23 +20,22 @@ const en = ({ isDivorce }: CommonContent) => ({
   },
 });
 
-// @TODO translations should be completed then verified
 const cy = ({ isDivorce }: CommonContent) => ({
   title: 'Dogfennau i’w rhoi i’ch gweinyddwr proses',
   applicationDownload: {
     reference: 'Divorce-Application',
     link: `/downloads/${isDivorce ? 'divorce-application' : 'application-to-end-civil-partnership'}`,
-    text: `View the ${isDivorce ? 'divorce application' : 'application to end your civil partnership'} (PDF)`,
+    text: `Gweld y ${isDivorce ? 'cais am ysgariad' : 'cais i ddod a’ch partneriaeth sifil i ben'} (PDF)`,
   },
   applicant2NopDownload: {
     reference: 'Notice of Proceedings',
     link: '/downloads/applicant2-notice-of-proceedings',
-    text: 'View the respondent notice of proceedings (PDF)',
+    text: 'Gweld hysbysiad o achosion yr atebydd (PDF)',
   },
   d10Download: {
     reference: 'D10',
     link: '/downloads/d10',
-    text: 'View the D10 document (PDF)',
+    text: 'Gweld y ddogfen D10 (PDF)',
   },
 });
 
