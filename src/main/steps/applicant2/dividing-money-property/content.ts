f717ff06e50a0fd6c6a2a5533dec16ba2b380fbf
adamg-hmcts <159780146+adamg-hmcts@users.noreply.github.com>
2026-03-02 12:11:12 +0000

    [NFDIV-3155] New amicable frontend journey (#4557)
    * Revert "chore(deps): update dependency html-webpack-plugin to v5.6.6 (#4535)"
    
    This reverts commit b0d494de837dff016675c07647cdbe592fff4212.
    
    * Reorder page sequence and add text for amicable journey
    
    * Content updates following legal and policy review
    
    * Linter corrections
    
    * Fix E2Es
    
    * Correct to api format tests
    
    * Add conditional text based on isDivorce
    
    * NFDIV-4405 Add Welsh Translations (#4586)
    
    * NFDIV-4405 Add Welsh Translations
    
    * NFDIV-4405 Add missing translations
    
    * Minor English and Welsh corrections
    
    * Showing radio button label
    
    ---------
    
    Co-authored-by: elis.davies <elis.davies@justice.gov.uk>
    
    * Make radios label visible
    
    ---------
    
    Co-authored-by: BWeeks-JusticeGovUK <92433491+BWeeks-JusticeGovUK@users.noreply.github.com>
    Co-authored-by: elis.davies <elis.davies@justice.gov.uk>
    Co-authored-by: pallavijustice <pallavi.saha@justice.gov.uk>
    Co-authored-by: pallavijustice <159147694+pallavijustice@users.noreply.github.com>

--- src/main/steps/applicant2/dividing-money-property/content.ts
+++ src/main/steps/applicant2/dividing-money-property/content.ts
@@ -1,18 +1,49 @@
+import { YesOrNo } from '../../../app/case/definition';
 import { TranslationFn } from '../../../app/controller/GetController';
 import { FormContent } from '../../../app/form/Form';
+import { isFieldFilledIn } from '../../../app/form/validation';
 import {
   form as applicant1Form,
   generateContent as applicant1GenerateContent,
 } from '../../applicant1/dividing-money-property/content';
 
+const labels = applicant1Content => {
+  return {
+    errors: {
+      applicant2ApplyForFinancialOrder: {
+        ...applicant1Content.errors.applicant1ApplyForFinancialOrder,
+      },
+    },
+  };
+};
+
 export const form: FormContent = {
   ...applicant1Form,
+  fields: {
+    applicant2ApplyForFinancialOrder: {
+      type: 'radios',
+      classes: 'govuk-radios',
+      label: l => l.doYouWantToApplyForFinancialOrder,
+      labelHidden: false,
+      hint: l => l.hint,
+      values: [
+        { label: l => l.yes, value: YesOrNo.YES },
+        {
+          label: l => l.no,
+          value: YesOrNo.NO,
+          conditionalText: l => `<p class="govuk-label">${l.noSelectedWarning}</p>`,
+        },
+      ],
+      validator: isFieldFilledIn,
+    },
+  },
 };
 
 export const generateContent: TranslationFn = content => {
   const applicant1Content = applicant1GenerateContent(content);
   return {
     ...applicant1Content,
+    ...labels(applicant1Content),
     form,
   };
 };
