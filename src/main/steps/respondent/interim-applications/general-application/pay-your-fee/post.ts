c80b4ae6313c2493dc7d1a0d085281438361f547
adamg-hmcts <adam.goddard@justice.gov.uk>
2026-03-13 09:04:13 +0000

    Reduce duplication in payment post controllers

--- src/main/steps/respondent/interim-applications/general-application/pay-your-fee/post.ts
+++ src/main/steps/respondent/interim-applications/general-application/pay-your-fee/post.ts
@@ -1,45 +1,9 @@
 import autobind from 'autobind-decorator';
-
-import {
-  CITIZEN_GENERAL_APPLICATION,
-  CaseData,
-  Fee,
-  ListValue,
-  OrderSummary,
-} from '../../../../../app/case/definition';
-import { AppRequest } from '../../../../../app/controller/AppRequest';
-import BasePaymentPostController from '../../../../../app/controller/BasePaymentPostController';
-import { AnyObject } from '../../../../../app/controller/PostController';
-import {
-  getGenAppFeeOrderSummary,
-  getGenAppPaymentsField,
-  getGenAppServiceRequest,
-  hasGenAppPaymentInProgress,
-} from '../../../../../app/utils/general-application-utils';
 import { GENERAL_APPLICATION_PAYMENT_CALLBACK, RESPONDENT } from '../../../../urls';
+import BaseGeneralApplicationPaymentPostController from '../../../../applicant1/interim-applications/common/general-application/pay-your-fee/post';
 
 @autobind
-export default class GeneralApplicationPaymentPostController extends BasePaymentPostController {
-  protected readyForPayment(req: AppRequest<AnyObject>): boolean {
-    return hasGenAppPaymentInProgress(req.session.isApplicant2, req.session.userCase);
-  }
-
-  protected awaitingPaymentEvent(): string {
-    return CITIZEN_GENERAL_APPLICATION;
-  }
-
-  protected getFeesFromOrderSummary(req: AppRequest<AnyObject>): ListValue<Fee>[] {
-    return (getGenAppFeeOrderSummary(req) as OrderSummary)?.Fees;
-  }
-
-  protected paymentsCaseField(req: AppRequest<AnyObject>): keyof CaseData {
-    return getGenAppPaymentsField(req) as keyof CaseData;
-  }
-
-  protected getServiceReferenceForFee(req: AppRequest<AnyObject>): string {
-    return getGenAppServiceRequest(req.session.userCase, req.session.isApplicant2) as string;
-  }
-
+export default class GeneralApplicationPaymentPostController extends BaseGeneralApplicationPaymentPostController {
   protected getPaymentCallbackPath(): string {
     return RESPONDENT + GENERAL_APPLICATION_PAYMENT_CALLBACK;
   }
