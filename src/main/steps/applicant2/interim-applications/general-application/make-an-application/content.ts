6496701925dc052d2efa950a67a39032d527bfb3
pallavijustice <pallavi.saha@justice.gov.uk>
2026-03-13 11:16:50 +0000

    NFDIV-5235 Changes to d11 landing page

--- src/main/steps/applicant2/interim-applications/general-application/make-an-application/content.ts
+++ src/main/steps/applicant2/interim-applications/general-application/make-an-application/content.ts
@@ -1,15 +1,9 @@
 import { TranslationFn } from '../../../../../app/controller/GetController';
 import { FormContent } from '../../../../../app/form/Form';
 import {
-  form as d11StartForm,
-  generateContent as d11StartPageContent,
+  form as applicant1Form,
+  generateContent as applicant1GenerateContent,
 } from '../../../../applicant1/interim-applications/general-application/make-an-application/content';
 
-export const form: FormContent = d11StartForm;
-
-export const generateContent: TranslationFn = content => {
-  const pageContent = d11StartPageContent(content);
-  return {
-    ...pageContent,
-  };
-};
+export const form: FormContent = applicant1Form;
+export const generateContent: TranslationFn = applicant1GenerateContent;
