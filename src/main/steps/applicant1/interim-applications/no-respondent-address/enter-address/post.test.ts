e138fcb172cec4661c587107d763278f3aabce22
pallavijustice <pallavi.saha@justice.gov.uk>
2026-03-25 15:00:01 +0000

    NFDIV-5328 - changes

---
+++ src/main/steps/applicant1/interim-applications/no-respondent-address/enter-address/post.test.ts
@@ -0,0 +1,35 @@
+import { mockRequest } from '../../../../../../test/unit/utils/mockRequest';
+import { mockResponse } from '../../../../../../test/unit/utils/mockResponse';
+import { CaseWithId } from '../../../../../app/case/case';
+import { CITIZEN_UPDATE, YesOrNo } from '../../../../../app/case/definition';
+
+import EnterTheirAddressPostController from './post';
+
+describe('EnterTheirAddressPostController', () => {
+  let userCase: Partial<CaseWithId>;
+  beforeEach(() => {
+    userCase = { id: '1234' };
+  });
+
+  it('Set undefined isOverseas fields to No', async () => {
+    const body = {
+      applicant1NoRespAddressAddressOverseas: undefined,
+    };
+
+    const expectedBody = {
+      applicant1NoRespAddressAddressOverseas: YesOrNo.NO,
+    };
+
+    const enterTheirAddressPostController = new EnterTheirAddressPostController({});
+
+    const req = mockRequest({ body, session: { userCase, isApplicant2: false } });
+    const res = mockResponse();
+    await enterTheirAddressPostController.post(req, res);
+
+    expect(req.locals.api.triggerEvent).toHaveBeenCalledWith(
+      '1234',
+      expectedBody,
+      CITIZEN_UPDATE
+    );
+  });
+});
