7516d47ffa163a6be8acda8b8d1276980e02d6a7
adamg-hmcts <159780146+adamg-hmcts@users.noreply.github.com>
2026-04-02 07:37:30 +0100

    [NFDIV-5447] Joint general application label (#4720)
    * Use new label for joint applicant 2 general applications
    
    * Joint applicant 2 general application label
    
    * Linter corrections

--- src/main/app/utils/general-application-utils.ts
+++ src/main/app/utils/general-application-utils.ts
@@ -20,17 +20,20 @@ const D11_GENERAL_APPLICATION_EXCLUDED_STATES: Set<State> = new Set([
   State.AwaitingGeneralReferralPayment,
 ]);
 
+const APPLICANT1_GEN_APP_PARTY_NAMES = new Set<GeneralParties>([GeneralParties.APPLICANT]);
+const APPLICANT2_GEN_APP_PARTY_NAMES = new Set<GeneralParties>([GeneralParties.RESPONDENT, GeneralParties.APPLICANT2]);
+
 export const findAllOnlineGenAppsForUser = (
   userCase: Partial<CaseWithId>,
   isApplicant2: boolean
 ): GeneralApplication[] | undefined => {
-  const generalApplicationParty = isApplicant2 ? GeneralParties.RESPONDENT : GeneralParties.APPLICANT;
+  const generalApplicationPartyNames = isApplicant2 ? APPLICANT2_GEN_APP_PARTY_NAMES : APPLICANT1_GEN_APP_PARTY_NAMES;
 
   return userCase?.generalApplications
     ?.map(generalApplicationValue => generalApplicationValue.value)
     ?.filter(
       application =>
-        application?.generalApplicationParty === generalApplicationParty &&
+        generalApplicationPartyNames.has(application?.generalApplicationParty as GeneralParties) &&
         application?.generalApplicationSubmittedOnline === YesOrNo.YES
     );
 };
@@ -85,8 +88,7 @@ export const hasGenAppSaveAndSignOutContent = (isApplicant2: boolean, userCase: 
     ? userCase.applicant2InterimApplicationType
     : userCase.applicant1InterimApplicationType;
 
-  const isDraftingD11Application =
-    interimApplicationType === InterimApplicationType.DIGITISED_GENERAL_APPLICATION_D11;
+  const isDraftingD11Application = interimApplicationType === InterimApplicationType.DIGITISED_GENERAL_APPLICATION_D11;
   const genAppAwaitingPayment = findGenAppAwaitingPayment(userCase, isApplicant2);
 
   const hasD11ApplicationPaymentInProgress =
