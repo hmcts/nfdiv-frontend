aeaf348350c522931f667d625bb934a980e04cf0
adamg-hmcts <adam.goddard@justice.gov.uk>
2026-04-16 08:56:31 +0100

    No respondent address updates based on E2E tests

--- src/main/steps/applicant1/interim-applications/common/upload-evidence/content.ts
+++ src/main/steps/applicant1/interim-applications/common/upload-evidence/content.ts
@@ -13,7 +13,7 @@ const en = ({ partner }: CommonContent, applicant1UploadDocumentContent) => ({
   statement: '',
   certifiedTranslation: `If your evidence is a conversation in a language other than English, you'll need to provide a <a class="govuk-link" target="_blank" href="${config.get(
     'govukUrls.certifiedTranslation'
-  )}">certified translation</a>.`,
+  )}">certified translation (opens in a new tab)</a>.`,
   toInclude: {
     header:
       "If you're uploading images or screenshots of a recent conversation by text, email or social media, make sure they include:",
@@ -51,7 +51,7 @@ const cy: typeof en = ({ partner }: CommonContent, applicant1UploadDocumentConte
   statement: '',
   certifiedTranslation: `Os yw eich tystiolaeth mewn iaith nad yw’n Saesneg, bydd arnoch angen darparu  <a class="govuk-link" target="_blank" href="${config.get(
     'govukUrls.certifiedTranslation'
-  )}">cyfieithiad ardystiedig</a>.`,
+  )}">cyfieithiad ardystiedig (yn agor mewn tab newydd)</a>.`,
   toInclude: {
     header:
       'Os ydych yn uwchlwytho delweddau o sgwrs ddiweddar trwy neges destun, e-bost neu’r cyfryngau cymdeithasol, gwnewch yn siŵr eu bod yn cynnwys:',
