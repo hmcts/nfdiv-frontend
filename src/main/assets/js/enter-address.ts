dbce511657d9f371ec37a52e2778acd623560e51
FaisalMoJ <faisal.amin@justice.gov.uk>
2026-04-28 14:35:38 +0100

    NFDIV-5141: Fix issues with addresses across different pages

--- src/main/assets/js/enter-address.ts
+++ src/main/assets/js/enter-address.ts
@@ -1,3 +1,5 @@
+import { isCountryUk } from '../../steps/applicant1Sequence';
+
 import {
   hideEnterPostcode,
   hideInternationalAddressFields,
@@ -14,22 +16,22 @@ import './address/submit';
 const form = getById('main-form') as HTMLFormElement | null;
 if (form && getById('enterPostcode')) {
   const formData = new FormData(form);
-  const uk = 'UK';
   const applicant1AddressCountry = formData.get('applicant1AddressCountry');
   const applicant2AddressCountry = formData.get('applicant2AddressCountry');
-  const applicant1SearchGovRecordsPartnerLastKnownAddress1 = formData.get(
-    'applicant1SearchGovRecordsPartnerLastKnownAddress1'
+  const applicant1SearchGovRecordsPartnerLastKnownAddressCountry = formData.get(
+    'applicant1SearchGovRecordsPartnerLastKnownAddressCountry'
   );
   const addressCountry =
-    applicant1AddressCountry || applicant2AddressCountry || applicant1SearchGovRecordsPartnerLastKnownAddress1;
+    applicant1AddressCountry || applicant2AddressCountry || applicant1SearchGovRecordsPartnerLastKnownAddressCountry;
   const hasBackendError = qsa('.govuk-error-summary').length > 1;
 
   if (addressCountry || hasBackendError) {
     hideEnterPostcode();
-    showUkAddressFields();
 
-    if ((addressCountry && addressCountry !== uk) || (!addressCountry && hasBackendError)) {
+    if ((addressCountry && !isCountryUk(addressCountry as string)) || (!addressCountry && hasBackendError)) {
       showInternationalAddressFields();
+    } else {
+      showUkAddressFields();
     }
   } else {
     hideUkAddressFields();
