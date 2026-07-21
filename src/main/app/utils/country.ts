b84e9a246425d2c24c24586a604657a56569cd64
adamg-hmcts <adam.goddard@justice.gov.uk>
2026-07-21 13:21:20 +0100

    Remove country javascript

--- src/main/app/utils/country.ts
+++
@@ -1,6 +0,0 @@
-export const isCountryUk = (value: string | undefined): boolean => {
-  const ukTerms = ['uk', 'unitedkingdom', 'u.k', 'u.k.'];
-  const country = value || '';
-
-  return ukTerms.includes(country.replace(' ', '').toLowerCase());
-};
