d87618ed754187e18f8ba1280533a3a8053662b8
adamg-hmcts <adam.goddard@justice.gov.uk>
2026-07-23 10:42:21 +0100

    Set up new Azure managed redis cache

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -11,6 +11,17 @@ data "azurerm_key_vault" "key_vault" {
   resource_group_name = local.vaultName
 }
 
+data "azurerm_subnet" "redis_private_endpoint" {
+  name                 = "core-infra-subnet-2-${var.env}"
+  resource_group_name  = "core-infra-${var.env}"
+  virtual_network_name = "core-infra-vnet-${var.env}"
+}
+
+data "azurerm_key_vault" "product_key_vault" {
+  name                = "${var.product}si-${var.env}"
+  resource_group_name = "${var.product}-shared-infrastructure-${var.env}"
+}
+
 module "nfdiv-frontend-redis6" {
   source                        = "git@github.com:hmcts/cnp-module-redis?ref=master"
   product                       = var.product
@@ -28,8 +39,36 @@ module "nfdiv-frontend-redis6" {
 
 }
 
+module "managed_redis" {
+  source = "git@github.com:hmcts/terraform-module-azure-managed-redis?ref=main"
+
+  product     = var.product
+  component   = var.component
+  env         = var.env
+  location    = var.location
+  common_tags = var.common_tags
+
+  sku_name = var.managed_redis_sku
+
+  public_network_access   = "Disabled"
+  create_private_endpoint = true
+  subnet_id               = data.azurerm_subnet.redis_private_endpoint.id
+  private_dns_zone_ids = [
+    "/subscriptions/${var.private_dns_subscription_id}/resourceGroups/core-infra-intsvc-rg/providers/Microsoft.Network/privateDnsZones/privatelink.redis.azure.net"
+  ]
+
+  access_keys_authentication_enabled = true
+  persistence_rdb_backup_frequency   = var.managed_redis_persistence_rdb_frequency
+}
+
 resource "azurerm_key_vault_secret" "redis6_access_key" {
   name         = "redis6-access-key"
   value        = module.nfdiv-frontend-redis6.access_key
   key_vault_id = data.azurerm_key_vault.key_vault.id
+}
+
+resource "azurerm_key_vault_secret" "managed_redis_connection_string" {
+  name         = "azure-managed-redis-connection-string"
+  value        = "rediss://default:${urlencode(module.managed_redis.primary_access_key)}@${module.managed_redis.hostname}:${module.managed_redis.port}"
+  key_vault_id = data.azurerm_key_vault.product_key_vault.id
 }
