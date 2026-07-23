54034c967e3a9c567cba1f4426270170a185221e
54687 <61242337+hmcts-jenkins-j-to-z[bot]@users.noreply.github.com>
2026-07-23 14:28:30 +0000

    Updating Terraform Formatting

--- infrastructure/import.tf
+++ infrastructure/import.tf
@@ -1,4 +1,4 @@
 import {
-    to = module.managed_redis.azurerm_managed_redis.redis
-    id = "/subscriptions/1c4f0704-a29e-403d-b719-b90c34ef14c9/resourceGroups/nfdiv-frontend-aat-rg/providers/Microsoft.Cache/redisEnterprise/nfdiv-frontend-aat"
-  }
+  to = module.managed_redis.azurerm_managed_redis.redis
+  id = "/subscriptions/1c4f0704-a29e-403d-b719-b90c34ef14c9/resourceGroups/nfdiv-frontend-aat-rg/providers/Microsoft.Cache/redisEnterprise/nfdiv-frontend-aat"
+}
