e0293ca074b08c7eef59697882dd2ffd3eb4e594
BWeeks-JusticeGovUK <92433491+BWeeks-JusticeGovUK@users.noreply.github.com>
2026-08-12 15:56:25 +0100

    NFDIV-5586 Correct step path

--- src/main/steps/getStepTemplatePath.ts
+++ src/main/steps/getStepTemplatePath.ts
@@ -1,9 +1,6 @@
 import path from 'path';
 
-const isTestRuntime = process.env.NODE_ENV === 'test' || Boolean(process.env.JEST_WORKER_ID);
-const runtimeStepsBaseDir = isTestRuntime
-  ? path.resolve(process.cwd(), 'src/main/steps')
-  : path.resolve(process.cwd(), 'src/main/main/steps');
+const runtimeStepsBaseDir = path.resolve(process.cwd(), 'src/main/steps');
 
 export const getStepTemplatePath = (stepPath: string, templateName: string): string => {
   return path.resolve(runtimeStepsBaseDir, stepPath, templateName);
