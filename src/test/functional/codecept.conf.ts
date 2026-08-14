2d5dfc7db0e70234d5acb22ade369ceb5e39c3c7
BWeeks-JusticeGovUK <92433491+BWeeks-JusticeGovUK@users.noreply.github.com>
2026-08-14 15:09:20 +0100

    Improve test reporting

--- src/test/functional/codecept.conf.ts
+++ src/test/functional/codecept.conf.ts
@@ -1,14 +1,18 @@
+import path from 'node:path';
+
 import { setHeadlessWhen } from '@codeceptjs/configure';
 
 import { config as testConfig } from '../config.js';
 
 setHeadlessWhen(testConfig.TestHeadlessBrowser);
 
+const reportDir = path.resolve(process.cwd(), 'functional-output/functional/reports');
+
 export const config: CodeceptJS.Config = {
   noGlobals: true,
   name: 'nfdiv-frontend-functional',
   gherkin: testConfig.Gherkin,
-  output: '../../../functional-output/functional/reports',
+  output: reportDir,
   helpers: testConfig.helpers,
   bootstrap: testConfig.bootstrap,
   teardown: testConfig.teardown,
@@ -20,13 +24,17 @@ export const config: CodeceptJS.Config = {
     retryFailedStep: {
       enabled: true,
     },
+    allure: {
+      enabled: true,
+      require: 'allure-codeceptjs',
+    },
     screenshot: {
       enabled: true,
       fullPageScreenshots: true,
     },
     junitReporter: {
       enabled: true,
-      output: 'functional-output/functional/reports',
+      output: reportDir,
       outputName: 'result.xml',
     },
   },
