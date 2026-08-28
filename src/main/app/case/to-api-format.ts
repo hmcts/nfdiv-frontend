cd9f504214c7c99da492269b55eb77f1faabeb8c
BWeeks-JusticeGovUK <92433491+BWeeks-JusticeGovUK@users.noreply.github.com>
2026-08-28 09:01:22 +0100

    NFDIV-5586 Use checkboxConverter

--- src/main/app/case/to-api-format.ts
+++ src/main/app/case/to-api-format.ts
@@ -33,18 +33,11 @@ import {
   applicant1SearchGovRecordsPartnerLastKnownAddressToApi,
   applicant2AddressToApi,
 } from './formatter/address.js';
+import { checkboxToYesNo as checkboxConverter } from './formatter/checkbox-converter';
 
 export type OrNull<T> = { [K in keyof T]: T[K] | null };
 
 type ToApiConverters = Partial<Record<keyof Case, string | ((data: Case) => OrNull<Partial<CaseData>>)>>;
-
-const checkboxConverter = (value: string | undefined) => {
-  if (value === null) {
-    return null;
-  }
-
-  return value === Checkbox.Checked ? YesOrNo.YES : YesOrNo.NO;
-};
 
 const prayerConverter = (applicant: 'applicant1' | 'applicant2') => {
   return data => {
