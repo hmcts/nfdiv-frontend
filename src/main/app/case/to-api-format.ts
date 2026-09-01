2d6a200186bc80cbdcebf16e57eeb25f3f7df919
FaisalMoJ <faisal.amin@justice.gov.uk>
2026-09-01 11:15:23 +0100

    NFDIV-5756: Fix single document upload checkbox issue

--- src/main/app/case/to-api-format.ts
+++ src/main/app/case/to-api-format.ts
@@ -192,17 +192,22 @@ const fields: ToApiConverters = {
       ? data.applicant2NameChangedHowOtherDetails
       : '',
   }),
-  applicant1CannotUploadDocuments: data => ({
-    applicant1CannotUploadSupportingDocument:
-      data.applicant1CannotUpload === Checkbox.Checked && data.applicant1CannotUploadDocuments
-        ? !Array.isArray(data.applicant1CannotUploadDocuments)
-          ? [data.applicant1CannotUploadDocuments]
-          : data.applicant1CannotUploadDocuments
-        : [],
-  }),
-  applicant1CannotUpload: data => ({
-    applicant1CannotUpload: data.applicant1CannotUpload === Checkbox.Checked ? YesOrNo.YES : YesOrNo.NO,
-  }),
+  applicant1CannotUploadDocuments: data => {
+    const cannotUpload = Object.prototype.hasOwnProperty.call(data, 'applicant1CannotUpload')
+      ? data.applicant1CannotUpload === Checkbox.Checked
+      : (data.applicant1CannotUploadDocuments?.length ?? 0) > 0;
+
+    return {
+      applicant1CannotUploadSupportingDocument:
+        cannotUpload && data.applicant1CannotUploadDocuments
+          ? !Array.isArray(data.applicant1CannotUploadDocuments)
+            ? [data.applicant1CannotUploadDocuments]
+            : data.applicant1CannotUploadDocuments
+          : [],
+
+      applicant1CannotUpload: cannotUpload ? YesOrNo.YES : YesOrNo.NO,
+    };
+  },
   applicant2CannotUploadDocuments: data => ({
     applicant2CannotUploadSupportingDocument: data.applicant2CannotUploadDocuments
       ? !Array.isArray(data.applicant2CannotUploadDocuments)
