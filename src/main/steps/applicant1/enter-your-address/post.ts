f0382eb35c355c5a37c7d2923285d962f821a769
FaisalMoJ <100577377+FaisalMoJ@users.noreply.github.com>
2026-09-07 16:20:46 +0100

    NFDIV-5743: Add logic to only run event when data changes (#4979)
    * NFDIV-5743: Add logic to only run event when data changes
    
    * NFDIV-5743: Refactor logic
    
    * NFDIV-5743: Add unit tests

--- src/main/steps/applicant1/enter-your-address/post.ts
+++ src/main/steps/applicant1/enter-your-address/post.ts
@@ -1,19 +1,26 @@
 import autobind from 'autobind-decorator';
+import { Response } from 'express';
 
-import { Case, CaseWithId } from '../../../app/case/case';
+import { Case } from '../../../app/case/case';
 import { YesOrNo } from '../../../app/case/definition';
 import { AppRequest } from '../../../app/controller/AppRequest';
 import { AnyObject } from '../../../app/controller/PostController';
+import { Form } from '../../../app/form/Form';
 import CitizenUpdateContactDetailsPostController from '../check-phone-number/post';
 
 @autobind
 export default class EnterYourAddressPostController extends CitizenUpdateContactDetailsPostController {
-  protected async save(req: AppRequest<AnyObject>, formData: Partial<Case>, eventName: string): Promise<CaseWithId> {
+  protected async saveAndContinue(
+    req: AppRequest<AnyObject>,
+    res: Response,
+    form: Form,
+    formData: Partial<Case>
+  ): Promise<void> {
     if (req.session.isApplicant2) {
       formData.applicant2AddressOverseas ??= YesOrNo.NO;
     } else {
       formData.applicant1AddressOverseas ??= YesOrNo.NO;
     }
-    return super.save(req, formData, eventName);
+    await super.saveAndContinue(req, res, form, formData);
   }
 }
